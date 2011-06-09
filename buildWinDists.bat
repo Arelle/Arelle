fda7df901cd600dcc4c8925d685f602b2429d925
Herman Fischer <fischer@markv.com>
2011-06-09 16:52:06 -0700

    update to build with lxml module

--- buildWinDists.bat
+++ buildWinDists.bat
@@ -4,6 +4,7 @@ rem both win 32 (x86) and win 64 (x64)
 @set PYTHON32DIR=c:\python32x86
 @set PYTHON64DIR=c:\python32
 @set NSISDIR=C:\Program Files (x86)\NSIS
+@set CMDLINEZIP=C:\Program Files (x86)\7z\7za.exe
 @set BUILT32DIR=build\exe.win32-3.2
 @set BUILT64DIR=build\exe.win-amd64-3.2
 
@@ -16,16 +17,26 @@ mkdir dist
 
 rem win 32 (x86) build
 "%PYTHON32DIR%\python" setup.py build_exe
+rem fix up lxml missing modules in cx_freeze build
+mkdir lxml
+copy "%PYTHON32DIR%\Lib\site-packages\lxml\__pycache__\_elementpath.cpython-32.pyc" lxml\_elementpath.pyc
+"%CMDLINEZIP%" a "%BUILT32DIR%\library.zip" lxml\_elementpath.pyc
+rmdir lxml/s/q
 "%NSISDIR%\makensis" installWin86.nsi
 rem rename for build date
 call buildRenameX86.bat
 
 rem win 64 build
 "%PYTHON64DIR%\python" setup.py build_exe
+rem fix up lxml missing modules in cx_freeze build
+mkdir lxml
+copy "%PYTHON64DIR%\Lib\site-packages\lxml\__pycache__\_elementpath.cpython-32.pyc" lxml\_elementpath.pyc
+"%CMDLINEZIP%" a "%BUILT64DIR%\library.zip" lxml\_elementpath.pyc
+rmdir lxml/s/q
 "%NSISDIR%\makensis" installWin64.nsi
 rem rename for build date
 call buildRenameX64.bat
 
 rem compact /c /f dist\exe.win32-3.2.exe
 
-rmdir build /s/q
+rem rmdir build /s/q
