c5f381aa0c233e4ef0dd4ad1499ce449c49e6447
Herman Fischer <fischer@markv.com>
2011-06-24 11:59:53 -0700

    update mac build process for lxml version

---
+++ arelle/Version.py
@@ -0,0 +1,8 @@
+'''
+This module represents the time stamp when Arelle was last built
+
+@author: Mark V Systems Limited
+(c) Copyright 2011 Mark V Systems Limited, All rights reserved.
+
+'''
+version = '2011-06-24 17:52 UTC'
