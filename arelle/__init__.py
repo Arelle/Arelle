f1fddc2a38bd7012b2cc37e09879b2d931617192
irjudson <irjudson@gmail.com>
2011-06-30 13:36:51 -0600

    Introducing six back into the mix.

---
+++ arelle/__init__.py
@@ -0,0 +1,10 @@
+from arelle.externals import six
+
+arelle_mods = [
+    ["urllib_parse", "urlparse", "urllib.parse"],
+    ["urllib_request", "urllib", "urllib.request"],
+    ["urllib_error", "urllib2", "urllib.error"]
+]
+
+for mod in arelle_mods:
+    six.add_move(six.MovedModule(*mod))
