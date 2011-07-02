8206de7f37ccd525e297846d46cbc21a9f97c1f4
Herman Fischer <fischer@markv.com>
2011-07-02 11:21:28 -0700

    Adapt Windows and MacOS applications for command line operation as built and installed.  (See arelle.org/documentation/command-line-operation)
    Adds new environment variable ARELLE_ARGS to get around MacOS
    inability to pass argv to python built application.
    
    Conflicts:
    
    	setup.py

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2011 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2011-07-01 05:45 UTC'
+version = '2011-07-02 15:58 UTC'
