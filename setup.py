764a8dce2559d1f5e81811c01ecbd11889197702
irjudson <irjudson@gmail.com>
2011-07-12 09:29:20 -0600

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -57,6 +57,13 @@ elif sys.platform == 'win32':
                 base="Win32GUI",
                 )                            
         ]
+else:
+    from setuptools import os, setup, find_packages
+    packages = find_packages('.')
+    dataFiles = [        
+	('config',['arelle/config/' + f for f in os.listdir('arelle/config')]),
+      ]
+    cx_FreezeExecutables = None
 
 setup(name='Arelle',
       version='0.9.0',
