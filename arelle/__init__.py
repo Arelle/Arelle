e9977580ce888fd961ac1b6f5995ecc015a85eeb
Ivan R. Judson <irjudson@gmail.com>
2011-07-19 11:48:39 -0600

    Adding tracing functions for following code execution.
    Signed-off-by: Ivan R. Judson <irjudson@gmail.com>

--- arelle/__init__.py
+++ arelle/__init__.py
@@ -8,3 +8,46 @@ arelle_mods = [
 
 for mod in arelle_mods:
     six.add_move(six.MovedModule(*mod))
+
+import os, sys, linecache, inspect, os.path
+
+def traceit(frame, event, arg):
+    depth = len(inspect.stack())
+    lineno = frame.f_lineno
+    if '__file__' in frame.f_globals:
+        filename = frame.f_globals['__file__']
+        if (filename.endswith('.pyc') or
+            filename.endswith('.pyo')):
+            filename = filename[:-1]
+        name = frame.f_globals['__name__']
+        line = linecache.getline(filename, lineno)
+    else:
+        name = '[unknown]'
+        try:
+            src = inspect.getsourcelines(frame)
+            line = src[lineno]
+        except IOError:
+            line = 'Unknown code named [%s].  VM instruction #%d' % \
+                   (frame.f_code.co_name, frame.f_lasti)
+            
+    if event == 'call':
+        fi = inspect.getframeinfo(frame)
+        print("%s -> %s [%s:%s]" % (" "*depth, name, os.path.basename(fi[0]), lineno))
+    elif event == 'line':
+        # We don't want line level details
+        pass
+    elif event == 'return':
+        fi = inspect.getframeinfo(frame)
+        print("%s <- %s [%s:%s]" % (" "*depth, name, os.path.basename(fi[0]), lineno))
+    elif event == 'exception':
+        # We don't want exception details, yet
+        pass
+    elif event == 'c_call':
+        print("%s -> %s [%s:%s]" % (" "*depth, name, os.path.basename(fi[0]), lineno))
+    elif event == 'c_return':
+        print("%s <- %s [%s:%s]" % (" "*depth, name, os.path.basename(fi[0]), lineno))
+    elif event == 'c_exception':
+        # We don't want exception details, yet
+        pass
+
+    return traceit
