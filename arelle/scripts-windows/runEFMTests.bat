fdaa8c35a28d50265d61c0488fd98842f9b0216b
Herman Fischer <fischer@markv.com>
2011-08-12 10:41:27 -0700

    add utr validation to efm test run

--- arelle/scripts-windows/runEFMTests.bat
+++ arelle/scripts-windows/runEFMTests.bat
@@ -10,4 +10,4 @@ rem Please edit to change the output log and output csv file locations
 
 @set ARELLE=c:\Program Files\Arelle\arelleCmdLine.exe
 
-"%ARELLE%" --file "%TESTCASESINDEXFILE%" --efm --validate --csvTestReport "%OUTPUTCSVFILE%" 1>  "%OUTPUTLOGFILE%" 2>&1
+"%ARELLE%" --file "%TESTCASESINDEXFILE%" --efm --utr --validate --csvTestReport "%OUTPUTCSVFILE%" 1>  "%OUTPUTLOGFILE%" 2>&1
