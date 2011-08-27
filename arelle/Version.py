aa455b189d37951beededbaf750601b8307f9d77
Mike Ivanov <mike@mikeivanov.com>
2011-08-26 17:48:47 -0700

    Don't track build artifacts

--- arelle/Version.py
+++
@@ -1,8 +0,0 @@
-'''
-This module represents the time stamp when Arelle was last built
-
-@author: Mark V Systems Limited
-(c) Copyright 2011 Mark V Systems Limited, All rights reserved.
-
-'''
-version = '2011-06-09 23:24 UTC'
