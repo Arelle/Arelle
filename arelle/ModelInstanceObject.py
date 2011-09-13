bf9b93bd3f39ba22637def525cbeb55739ce2792
Herman Fischer <fischer@markv.com>
2011-09-13 11:41:47 -0700

    change measures to use xml-validated PSVI qnames, was messing up on space collapsing

--- arelle/ModelInstanceObject.py
+++ arelle/ModelInstanceObject.py
@@ -698,8 +698,7 @@ class ModelDimensionValue(ModelObject):
             return (str(self.dimensionQname), etree.tounicode( XmlUtil.child(self) ) )
         
 def measuresOf(parent):
-    return sorted([ModelValue.qname(m, XmlUtil.text(m)) 
-                   for m in parent.iterchildren(tag="{http://www.xbrl.org/2003/instance}measure")])
+    return sorted([m.xValue for m in parent.iterchildren(tag="{http://www.xbrl.org/2003/instance}measure")])
 
 def measuresStr(m):
     return m.localName if m.namespaceURI in (XbrlConst.xbrli, XbrlConst.iso4217) else str(m)
