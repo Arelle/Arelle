dc1f6c7da87e1c02579253724a9517a8317accf8
Herman Fischer <fischer@markv.com>
2011-10-06 11:14:52 -0700

    change EFM tests to write log to file using file handler for utf-8 encoding for new errors with unicode characters in them

--- scripts/runEFMTests.bat
+++ scripts/runEFMTests.bat
@@ -1,10 +1,12 @@
 rem Run SEC Edgar Filer Manual (EFM) Conformance Suite tests
 
-@set TESTCASESROOT=C:\Users\Herm Fischer\Documents\mvsl\projects\SEC\Local.Conformance\conformance\Private\Formula\Extension-Conformance\root\efm-16-110225\conf
+@set TESTCASESROOT=C:\Users\Herm Fischer\Documents\mvsl\projects\SEC\Local.Conformance\conformance\Private\Formula\Extension-Conformance\root\efm-18-111004\conf
 
 @set TESTCASESINDEXFILE=%TESTCASESROOT%\testcases.xml
 
 @set OUTPUTLOGFILE=c:\temp\EFM-test-log.txt
+
+@set OUTPUTERRFILE=c:\temp\EFM-test-err.txt
 
 @set OUTPUTCSVFILE=c:\temp\EFM-test-report.csv
 
@@ -13,4 +15,4 @@ rem Run SEC Edgar Filer Manual (EFM) Conformance Suite tests
 @set PYTHONDIR=c:\python31
 @set PYTHONPATH=..
 
-"%PYTHONDIR%\python" -m arelle.CntlrCmdLine --file "%TESTCASESINDEXFILE%" --efm --utr --validate --csvTestReport "%OUTPUTCSVFILE%" 1>  "%OUTPUTLOGFILE%" 2>&1
+"%PYTHONDIR%\python" -m arelle.CntlrCmdLine --file "%TESTCASESINDEXFILE%" --efm --utr --validate --csvTestReport "%OUTPUTCSVFILE%" --logFile "%OUTPUTLOGFILE%" 2>  "%OUTPUTERRFILE%"
