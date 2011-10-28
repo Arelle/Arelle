c05da1befe58e1edc9185641961db61e85dd5d0b
Marcos Marado <mindboosternoori@gmail.com>
2011-10-28 18:18:51 +0100

    added Linux build script... even if it's just a stub right now

---
+++ buildLinuxDist.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# this isn't done yet...
+echo "Sorry, Linux support isn't written yet..."
+exit
+
+# remove old build
+/bin/rm -rf build
+/bin/rm -rf dist
+
+# set the build date in version.py
+python3.2 buildVersion.py
+
+# create new app
+python3.2 setup.py
+
+# add the icon file to resources
+cp -R arelle/images/arelle.icns dist/Arelle.app/Contents/Resources
+
+# add icon and config files to resources
+cp -R arelle/images dist/Arelle.app/Contents/Resources
+cp -R arelle/config dist/Arelle.app/Contents/Resources
+cp -R arelle/examples dist/Arelle.app/Contents/Resources
+cp -R arelle/locale dist/Arelle.app/Contents/Resources
+
+# add tcl and tk 8.6 versions
+cp -R /library/frameworks/tcl.framework/versions dist/Arelle.app/Contents/Frameworks/Tcl.framework
+cp -R /library/frameworks/tk.framework/versions dist/Arelle.app/Contents/Frameworks/Tk.framework
+rm -R dist/Arelle.app/Contents/Frameworks/Tcl.framework/Versions/8.5
+rm -R dist/Arelle.app/Contents/Frameworks/Tk.framework/Versions/8.5
+
+# add lxml _elementpath to lib
+#mkdir lxml
+#cp ~/Library/Python/3.2/lib/python/site-packages/lxml/__pycache__/_elementpath.cpython-32.pyc lxml/_elementpath.pyc
+#zip -u dist/Arelle.app/Contents/Resources/lib/python32.zip lxml lxml/_elementpath.pyc
+#/bin/rm -rf lxml
+/bin/rm -rf dist_pkg
+
+mkdir dist_pkg
+/developer/applications/utilities/packagemaker.app/contents/macos/packagemaker --root dist --id org.arelle.Arelle --title 'Arelle Open Source XBRL Platform' --out dist_pkg/arelle.pkg --no-recommend --verbose --version 2011041601
+#/developer/applications/utilities/packagemaker.app/contents/macos/packagemaker --doc arelle.pmdoc --out dist_pkg/arelle.pkg --verbose
+
+# create the .dmg file
+/bin/rm -rf dist_dmg
+mkdir dist_dmg
+#hdiutil create -fs HFS+ -volname "ARELLE" -srcfolder dist_pkg dist_dmg/arelle.dmg
+hdiutil create -fs HFS+ -volname "ARELLE" -srcfolder dist dist_dmg/arelle.dmg
+
+# rename the .dmg file with the exact same version date as Version.py
+sh -x buildRenameDmg.sh
+
+# delete the built application so that the package can be installed to test it
+#/bin/rm -rf dist
+
+
+
+
