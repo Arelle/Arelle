30bef00de1f7b2eac91afe40a7dd332ce84f9752
Herman Fischer <fischer@markv.com>
2011-12-18 15:48:17 -0800

    fix shutdown glitch if modelXbrl already closed but still visible

--- arelle/CntlrWinMain.py
+++ arelle/CntlrWinMain.py
@@ -883,7 +883,7 @@ class CntlrWinMain (Cntlr.Cntlr):
             self.saveConfig()
         
     def setValidateTooltipText(self):
-        if self.modelManager.modelXbrl and self.modelManager.modelXbrl.modelDocument:
+        if self.modelManager.modelXbrl and hasattr(self.modelManager.modelXbrl,"modelDocument"):
             valType = self.modelManager.modelXbrl.modelDocument.type
             if valType == ModelDocument.Type.TESTCASESINDEX:
                 v = _("Validate testcases")
