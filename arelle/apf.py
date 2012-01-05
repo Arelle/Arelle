ccfb1e9c7b8e0bff924999812b380d432b65fd6a
Régis Décamps <decamps@users.sf.net>
2012-01-05 21:09:59 +0100

    Utility function to load the locale easily (also provides omre checks)

--- arelle/apf.py
+++ arelle/apf.py
@@ -1,14 +1,32 @@
 __author__ = "Régis Décamps"
 
-import os, imp
-import sys
+import os, imp, sys
+import locale, gettext
 
 '''
 Arelle plugin framework is heavily inspired by Marty Alchin's Simple plugin framework.
 http://martyalchin.com/2008/jan/10/simple-plugin-framework/
 '''
 PLUGIN_DIRECTORY = "plugins"
-
+_ = lambda x:x #TODO: load arelle gettext
+def l10n(modulepath, domain):
+    ''' This utility function returns the gettext.gettext method for the default locale
+    when it is available. Otherwise it returns the identity function.
+    A plugin should use it this way:
+    _ = apf.gettext()
+    '''
+    if isinstance(modulepath, list):
+        modulepath = modulepath[0]
+    try:
+        localedir = modulepath + os.sep + 'locale'
+        languages = locale.getdefaultlocale()
+        t = gettext.translation(domain, localedir, languages)
+        # define a short alias
+        return t.gettext
+    except:
+        print(sys.exc_info())
+        return lambda x: x
+    
 class MountPoint(type):
     '''
     * A way to declare a mount point for plugins. Since plugins are an example of loose coupling, there needs to be a neutral location, somewhere between the plugins and the code that uses them, that each side of the system can look at, without having to know the details of the other side. Trac calls this is an “extension point”.
@@ -41,10 +59,12 @@ def load_plugins():
         try :
             file, path, description = imp.find_module(dir, [PLUGIN_DIRECTORY])
             module = imp.load_module(dir, file, path, description)
-            print("Plugin {} v{} by {} loaded".format(dir, module.__version__, module.__author__))
+            print(_("Plugin %(plugin)s v%(version)s by %(author)s loaded") % \
+                    {'plugin':dir, 'version':module.__version__, 'author':module.__author__})
         except :
             # non modules will fail
-            print(sys.exc_info()[1])
+            print(_("Plugin %(plugin) failed to load: %(reason)") % \
+                    { 'plugin':dir, 'reason':sys.exc_info()[1]})
             pass
 class ExtensionsAt(object):
     ''' Descriptor to get plugins on a given mount point.
