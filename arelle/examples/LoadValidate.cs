60891330a4fae341e6de9de8569f2e2001faa1e7
Herman Fischer <fischer@markv.com>
2012-02-04 11:38:29 -0800

    add Java and C# examples of validating in Arelle with web server

---
+++ arelle/examples/LoadValidate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace ArelleCsharpProject
+{
+    class LoadValidate
+    {
+        static void Main(string[] args)
+        {
+		    string restAPIstr =
+			    "http://localhost:8080/rest/xbrl/" + 
+			    "C:/Users/John%20Doe/Samples/instance0010000.xbrl" +
+			    "/validation/xbrl?media=text";
+
+            HttpWebRequest req = WebRequest.Create(restAPIstr) as HttpWebRequest;
+
+            string line;
+            using (HttpWebResponse resp = req.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(resp.GetResponseStream());
+                while ((line = reader.ReadLine()) != null)
+                    Debug.WriteLine(line);
+            }
+        }
+    }
+}
