5f0b5ce8df0b3e3aef9ad716154ceb33bdf3f0e0
Herman Fischer <fischer@markv.com>
2012-02-29 17:15:26 -0800

    check for pre-existing linux build before preceeding

--- buildLinuxDist.sh
+++ buildLinuxDist.sh
@@ -7,6 +7,12 @@ python3.2 buildVersion.py
 
 BUILT64=exe.linux-x86_64-3.2
 
+if [ -d build/${BUILT64} ]
+  then
+    rm -R build/${BUILT64}
+fi
+mkdir build/${BUILT64}
+
 # run cx_Freeze setup
 python3.2 setup.py build_exe
 cp arelle/scripts-unix/* build/${BUILT64}
