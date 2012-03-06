e6f41f4fd1b6e5b13ab0a9de03211385e097bd51
Herman Fischer <fischer@markv.com>
2012-03-06 14:25:43 -0800

    changed copyright symbol to '(c)'

--- arelle/xlrd/__init__.py
+++ arelle/xlrd/__init__.py
@@ -1,4 +1,4 @@
-# Copyright © 2005-2009 Stephen John Machin, Lingfo Pty Ltd
+# Copyright (c) 2005-2009 Stephen John Machin, Lingfo Pty Ltd
 # This module is part of the xlrd3 package, which is released under a
 # BSD-style licence.
 #
