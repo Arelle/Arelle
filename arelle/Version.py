9346ccbe8cfabb48f89ef7d58fb7797f633a565f
Herman Fischer <fischer@markv.com>
2012-03-10 14:04:26 -0800

    build of 2012-03-10

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2012 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2012-02-28 16:49 UTC'
+version = '2012-03-10 21:03 UTC'
