2a78187038dd4b7b5d0ff2f908722030fd66fe2d
Herman Fischer <fischer@markv.com>
2012-06-14 10:18:00 -0700

    new plugin crashTest: used to test how uncaught exceptions are handled (for graceful treatment of users)

---
+++ arelle/examples/plugin/crashTest.py
@@ -0,0 +1,36 @@
+'''
+Crash test is a plug in to cause an uncaught exception to test its recover
+
+(c) Copyright 2012 Mark V Systems Limited, All rights reserved.
+'''
+def crashMenuEntender(cntlr, menu):
+    menu.add_command(label="Crash now!!!", underline=0, command=lambda: crashMenuCommand(cntlr) )
+
+def crashMenuCommand(cntlr):
+    foo = 25
+    foo /= 0
+
+def crashCommandLineOptionExtender(parser):
+    parser.add_option("--crash-test", 
+                      action="store_true", 
+                      dest="crashTest", 
+                      help=_('Test what happens with an exception'))
+
+def crashCommandLineXbrlRun(cntlr, options, modelXbrl):
+    if options.crashTest:
+        foo = 25
+        foo /= 0
+
+
+__pluginInfo__ = {
+    'name': 'Crash Test',
+    'version': '0.9',
+    'description': "Used to test that uncaught exceptions report their cause to the Arelle user.",
+    'license': 'Apache-2',
+    'author': 'Mark V Systems Limited',
+    'copyright': '(c) Copyright 2012 Mark V Systems Limited, All rights reserved.',
+    # classes of mount points (required)
+    'CntlrWinMain.Menu.Tools': crashMenuEntender,
+    'CntlrCmdLine.Options': crashCommandLineOptionExtender,
+    'CntlrCmdLine.Xbrl.Run': crashCommandLineXbrlRun,
+}
