810b3ba25d078147e07e16364bfc73f9daf6c6b6
Herman Fischer <fischer@markv.com>
2012-06-29 06:58:21 -0700

    incorporate further staff recommendations

--- arelle/examples/plugin/validateSecTagging.py
+++ arelle/examples/plugin/validateSecTagging.py
@@ -7,83 +7,80 @@ def setup(val):
     val.twoWayPriItemDefLabelPattern = re.compile('|'.join([
             # from http://www.sec.gov/spotlight/xbrl/staff-review-observations-061511.shtml 
             # Cash Flow
-            r"Increase decrease",
-            r"Provided by used in",
-            r"Net",
-            r"Change in",
-            r"Proceeds from payments for",
-            r"Proceeds from payments to",
+            r"increase decrease",
+            r"provided by used in",
+            r"net",
+            r"change in",
+            r"proceeds from payments (for|to)",
             # Income statement
-            r"Gain Loss",
-            r"Profit Loss",
-            r"Income expense",
-            r"Per share",
+            r"(gain|profit) loss",
+            r"income (expense|loss)",
+            r"per share",
             # Statement of Stockholders Equity
-            r"Equity",
-            r"Retained Earnings",
+            r"equity",
+            r"retained earnings",
+            r"conversion of units",
+            ]).replace(r" ",r"\W+"))
+    val.possibleTwoWayPriItemDefLabelPattern = re.compile('|'.join([
+            # from http://www.sec.gov/spotlight/xbrl/staff-review-observations-061511.shtml 
+            # Cash Flow
+            r"increase (\w+ )+decrease",
+            r"provided by (\w+ )+used in",
+            r"proceeds from (\w+ )+payments (for|to)",
             ]).replace(r" ",r"\W+"))
     val.twoWayPriItemStdLabelPattern = re.compile('|'.join([
             # from Eric Cohen
-            r"Appreciation Depreciation",
-            r"Asset Liability",
-            r"Assets Acquired Liabilities Assumed",
-            r"Benefit Expense",
-            r"Expense Benefit",
-            r"Cost Credit",
-            r"Costs Credits",
-            r"Deductions Charges",
-            r"Discount Premium",
-            r"Due from to",
-            r"Earnings Losses",
-            r"Earnings Deficit",
-            r"Excess Shortage",
-            r"Gains Losses",
-            r"Impairment Recovery",
-            r"Income Loss",
-            r"Liability Refund",
-            r"Loss Recovery",
-            r"Obligation Asset",
-            r"Obligations Assets",
-            r"Proceeds from Repayments of",
-            r"Proceeds from Repurchase of",
-            r"Provided by Used in",
-            r"Provisions Recoveries",
-            r"Retained Earnings Accumulated Deficit",
+            r"appreciation depreciation",
+            r"asset liability",
+            r"assets acquired liabilities assumed",
+            r"benefit expense",
+            r"expense benefit",
+            r"cost credit",
+            r"costs credits",
+            r"deductions charges",
+            r"discount premium",
+            r"due from to",
+            r"earnings losses",
+            r"earnings deficit",
+            r"excess shortage",
+            r"gains losses",
+            r"impairment recovery",
+            r"income loss",
+            r"liability refund",
+            r"loss recovery",
+            r"obligation asset",
+            r"obligations assets",
+            r"proceeds from repayments of",
+            r"proceeds from repurchase of",
+            r"provided by used in",
+            r"provisions recoveries",
+            r"retained earnings accumulated deficit",
             r"\w+ per \w+",
+            ]).replace(r" ",r"\W+"))
+    val.oneWayPriItemStdLabelPattern = re.compile('|'.join([
+            r"payments of (\w+ )*(dividends|capital)",
+            r"(stocks|shares) (\w+ )*issued",
+            r"stock (\w+ )*repurchase(d)?",
+            r"treasury stock (\w+ )*(beginning|ending|acquired|reissued|retired)",
+            r"accumulated depreciation (\w+ )*amortization",
+            r"accumulated other-than-temporary impairments",
+            r"allowance (\w+ )*doubtful accounts",
+            r"amortization (\w+ )*pension costs",
+            r"available for sale securities (\w+ )*continuous loss position",
+            r"available for sale securities gross unrealized losses",
             ]).replace(r" ",r"\W+"))
     val.twoWayMemberStdLabelPattern = re.compile('|'.join([
             # per Eric Cohen
-            r"Change in \w+",
-            r"\w+ Elimination \w+",
-            r"Adjustments for \w+",
-            r"Cumulative-Effect Adjustment \w+",
-            r"Effect of Illegal Acts in \w+",
-            r"Gain Loss \w+",
-            r"Gains Losses \w+",
-            r"Income Loss \w+",
-            r"Netting",
-            r"Adjustment", # added when removing below val.twoWayMbrQnamePattern per EC 2012-06-28 
+            r"change (in|during) \w+",
+            r"\w+ elimination \w+",
+            r"adjustment",
+            r"adjustments for \w+",
+            r"effect\s",
+            r"gain loss \w+",
+            r"gains losses \w+",
+            r"income loss \w+",
+            r"net(ting)?",
             ]).replace(r" ",r"\W+"))
-    ''' believe below was mis-understanding per ED 2012-06-28
-    val.twoWayMbrQnamePattern = re.compile('|'.join([
-            # per Eric Cohen
-            r"us-gaap:AccumulatedDefinedBenefitPlansAdjustmentMember",
-            r"us-gaap:AccumulatedTranslationAdjustmentMember",
-            r"us-gaap:AdjustmentsForNewAccountingPrincipleEarlyAdoptionMember",
-            r"us-gaap:CumulativeEffectAdjustmentConsolidationOfVariableInterestEntityMember",
-            r"us-gaap:CumulativeEffectAdjustmentDeconsolidationOfVariableInterestEntityMember",
-            r"us-gaap:FairValueAdjustmentToInventoryMember",
-            r"us-gaap:FairValueAdjustmentsOnHedgesAndDerivativeContractsMember",
-            r"us-gaap:LeasesAcquiredInPlaceMarketAdjustmentMember",
-            r"us-gaap:MaterialEffectOnLiabilityForUnpaidClaimsAndClaimsAdjustmentExpenseFromChangeInAccountingPrincipleMember",
-            r"us-gaap:MaterialErrorInLiabilityForUnpaidClaimsAndClaimsAdjustmentExpenseMember",
-            r"us-gaap:MinorRestatementOfOpeningLiabilityForUnpaidClaimsAndClaimsAdjustmentExpenseSAB108Member",
-            r"us-gaap:PurchasePriceAllocationAdjustmentsMember",
-            r"us-gaap:RestatementAdjustmentMember",
-            r"us-gaap:ScenarioAdjustmentMember",
-            r"us-gaap:YearEndAdjustmentMember",
-            ]).replace(r" ",r"\W+"))
-    '''
 
 def factCheck(val, fact):
     qname = str(fact.qname)
@@ -92,17 +89,19 @@ def factCheck(val, fact):
     stdLabel = concept.label(lang="en", fallbackToQname=False)
     defLabel = concept.label(preferredLabel=XbrlConst.documentationLabel, lang="en-US", fallbackToQname=False)
     
-    if not ((defLabel is not None and val.twoWayPriItemDefLabelPattern.search(defLabel, re.IGNORECASE)) or
-            (stdLabel is not None and val.twoWayPriItemStdLabelPattern.search(stdLabel, re.IGNORECASE)) or
-            context is not None and (
-                # only check non-default members (for now)
-                any((val.twoWayPriItemStdLabelPattern.search(dim.member.label(lang="en-US", fallbackToQname=False), re.IGNORECASE)
+    if fact.isNumeric and fact.xValue < 0 and (
+        (not ((defLabel is not None and val.twoWayPriItemDefLabelPattern.search(defLabel, re.IGNORECASE)) or
+              (stdLabel is not None and val.twoWayPriItemStdLabelPattern.search(stdLabel, re.IGNORECASE)) or
+               context is not None and (
+                  any((val.twoWayPriItemStdLabelPattern.search(dim.member.label(lang="en-US", fallbackToQname=False), re.IGNORECASE)
                      # remove per EC 2012-06-28: or val.twoWayMbrQnamePattern.match(str(dim.memberQname))
-                     )
-                    for dim in context.qnameDims.values()
-                    if dim.isExplicit)
-                         )
-            ) and fact.isNumeric and fact.xValue < 0:
+                      )
+                      for dim in context.qnameDims.values()
+                      if dim.isExplicit)
+                                       )
+              )) or
+        ((stdLabel is not None and val.oneWayPriItemStdLabelPattern.search(stdLabel, re.IGNORECASE)))
+            ):
             val.modelXbrl.warning("secStaffObservation.nonNegativeFact",
                 _("%(fact)s in context %(contextID)s unit %(unitID)s should be nonnegative"),
                 modelObject=fact, fact=fact.qname, contextID=fact.contextID, unitID=fact.unitID)
