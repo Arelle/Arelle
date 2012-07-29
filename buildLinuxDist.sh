26c15f45a4fbbf594a938a7ce3eccdbd6a95d583
Herman Fischer <fischer@markv.com>
2012-07-29 08:07:45 -0700

    additional comments

--- buildLinuxDist.sh
+++ buildLinuxDist.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # this file must have unix newlines (to prevent extraneous errors when running)
+# must run sudo on ubuntu
 
 # create version with date and a shell file to name output with the date
 python3.2 buildVersion.py
@@ -22,3 +23,4 @@ tar -c -gzip -f ../../dist/${BUILT64}.tar.gz .
 cd ../..
 
 /bin/sh buildRenameLinux-x86_64.sh
+# rm -R build2
