aa1c8923d458c17303c0bf40607b6fae1f798b90
Herman Fischer <fischer@markv.com>
2012-08-15 14:07:53 -0700

    ARELLE-131: add us-gaap deprecated concepts warning

--- arelle/examples/plugin/validateUSBestPractices.py
+++ arelle/examples/plugin/validateUSBestPractices.py
@@ -1,11 +1,46 @@
 from arelle import PluginManager
 from arelle.ModelValue import qname
-from arelle import Locale, XbrlConst
-import re
+from arelle import Locale, ModelXbrl, XbrlConst
+import os, io, re, json
 
 def setup(val):
     val.linroleDefinitionIsDisclosure = re.compile(r"-\s+Disclosure\s+-\s",
                                                    re.IGNORECASE)
+    
+    # load deprecated concepts for 2012 us-gaap
+    if "http://fasb.org/us-gaap/2012-01-31" in val.modelXbrl.namespaceDocs:
+        usgaapDoc = val.modelXbrl.namespaceDocs["http://fasb.org/us-gaap/2012-01-31"][0]
+        deprecationsJsonFile = usgaapDoc.filepathdir + os.sep + "deprecated-concepts.json"
+        try:
+            with io.open(deprecationsJsonFile, 'rt', encoding='utf-8') as f:
+                val.usgaapDeprecations = json.load(f)
+        except Exception:
+            val.modelXbrl.modelManager.addToLog(_("loading us-gaap deprecated concepts in cache"))
+            val.usgaapDeprecations = {}
+            # load without SEC/EFM validation (doc file would not be acceptable)
+            priorValidateDisclosureSystem = val.modelXbrl.modelManager.validateDisclosureSystem
+            val.modelXbrl.modelManager.validateDisclosureSystem = False
+            deprecationsInstance = ModelXbrl.load(val.modelXbrl.modelManager, 
+                  "http://xbrl.fasb.org/us-gaap/2012/elts/us-gaap-doc-2012-01-31.xml", 
+                  _("built deprecations table in cache"))
+            val.modelXbrl.modelManager.validateDisclosureSystem = priorValidateDisclosureSystem
+            if deprecationsInstance is None:
+                val.modelXbrl.error("arelle:notLoaded",
+                    _("US-GAAP documentation not loaded: %(file)s"),
+                    modelXbrl=val, file="us-gaap-doc-2012-01-31.xml")
+            else:   # load deprecations
+                for labelRel in deprecationsInstance.relationshipSet(XbrlConst.conceptLabel).modelRelationships:
+                    modelDocumentation = labelRel.toModelObject
+                    conceptName = labelRel.fromModelObject.name
+                    if modelDocumentation.role == 'http://www.xbrl.org/2009/role/deprecatedLabel':
+                        val.usgaapDeprecations[conceptName] = (val.usgaapDeprecations.get(conceptName, ('',''))[0], modelDocumentation.text)
+                    elif modelDocumentation.role == 'http://www.xbrl.org/2009/role/deprecatedDateLabel':
+                        val.usgaapDeprecations[conceptName] = (modelDocumentation.text, val.usgaapDeprecations.get(conceptName, ('',''))[1])
+            with io.open(deprecationsJsonFile, 'wt', encoding='utf-8') as f:
+                jsonStr = _STR_UNICODE(json.dumps(val.usgaapDeprecations, ensure_ascii=False, indent=0)) # might not be unicode in 2.7
+                f.write(jsonStr)  # 2.7 gets unicode this way
+            deprecationsInstance.close()
+            del deprecationsInstance # dereference closed modelXbrl
 
 def factCheck(val, fact):
     concept = fact.concept
@@ -36,6 +71,20 @@ def factCheck(val, fact):
                         _("Disclosure %(fact)s in context %(contextID)s value %(value)s is a fraction"),
                         modelObject=fact, fact=fact.qname, contextID=fact.contextID, value=fact.value)
                     
+        # deprecated concept
+        if concept.qname.namespaceURI == "http://fasb.org/us-gaap/2012-01-31":
+            if concept.name in val.usgaapDeprecations:
+                deprecation = val.usgaapDeprecations[concept.name]
+                val.modelXbrl.warning("FASB:deprecatedConcept",
+                    _("Concept of fact %(fact)s in context %(contextID)s value %(value)s was deprecated on %(date)s: %(documentation)s"),
+                    modelObject=fact, fact=fact.qname, contextID=fact.contextID, value=fact.value, 
+                    date=deprecation[0], documentation=deprecation[1])
+        elif concept.get("{http://fasb.org/us-gaap/attributes}deprecatedDate"):
+            val.modelXbrl.warning("FASB:deprecatedConcept",
+                _("Concept of fact %(fact)s in context %(contextID)s value %(value)s was deprecated on %(date)s"),
+                modelObject=fact, fact=fact.qname, contextID=fact.contextID, value=fact.value, 
+                date=concept.get("{http://fasb.org/us-gaap/attributes}deprecatedDate"))
+                    
     except Exception as err:
         val.modelXbrl.warning("US-BPG.testingException",
             _("%(fact)s in context %(contextID)s unit %(unitID)s value %(value)s cannot be tested due to: %(err)s"),
@@ -44,6 +93,8 @@ def factCheck(val, fact):
 
 def final(val):
     del val.linroleDefinitionIsDisclosure
+    if hasattr(val, 'usgaapDeprecations'):
+        del val.usgaapDeprecations
     
 __pluginInfo__ = {
     # Do not use _( ) in pluginInfo itself (it is applied later, after loading
