95991e2165ab6d4df387a7b6b78c91c76582d38e
Régis Décamps <decamps@users.sf.net>
2012-11-24 15:27:44 +0100

    Start v0.2

--- plugins/analytics/tracker.py
+++ plugins/analytics/tracker.py
@@ -30,7 +30,7 @@ __author__ = 'R\u00e9gis D\u00e9camps'
 __copyright__ = "Copyright 2012, Autorit\u00e9 de contr\u00f4le prudentiel"
 __credits__ = []
 __license__ = "Apache-2"
-__version__ = "0.1"
+__version__ = "0.2"
 __email__ = "regis.decamps@banque-france.fr"
 __status__ = "Development"
 
@@ -111,7 +111,7 @@ def ga_function_decorated(ga, func):
 # Manigest for this plugin
 __pluginInfo__ = {
     'name': 'ga',
-    'version': '0.1',
+    'version': '0.2',
     'description': '''Google analytics collects anonymous usage statistics, so that Arelle can be improved on features that are most frequently used'''
     ,
     'localeURL': "locale",
