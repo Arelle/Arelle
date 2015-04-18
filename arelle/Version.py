5a47f7c4b235d5696fb91137248058923ffc9ec4
Herm Fischer <hermf@Herm-MacBook.attlocal.net>
2015-04-18 15:43:10 -0700

    pull request 45, Ascone

--- arelle/Version.py
+++ arelle/Version.py
@@ -2,7 +2,7 @@
 This module represents the time stamp when Arelle was last built
 
 @author: Mark V Systems Limited
-(c) Copyright 2014 Mark V Systems Limited, All rights reserved.
+(c) Copyright 2015 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2014-10-07 17:36 UTC'
+version = '2015-03-23 18:13 UTC'
