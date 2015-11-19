fce7982df32d07ae5361277f27dba2d1eed01b3c
Herman Fischer <fischer@markv.com>
2015-11-18 16:50:00 -0800

    update version (usually generated by building setup scripts)

--- arelle/Version.py
+++ arelle/Version.py
@@ -5,4 +5,4 @@ This module represents the time stamp when Arelle was last built
 (c) Copyright 2015 Mark V Systems Limited, All rights reserved.
 
 '''
-version = '2015-08-06 18:17 UTC'
+version = '2015-11-16 07:17 UTC'
