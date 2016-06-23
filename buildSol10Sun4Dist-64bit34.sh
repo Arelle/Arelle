1d85675b7879e357ecf995e02038853e39c4c65e
Herman Fischer <fischer@markv.com>
2016-06-22 22:35:18 -0700

    update Sparc build to clean git stuff and prior pyc files

--- buildSol10Sun4Dist-64bit34.sh
+++ buildSol10Sun4Dist-64bit34.sh
@@ -8,6 +8,9 @@ PYTHON=python3.4
 # create version with date and a shell file to name output with the date
 ${PYTHON} buildVersion.py
 
+# clean all previously compiled pyc files
+find arelle -name '*.cpython-3?.pyc' -exec rm {} \;
+
 BUILT64=exe.solaris-2.10-sun4v.64bit-3.4
 
 if [ -d build/${BUILT64} ]
@@ -24,6 +27,9 @@ fi
 # run cx_Freeze setup
 ${PYTHON} setup.py build
 cp arelle/scripts-unix/* build/${BUILT64}
+
+# remove .git subdirectories
+find $BUILT64 -name .git -exec rm -fR {} \;
 
 cd build/${BUILT64}
 
@@ -50,8 +56,13 @@ cd ../..
 # add arelle into SEC XBRL.JAR
 cd build
 mv ${BUILT64} arelle
-cp /export/home/slee/Documents/vm-files/XBRL.JAR ../dist
-jar uf ../dist/XBRL.JAR arelle
+rm -fr arelle/examples
+cp /export/home/edgar/xbrl.jar ../dist
+echo updating xbrl.jar
+jar tvf ../dist/xbrl.jar | grep library.zip
+jar uf ../dist/xbrl.jar arelle
+jar tvf ../dist/xbrl.jar | grep library.zip
+jar uf ../dist/xbrl.jar arelle
 mv arelle ${BUILT64}
 cd ..
 
