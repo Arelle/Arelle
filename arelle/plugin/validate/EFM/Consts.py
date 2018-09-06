f9e68eea506f3613efc25aec5b997e85b98ff58b
Herman Fischer <fischer@markv.com>
2018-09-06 00:00:36 -0700

    plugin validate/EFM: update to preview EDGAR release 18.3

--- arelle/plugin/validate/EFM/Consts.py
+++ arelle/plugin/validate/EFM/Consts.py
@@ -4,6 +4,8 @@ Created on Jun 30, 2018
 @author: Mark V Systems Limited
 (c) Copyright 2010 Mark V Systems Limited, All rights reserved.
 '''
+import re
+
 #qnFasbExtensibleListItemTypes = (qname("{http://fasb.org/us-types/2017-01-31}us-types:extensibleListItemType"),
 #                                 qname("{http://fasb.org/srt-types/2018-01-31}srt-types:extensibleListItemType"))
 
@@ -21,7 +23,7 @@ srtAxisIfrsMembers = { # members of IFRS axes which have SRT corresponding membe
     "RangeAxis": {"RangesMember", "BottomOfRangeMember", "WeightedAverageMember", "TopOfRangeMember"}    }
 """
 
-EdgarDocumentTypes = {
+edgarDocumentTypes = {
 	"10-12B",
     "10-12B/A",
     "10-12G",
@@ -118,7 +120,7 @@ EdgarDocumentTypes = {
     "SP 15D2/A"
   }
   
-EdgarSubmissionTypeAllowedDocumentTypes = { 
+edgarSubmissionTypeAllowedDocumentTypes = { 
 	"10-12B": ("10-12B", "Other"),
 	"10-12B/A": ("10-12B/A", "Other"),
 	"10-12G": ("10-12G", "Other"),
@@ -214,3 +216,135 @@ EdgarSubmissionTypeAllowedDocumentTypes = {
 	"SDR-A": ("K SDR", "L SDR"),
 	"SDR/W ": ("K SDR", "L SDR")
  }
+
+submissionTypesAllowingEmergingGrowthCompanyFlag = \
+submissionTypesAllowingExTransitionPeriodFlag = {"10-12B", "10-12B/A", "10-12G", "10-12G/A", "10-K", "10-KT", "10-K/A", "10-KT/A", "10-Q", "10-Q/A", "10-QT", "10-QT/A", 
+                 "20-F", "20-F/A", "40-F", "40-F/A", "8-K", "8-K/A", "F-1", "F-1/A", "F-3", "F-3/A", "F-4", "F-4/A", 
+                 "S-1", "S-1/A", "S-3", "S-3/A", "S-4", "S-4/A"}
+submissionTypesAllowingPeriodOfReport={
+    "N-CSR", "N-CSR/A", "N-CSRS", "N-CSRS/A", "N-Q", "10-Q", "10-Q/A", "10-QT", "10-QT/A", "10-K", "10-K/A", "10-KT", "10-KT/A", "20-F", "20-F/A", "40-F", "40-F/A",
+    "8-K", "8-K/A", "6-K", "6-K/A", "8-K12B", "8-K12B/A", "8-K12G3", "8-K12G3/A", "8-K15D5", "8-K15D5/A", "N-Q/A", "SP 15D2", "SP 15D2/A", 
+    }
+submissionTypesAllowingWellKnownSeasonedIssuer = \
+submissionTypesAllowingShellCompanyFlag = \
+submissionTypesAllowingVoluntaryFilerFlag = \
+submissionTypesAllowingAcceleratedFilerStatus = {"10-K", "10-K/A", "10-KT", "10-KT/A", "20-F", "20-F/A"}
+submissionTypesAllowingEdgarSmallBusinessFlag = {"10-K", "10-K/A", "10-KT", "10-KT/A", "10-Q", "10-Q/A", "10-QT", "10-QT/A", "S-1", "S-1/A", "S-3", "S-3/A", "S-4", 
+                                                 "S-4/A", "S-11", "S-11/A", "10-12B", "10-12B/A", "10-12G", "10-12G/A", "S-11MEF", "S-1MEF", "S-3D", "S-3DPOS", "S-3MEF", 
+                                                 "S-4 POS", "S-4EF", "S-4MEF"}
+submissionTypesAllowingEntityInvCompanyType = {"485BPOS", "497"}
+submissionTypesAllowingSeriesClasses = {"485BPOS", "497", "N-CSR", "N-CSR/A", "N-CSRS", "N-CSRS/A"}
+# doc type requirements are for EFM 6.5.20 and are in some cases a superset of what the submission allows.
+docTypesRequiringPeriodOfReport = {"10", "10-K", "10-Q", "20-F", "40-F", "6-K", "8-K", 
+    "F-1", "F-10", "F-3", "F-4", "F-9", "S-1", "S-11", "S-3", "S-4", "POS AM", "10-KT", "10-QT", "POS EX", 
+    "10/A", "10-K/A", "10-Q/A", "20-F/A", "40-F/A", "6-K/A", "8-K/A", "F-1/A", "F-10/A", "F-3/A", "F-4/A", 
+    "F-9/A", "S-1/A", "S-11/A", "S-3/A", "S-4/A", "10-KT/A", "10-QT/A", "485BPOS", "497", 
+    "N-CSR", "N-CSRS", "N-Q", "N-CSR/A", "N-CSRS/A", "N-Q/A", "K SDR", "L SDR" }
+docTypesRequiringEntityWellKnownSeasonedIssuer = {"10-K", "10-K/A", "10-KT", "10-KT/A", "20-F", "20-F/A"}
+docTypesRequiringEntityVolFilersAndPubFloat = {"10-K", "10-KT", "10-K/A", "10-KT/A" }
+docTypesRequiringEntityFilerCategory = {"10-K", "10-K/A", "10-KT", "10-KT/A", "20-F", "20-F/A", "10-Q", "10-Q/A", "10-QT", "10-QT/A", "S-1", "S-1/A", "S-3", "S-3/A",
+                                         "S-4", "S-4/A", "S-11", "S-11/A", "S-11MEF", "S-1MEF", "S-3D", "S-3DPOS", "S-3MEF", "S-4 POS", "S-4EF", "S-4MEF", "POS AM", "S-3ASR"}
+
+docTypeDeiItems = ( # ({set of doc types}, (list of dei Names required - list is for repeatability)), ...
+      ({"10-K", "10-KT", "10-Q", "10-QT", "20-F", "40-F",
+        "10-K/A", "10-KT/A", "10-Q/A", "10-QT/A", "20-F/A", "40-F/A",
+        "6-K", "NCSR", "N-CSR", "N-CSRS", "N-Q",
+        "6-K/A", "NCSR/A", "N-CSR/A", "N-CSRS/A", "N-Q/A",
+        "10", "S-1", "S-3", "S-4", "S-11", "POS AM",
+        "10/A", "S-1/A", "S-3/A", "S-4/A", "S-11/A", 
+        "8-K", "F-1", "F-3", "F-10", "497", "485BPOS",
+        "8-K/A", "F-1/A", "F-3/A", "F-10/A", "K SDR", "L SDR",
+        "Other"},
+        ("EntityRegistrantName", "EntityCentralIndexKey")),
+      ({"10-K", "10-KT", "20-F", "40-F",
+        "10-K/A", "10-KT/A", "20-F/A", "40-F/A"},
+       ("EntityCurrentReportingStatus",)),
+     (docTypesRequiringEntityVolFilersAndPubFloat,
+      ("EntityVoluntaryFilers", "EntityPublicFloat")),
+      ({"10-K", "10-KT", "10-Q", "10-QT", "20-F", "40-F",
+        "10-K/A", "10-KT/A", "10-Q/A", "10-QT/A", "20-F/A", "40-F/A",
+        "6-K", "NCSR", "N-CSR", "N-CSRS", "N-Q",
+        "6-K/A", "NCSR/A", "N-CSR/A", "N-CSRS/A", "N-Q/A", "K SDR", "L SDR"},
+        ("CurrentFiscalYearEndDate", "DocumentFiscalYearFocus", "DocumentFiscalPeriodFocus")),
+      (docTypesRequiringEntityFilerCategory,
+        ("EntityFilerCategory",)),
+       (docTypesRequiringEntityWellKnownSeasonedIssuer,
+         ("EntityWellKnownSeasonedIssuer",)),
+       ({"SD", "SD/A"},
+         ("EntityReportingCurrencyISOCode", ))
+    )
+
+docTypesExemptFromRoleOrder = \
+submissionTypesExemptFromRoleOrder = ('485BPOS','497')
+
+standardNamespacesPattern = re.compile(
+    # non-IFRS groups 1 - authority, 2 - taxonomy (e.g. us-gaap, us-types), 3 - year
+    r"http://(xbrl\.us|fasb\.org|xbrl\.sec\.gov)/("
+            r"dei|us-gaap|srt|us-types|us-roles|srt-types|srt-roles|rr|country|currency|exch|invest|naics|sic|stpr"
+            r")/([0-9]{4})-[0-9]{2}-[0-9]{2}$"
+    # ifrs groups 4 - year, 5 - taxonomy (e.g. ifrs-full)
+    r"|http://xbrl.ifrs.org/taxonomy/([0-9]{4})-[0-9]{2}-[0-9]{2}/(ifrs[\w-]*)$")
+
+usDeprecatedLabelPattern = re.compile(r"^.* \(Deprecated (....-..-..)\)$")
+usDeprecatedLabelRole = "http://www.xbrl.org/2003/role/label"
+ifrsDeprecatedLabelPattern = re.compile(r"^\s*([0-9]{4}-[0-1][0-9]-[0-2][0-9])\s*$")
+ifrsDeprecatedLabelRole = "http://www.xbrl.org/2009/role/deprecatedDateLabel"
+
+latestTaxonomyDocs = { # note that these URLs are blocked by EFM validation modes
+    # US FASB/SEC taxonomies
+    "country/*": {
+        "namespace": "http://xbrl.sec.gov/country/2017-01-31",
+        "deprecatedLabels": "https://xbrl.sec.gov/country/2017/country-lab-2017-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "currency/*": {
+        "namespace": "http://xbrl.sec.gov/currency/2017-01-31",
+        "deprecatedLabels": "https://xbrl.sec.gov/currency/2017/currency-lab-2017-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "dei/*": {
+        "namespace": "http://xbrl.sec.gov/dei/2018-01-31",
+        "deprecatedLabels": "https://xbrl.sec.gov/dei/2018/dei-lab-2018-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "exch/*": {
+        "namespace": "http://xbrl.sec.gov/exch/2018-01-31",
+        "deprecatedLabels": "https://xbrl.sec.gov/exch/2018/exch-lab-2018-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "rr/*": {
+        "namespace": "http://xbrl.sec.gov/rr/2018-01-31",
+        "deprecatedLabels": "https://xbrl.sec.gov/rr/2018/rr-lab-2018-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "srt/*": {
+        "namespace": "http://fasb.org/srt/2018-01-31",
+        "deprecatedLabels": "http://xbrl.fasb.org/srt/2018/elts/srt-lab-2018-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "stpr/*": {
+        "namespace": "http://xbrl.sec.gov/stpr/2018-01-31",
+        "deprecatedLabels": "https://xbrl.sec.gov/stpr/2018/stpr-lab-2018-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    "us-gaap/*": {
+        "namespace": "http://fasb.org/us-gaap/2018-01-31",
+        "deprecatedLabels": "http://xbrl.fasb.org/us-gaap/2018/elts/us-gaap-lab-2018-01-31.xml",
+        "deprecatedLabelRole": usDeprecatedLabelRole,
+        "deprecationDatePattern": usDeprecatedLabelPattern
+        },
+    # International taxonomies
+    "ifrs-full/*": {
+        "namespace": "http://xbrl.ifrs.org/taxonomy/2018-03-16/ifrs-full",
+        "deprecatedLabels": "http://xbrl.ifrs.org/taxonomy/2018-03-16/deprecated/depr-lab_full_ifrs-en_2018-03-16.xml",
+        "deprecatedLabelRole": ifrsDeprecatedLabelRole,
+        "deprecationDatePattern": ifrsDeprecatedLabelPattern
+        }
+    }
