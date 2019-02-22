fa3a18df9396c36de90b15cfeaf608f4bdda5eb7
Herman Fischer <fischer@markv.com>
2019-02-22 09:32:14 -0800

    Add command line operation to transforms/tester plugin

--- arelle/plugin/transforms/tester.py
+++ arelle/plugin/transforms/tester.py
@@ -4,19 +4,46 @@ Created on Feb 1, 2019
 @author: Mark V Systems Limited
 (c) Copyright 2019 Mark V Systems Limited, All rights reserved.
 
-This plugin allows GUI users to test transforms,
+This plugin allows GUI and command line users to test transforms.
 
 Custom extensions are also made available when their plugin has been loaded
 (e.g., for SEC custom transforms use plugin transforms/SEC, validate/EFM or EdgarRenderer)
 
-Errors are shown both in results field as well as reported in the message pane.
+Errors are shown both in results field as well as reported in the message pane (GUI) and reported in log (command line).
+
+For GUI operation tools -> transformation tester:
+
+    Select registry (ixt v1-4, ixt-sec)
+    Select or enter transform
+    Enter source text
+    Press “transform”
+    Result (or error code)
+
+For command line operation:
+
+    arelleCmdLine --plugins transforms/tester --testTransform 'registry name transformation name pattern' (space separatedd)
+    note: the transform name may be optionally prefixed
+    results or errors are in the log
+    
+    arelleCmdLine --plugins transforms/tester --testTransform 'ixt v3 datedaymonthen 29th February' or
+    arelleCmdLine --plugins transforms/tester --testTransform 'ixt v3 ixt:datedaymonthen 29th February'
+     
+    SEC transform example: 
+    
+    arelleCmdLine --plugins 'transforms/tester|transforms/SEC' --testTransform "ixt-sec durwordsen 23 days"
+    
+    Help instructions and list of available transforms:
+    
+    arelleCmdLine --plugins transforms/tester --testTransform 'help'  (or '?') or
+    arelleCmdLine --plugins 'transforms/tester|transforms/SEC' --testTransform 'help'
+
 '''
 from tkinter import Toplevel, StringVar, N, S, E, EW, W, PhotoImage
 try:
     from tkinter.ttk import Frame, Button, Entry
 except ImportError:
     from ttk import Frame, Button
-import os, re
+import os, re, logging
 from arelle.FunctionIxt import ixtNamespaces, ixtNamespaceFunctions
 from arelle.UiUtil import gridHdr, gridCell, gridCombobox, label, checkbox
 from arelle.CntlrWinTooltip import ToolTip
@@ -24,11 +51,67 @@ from arelle.ModelFormulaObject import Trace
 from arelle.XmlUtil import setXmlns
 from arelle import ModelDocument, ModelXbrl, ValidateXbrl, XbrlConst, XPathParser, XPathContext
 
+class TransformTester:
+    def __init__(self, cntlr, isCmdLine=False):
+        self.cntlr = cntlr
+
+        # setup tester
+        xml = "<rootElement/>"
+        self.modelXbrl = ModelXbrl.create(cntlr.modelManager, ModelDocument.Type.UnknownNonXML, initialXml=xml, isEntry=True)
+        self.validator = ValidateXbrl.ValidateXbrl(self.modelXbrl)
+        self.validator.validate(self.modelXbrl)  # required to set up
+        cntlr.showStatus(_("Initializing Formula Grammar"))
+        XPathParser.initializeParser(cntlr.modelManager)
+        cntlr.showStatus(None)
+
+        self.trRegs = sorted(ixtNamespaces.keys())
+        self.trPrefixNSs = dict((qn.prefix, qn.namespaceURI) 
+                                  for qn in self.modelXbrl.modelManager.customTransforms.keys())
+        self.trRegs.extend(sorted(self.trPrefixNSs.keys()))
+        self.trPrefixNSs.update(ixtNamespaces)
+
+    def getTrNames(self, trReg):
+        trNS = self.trPrefixNSs[trReg]
+        trPrefix = trReg.split()[0] # for ixt remove TRn part
+        setXmlns(self.modelXbrl.modelDocument, trPrefix, trNS)
+        if trNS in ixtNamespaceFunctions:
+            return sorted("{}:{}".format(trPrefix, key)
+                          for key in ixtNamespaceFunctions[trNS].keys())
+        # custom transforms
+        return sorted(str(trQn)
+                      for trQn in self.modelXbrl.modelManager.customTransforms.keys()
+                      if trQn.prefix == trPrefix)
+        
+    def transform(self, trReg, trName, sourceValue):
+        try:                            
+            trNS = self.trPrefixNSs[trReg]
+            trPrefix = trReg.split()[0] # for ixt remove TRn part
+            setXmlns(self.modelXbrl.modelDocument, trPrefix, trNS)
+            self.modelXbrl.modelManager.showStatus(_("Executing call"))
+            elt = self.modelXbrl.modelDocument.xmlRootElement
+            if ':' in trName:
+                prefixedFnName = trName
+            else:
+                prefixedFnName = "{}:{}".format(trPrefix, trName)
+            callExprStack = XPathParser.parse(self.validator, 
+                                              '{}("{}")'.format(prefixedFnName, sourceValue),
+                                              elt, trName + " call", Trace.CALL)
+            xpathContext = XPathContext.create(self.modelXbrl, sourceElement=elt)
+            result = xpathContext.evaluate(callExprStack)
+            while result and isinstance(result, (tuple,list,set)):
+                result = next(iter(result)) # de-sequence result
+            return result
+        except XPathContext.XPathException as err:
+            self.modelXbrl.error(err.code, err.message)
+            return err
+        
 class DialogTransformTester(Toplevel):
-    def __init__(self, mainWin):
-        parent = mainWin.parent
+    def __init__(self, tester):
+        self.tester = tester
+        self.mainWin = tester.cntlr
+
+        parent = self.mainWin.parent
         super(DialogTransformTester, self).__init__(parent)
-        self.mainWin = mainWin
         self.parent = parent
         parentGeometry = re.match("(\d+)x(\d+)[+]?([-]?\d+)[+]?([-]?\d+)", parent.geometry())
         dialogX = int(parentGeometry.group(3))
@@ -40,29 +123,13 @@ class DialogTransformTester(Toplevel):
         
         frame = Frame(self)
         
-        # setup tester
-        xml = "<rootElement/>"
-        self.modelXbrl = ModelXbrl.create(mainWin.modelManager, ModelDocument.Type.UnknownNonXML, initialXml=xml, isEntry=True)
-        self.validator = ValidateXbrl.ValidateXbrl(self.modelXbrl)
-        self.validator.validate(self.modelXbrl)  # required to set up
-        mainWin.showStatus(_("Initializing Formula Grammar"))
-        XPathParser.initializeParser(mainWin.modelManager)
-        mainWin.showStatus(None)
-
-        self.trRegs = sorted(ixtNamespaces.keys())
-        self.trReg = self.trRegs[-1] # default is latest
-        self.trPrefixNSs = dict((qn.prefix, qn.namespaceURI) 
-                                  for qn in self.modelXbrl.modelManager.customTransforms.keys())
-        self.trRegs.extend(sorted(self.trPrefixNSs.keys()))
-        self.trPrefixNSs.update(ixtNamespaces)
-        self.trNames = self.getTrNames()
-
         # load grid
         trRegLabel = label(frame, 0, 0, _("Registry:"))
+        trReg = self.tester.trRegs[-1] # default is latest
         self.trRegName = gridCombobox(frame, 1, 0, 
-                                      value=self.trReg,
-                                      values=self.trRegs, 
-                                      comboboxselected=self.trRegComboBoxSelected)
+                                      value=trReg,
+                                      values=self.tester.trRegs, 
+                                      comboboxselected=self.dialogTrRegComboBoxSelected)
         trRegToolTipMessage = _("Select Transformation Registry")
         ToolTip(self.trRegName, text=trRegToolTipMessage, wraplength=360)
         ToolTip(trRegLabel, text=trRegToolTipMessage, wraplength=360)
@@ -70,8 +137,8 @@ class DialogTransformTester(Toplevel):
         trNameLabel = label(frame, 0, 1, _("Transform:"))
         self.trNameName = gridCombobox(frame, 1, 1, 
                                       value="",
-                                      values=self.trNames, 
-                                      comboboxselected=self.trNameComboBoxSelected)
+                                      values=self.tester.getTrNames(trReg), 
+                                      comboboxselected=self.dialogTrNameComboBoxSelected)
         trRegToolTipMessage = _("Select or enter transform")
         ToolTip(self.trRegName, text=trRegToolTipMessage, wraplength=360)
         ToolTip(trRegLabel, text=trRegToolTipMessage, wraplength=360)
@@ -93,11 +160,11 @@ class DialogTransformTester(Toplevel):
         resultEntry.grid(row=3, column=1, sticky=EW, pady=3, padx=3)
 
         
-        mainWin.showStatus(None)
+        self.mainWin.showStatus(None)
 
-        btnPad = 2 if mainWin.isMSW else 0 # buttons too narrow on windows
-        okButton = Button(frame, text=_("Transform"), width=8 + btnPad, command=self.ok)
-        cancelButton = Button(frame, text=_("Done"), width=4 + btnPad, command=self.close)
+        btnPad = 2 if self.mainWin.isMSW else 0 # buttons too narrow on windows
+        okButton = Button(frame, text=_("Transform"), width=8 + btnPad, command=self.dialogOk)
+        cancelButton = Button(frame, text=_("Done"), width=4 + btnPad, command=self.dialogClose)
         cancelButton.grid(row=4, column=0, sticky=E, columnspan=2, pady=3, padx=3)
         okButton.grid(row=4, column=0, sticky=E, columnspan=2, pady=3, padx=64)
         ToolTip(okButton, text=_("Transform the source entered. "), wraplength=240)
@@ -113,62 +180,73 @@ class DialogTransformTester(Toplevel):
         #self.bind("<Return>", self.ok)
         #self.bind("<Escape>", self.close)
         
-        self.protocol("WM_DELETE_WINDOW", self.close)
+        self.protocol("WM_DELETE_WINDOW", self.dialogClose)
         self.grab_set()
         
         self.wait_window(self)
         
-    def ok(self, event=None):
-        trName = self.trNameName.value
-        sourceValue = self.sourceVar.get()
-        try:                            
-            self.modelXbrl.modelManager.showStatus(_("Executing call"))
-            elt =self.modelXbrl.modelDocument.xmlRootElement
-            callExprStack = XPathParser.parse(self.validator, 
-                                              '{}("{}")'.format(trName, sourceValue),
-                                              elt, trName + " call", Trace.CALL)
-            xpathContext = XPathContext.create(self.modelXbrl, sourceElement=elt)
-            result = xpathContext.evaluate(callExprStack)
-            while result and isinstance(result, (tuple,list,set)):
-                result = next(iter(result)) # de-sequence result
+    def dialogOk(self, event=None):
+        result = self.tester.transform(
+            self.trRegName.get(),
+            self.trNameName.value,
+            self.sourceVar.get())
+        if isinstance(result, XPathContext.XPathException):
             self.resultVar.set(str(result))
-        except XPathContext.XPathException as err:
-            self.resultVar.set(str(err))
-            self.modelXbrl.error(err.code, err.message)
-        
-    def close(self, event=None):
-        self.modelXbrl.close()
+        else:
+            self.resultVar.set(str(result))
+            
+    def dialogClose(self, event=None):
+        self.tester.modelXbrl.close()
         self.parent.focus_set()
         self.destroy()
 
-    def trRegComboBoxSelected(self, *args):
-        self.trReg = self.trRegName.get()
-        self.trNames = self.getTrNames()
-        self.trNameName["values"] = self.trNames
+    def dialogTrRegComboBoxSelected(self, *args):
+        self.trNameName["values"] = self.tester.getTrNames( self.trRegName.get() )
         
-    def trNameComboBoxSelected(self, *args):
+    def dialogTrNameComboBoxSelected(self, *args):
         pass
         
-    def getTrNames(self):
-        self.trNS = self.trPrefixNSs[self.trReg]
-        self.trPrefix = self.trReg.split()[0] # for ixt remove TRn part
-        setXmlns(self.modelXbrl.modelDocument, self.trPrefix, self.trNS)
-        if self.trNS in ixtNamespaceFunctions:
-            return sorted("{}:{}".format(self.trPrefix, key)
-                          for key in ixtNamespaceFunctions[self.trNS].keys())
-        # custom transforms
-        return sorted(str(trQn)
-                      for trQn in self.modelXbrl.modelManager.customTransforms.keys()
-                      if trQn.prefix == self.trPrefix)
-        
+ 
+def cmdLineOptionExtender(parser, *args, **kwargs):
+    parser.add_option("--testTransform", 
+                      action="store", 
+                      dest="testTransformCmdLineArg", 
+                      help=_("Test a transformation registry transform. "
+                             "Enter 'help' or '?' for a list of transformation registries available.  "
+                             "Enter registry name, space, transformation name, space and pattern.  "
+                             "E.g., 'ixt v3 datedaymonthen 29th February' or ixt v3 ixt:datedaymonthen 29th February'. "))
+    
+def cmdLineRun(cntlr, options, *args, **kwargs):
+    if options.testTransformCmdLineArg:
+        tester = TransformTester(cntlr)
+        arg = options.testTransformCmdLineArg 
+        argWord, _sep, rest = arg.partition(" ")
+        trReg = None
+        for _regName in tester.trPrefixNSs.keys():
+            if arg.startswith(_regName):
+                trReg = _regName
+                rest = arg[len(trReg)+1:]
+        if trReg is None or argWord in ("help", "?"):
+            cntlr.addToLog("Registries available: {}".format(", ".join(tester.trRegs)), 
+                           messageCode="tester:registries", level=logging.INFO)
+        else:
+            trName, _sep, sourceValue = rest.partition(" ")
+            print("reg {} name {} source {}".format(trReg, trName, sourceValue))
+            result = tester.transform(trReg, trName, sourceValue)
+            tester.modelXbrl.info("tester:transformation",
+                                  "%(registry)s %(transformName)s source '%(sourceValue)s' result '%(resultValue)s'",
+                                  registry=trReg, transformName=trName, sourceValue=sourceValue, resultValue=str(result))
+        tester.modelXbrl.close()
 
 def transformationTesterMenuExtender(cntlr, menu, *args, **kwargs):
-    def transformationTester():
-        DialogTransformTester(cntlr)
+    def guiTransformationTester():
+        tester = TransformTester(cntlr, isCmdLine=True)
+        DialogTransformTester( tester )
+        tester.modelXbrl.close()
 
     menu.add_command(label="Transformtion Tester", 
                      underline=0, 
-                     command=lambda: transformationTester() )
+                     command=lambda: guiTransformationTester() )
 
 
 __pluginInfo__ = {
@@ -180,6 +258,8 @@ __pluginInfo__ = {
     'author': 'Mark V Systems',
     'copyright': '(c) Copyright 2019 Mark V Systems Limited, All rights reserved.',
     # classes of mount points (required)
+    'CntlrCmdLine.Options': cmdLineOptionExtender,
+    'CntlrCmdLine.Utility.Run': cmdLineRun, 
     'CntlrWinMain.Menu.Tools': transformationTesterMenuExtender
 
 }
