0b9e7833c4d6ea5d9a301c66cb0ef8ec9e70e419
Herman Fischer <fischer@markv.com>
2019-12-14 08:03:26 -0600

    correct startup error message formatting for unrecognized arguments

--- arelle/CntlrCmdLine.py
+++ arelle/CntlrCmdLine.py
@@ -414,7 +414,7 @@ def parseAndRun(args):
         except UnicodeEncodeError:
             print(text.encode("ascii", "replace").decode("ascii"))
     elif len(leftoverArgs) != 0 and (not hasWebServer or options.webserver is None):
-        parser.error(_("unrecognized arguments: {}".format(', '.join(leftoverArgs))))
+        parser.error(_("unrecognized arguments: {}").format(', '.join(leftoverArgs)))
     elif (options.entrypointFile is None and 
           ((not options.proxy) and (not options.plugins) and
            (not any(pluginOption for pluginOption in parser.option_list[pluginOptionsIndex:pluginLastOptionIndex])) and
