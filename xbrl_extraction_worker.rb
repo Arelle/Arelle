200416b5c232af60e98966831489ab5d8a22a98a
Rainmaker <obioraakubue@yahoo.com>
2020-11-21 03:26:23 -0500

    clean up shoryuken args - fix extraction

--- xbrl_extraction_worker.rb
+++ xbrl_extraction_worker.rb
@@ -4,9 +4,15 @@ class XbrlExtractionWorker
 
   shoryuken_options queue: 'xbrl_extraction', auto_delete: true
 
-  def perform(sqs_msg)
-    extract_from_path = sqs_msg[:extract_from_path]
-    extract_to_path   = sqs_msg[:extract_to_path]
-    system("python3 /opt/arelle/arelleCmdLine.py -f #{extract_from_path} --concepts #{extract_to_path}")
+  def perform(sqs_msg, body)
+    Shoryuken.logger.info("[XbrlExtractionWorker] Received body #{body.inspect} from message #{sqs_msg.inspect}")
+
+    message_body      = JSON.parse(body, symbolize_names: true)
+    extract_from_path = message_body.fetch(:extract_from_path)
+    extract_to_path   = message_body.fetch(:extract_to_path)
+
+    system("python3 /opt/arelle/arelleCmdLine.py -f #{extract_from_path} \
+      --facts #{extract_to_path} \
+      --factListCols \"Label Name contextRef unitRef Dec Prec Lang Value EntityScheme EntityIdentifier Period Dimensions\"")
   end
 end
