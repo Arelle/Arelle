eded1895f0a6f126e0a3fb9242d611a09af3a9a8
Rainmaker <obioraakubue@yahoo.com>
2020-11-21 02:02:17 -0500

    clean up shoryuken args

--- xbrl_extraction_worker.rb
+++ xbrl_extraction_worker.rb
@@ -4,7 +4,9 @@ class XbrlExtractionWorker
 
   shoryuken_options queue: 'xbrl_extraction', auto_delete: true
 
-  def perform(sqs_msg, extract_from_path, extract_to_path)
+  def perform(sqs_msg)
+    extract_from_path = sqs_msg[:extract_from_path]
+    extract_to_path   = sqs_msg[:extract_to_path]
     system("python3 /opt/arelle/arelleCmdLine.py -f #{extract_from_path} --concepts #{extract_to_path}")
   end
 end
