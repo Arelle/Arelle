96d991895ebebfa9ddd48f78b23cb1804ea6312c
Herman Fischer <fischer@markv.com>
2021-09-09 09:59:22 -0700

    update taxonomy packages test script for updated index file

--- scripts/runPackageTests.sh
+++ scripts/runPackageTests.sh
@@ -6,21 +6,10 @@ TESTCASESROOT=/Users/hermf/Documents/mvsl/projects/XBRL.org/taxonomy-package-con
 rm -fr /users/hermf/temp/Package-test-*
 OUTPUTLOGFILE=/users/hermf/temp/Package-test-log.txt
 OUTPUTERRFILE=/users/hermf/temp/Package-test-err.txt
-OUTPUTCSVFILE=/users/hermf/temp/Package-test-report.csv
+OUTPUTCSVFILE=/users/hermf/temp/Package-test-report.xlsx
 TESTCASESINDEXFILE="$TESTCASESROOT/index.xml"
 
 ARELLEDIR=/users/hermf/Documents/mvsl/projects/arelle/arelleproject/src
 
-# standard approach doesn't work because plugins load differently from instances
+python3.9 ${ARELLEDIR}/arelleCmdLine.py --file "$TESTCASESINDEXFILE" --validate --csvTestReport "$OUTPUTCSVFILE"  --logFile "$OUTPUTLOGFILE" 2>  "$OUTPUTERRFILE"
 
-# python3.9 ${ARELLEDIR}/arelleCmdLine.py --file "$TESTCASESINDEXFILE" --validate --csvTestReport "$OUTPUTCSVFILE"  --logFile "$OUTPUTLOGFILE" 2>  "$OUTPUTERRFILE"
-
-PACKAGES=`ls -1 ${TESTCASESROOT}/*.zip`
-
-for PACKAGE in $PACKAGES
-  do
-  PACKAGEFILE=$(basename $PACKAGE)
-  echo Package: $PACKAGEFILE >> $OUTPUTLOGFILE
-  python3.9 ${ARELLEDIR}/arelleCmdLine.py --file "[]" --packages ${PACKAGE} --logFile "$OUTPUTLOGFILE" 2>  "$OUTPUTERRFILE"
-  echo "" >> $OUTPUTLOGFILE
-  done
