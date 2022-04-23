ea06c9b945d5f046c524360e9166f15e04fdcd69
Derek Gengenbacher <derek.gengenbacher@workiva.com>
2022-04-23 09:35:47 -0600

    Add IXDS plugin for EFM validation

--- scripts/conformance_suites/run_efm_conformance_suite.py
+++ scripts/conformance_suites/run_efm_conformance_suite.py
@@ -5,23 +5,21 @@ from arelle.CntlrCmdLine import parseAndRun
 
 CONFORMANCE_SUITE = 'tests/resources/conformance_suites/efm_conformance_suite_2022.zip/conf'
 TAXONOMY_PACKAGE = 'tests/resources/taxonomy_packages/edgarTaxonomiesPackage-22.1.zip'
-PLUGIN = 'validate/EFM'
+EFM_PLUGIN = 'validate/EFM'
+IXDS_PLUGIN = 'inlineXbrlDocumentSet'
 FILTER = '(?!arelle:testcaseDataUnexpected)'
 
 BASE_ARGS = [
-    '--logCodeFilter', FILTER,
-    '--packages', '{}'.format(os.path.abspath(TAXONOMY_PACKAGE)),
-    '--plugins', PLUGIN,
-    '--testcaseResultsCaptureWarnings',
-    '--validate'
-]
-
-IXBRL_ARGS = [
+    '--csvTestReport', './EFM-conf-report.xlsx',
     '--disclosureSystem', 'efm-pramatic',
     '--file', os.path.abspath(os.path.join(CONFORMANCE_SUITE, 'testcases.xml')),
     '--formula', 'run',
+    '--logCodeFilter', FILTER,
     '--logFile', './EFM-conf-log.txt',
-    '--csvTestReport', './EFM-conf-report.xlsx',
+    '--packages', '{}'.format(os.path.abspath(TAXONOMY_PACKAGE)),
+    '--plugins', '{}|{}'.format(EFM_PLUGIN, IXDS_PLUGIN),
+    '--testcaseResultsCaptureWarnings',
+    '--validate'
 ]
 
 
@@ -39,5 +37,4 @@ def run_efm_conformance_suite(args):
 
 if __name__ == "__main__":
     print('Running EFM tests...')
-    runtime_args = BASE_ARGS + IXBRL_ARGS
-    run_efm_conformance_suite(runtime_args)
+    run_efm_conformance_suite(BASE_ARGS)
--- tests/integration_tests/validation/EFM/test_efm_conformance_suite.py
+++ tests/integration_tests/validation/EFM/test_efm_conformance_suite.py
@@ -7,14 +7,15 @@ from tests.integration_tests.validation.validation_util import get_test_data
 
 CONFORMANCE_SUITE = 'tests/resources/conformance_suites/efm_conformance_suite_2022.zip/conf'
 TAXONOMY_PACKAGE = 'tests/resources/taxonomy_packages/edgarTaxonomiesPackage-22.1.zip'
-PLUGIN = 'validate/EFM'
+EFM_PLUGIN = 'validate/EFM'
+IXDS_PLUGIN = 'inlineXbrlDocumentSet'
 ARGS = [
     '--disclosureSystem', 'efm-pragmatic',
     '--file', os.path.abspath(os.path.join(CONFORMANCE_SUITE, 'testcases.xml')),
     '--formula', 'run',
     '--keepOpen',
     '--packages', '{}'.format(os.path.abspath(TAXONOMY_PACKAGE)),
-    '--plugins', PLUGIN,
+    '--plugins', '{}|{}'.format(EFM_PLUGIN, IXDS_PLUGIN),
     '--testcaseResultsCaptureWarnings',
     '--validate'
 ]
