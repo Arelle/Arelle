126b505fedaa43969dc105735bd58d2a31e62f26
aaroncameron-wk <aaron.cameron@workiva.com>
2022-06-14 14:08:46 -0600

    Expected failure IDs for XBRL 2.1 and Dimensions

--- tests/integration_tests/validation/XBRL/test_xbrl_dimensions_conformance_suite.py
+++ tests/integration_tests/validation/XBRL/test_xbrl_dimensions_conformance_suite.py
@@ -18,12 +18,29 @@ if os.getenv('CONFORMANCE_SUITES_TEST_MODE') == 'OFFLINE':
 
 TEST_DATA = get_test_data(ARGS)
 
+EXPECTED_FAILURE_IDS = [
+    # The value of the xbrldt:targetRole attribute is valid
+    # Expected: sche:XmlSchemaError, Actual: xbrldte:TargetRoleNotResolvedError
+    '000-Schema-invalid/001-Taxonomy/V-03',
+    # An all hypercube has an msdos path in the targetRole attribute to locate the domain - dimension arc network
+    # Expected: sche:XmlSchemaError, Actual: xbrldte:TargetRoleNotResolvedError
+    '000-Schema-invalid/001-Taxonomy/V-08',
+    # A dimension-domain relationship has an msdos path in targetRole attribute to locate the domain-member arc network
+    # Expected: sche:XmlSchemaError, Actual: xbrldte:TargetRoleNotResolvedError
+    '000-Schema-invalid/001-Taxonomy/V-09',
+    # A domain-member relationship has an msdos path in targetRole attribute to locate the domain-member arc network
+    # Expected: sche:XmlSchemaError, Actual: xbrldte:TargetRoleNotResolvedError
+    '000-Schema-invalid/001-Taxonomy/V-10',
+]
+
 
 @pytest.mark.parametrize("result", TEST_DATA)
-def test_xbrl_dimensions_conformance_suite(result):
+def test_xbrl_dimensions_conformance_suite(result, request):
     """
     Test the XBRL Dimensions 1.0 Conformance Suite
     """
+    if request.node.callspec.id in EXPECTED_FAILURE_IDS:
+        pytest.xfail(f"Test '{request.node.callspec.id}' not supported yet")
     assert result.get('status') == 'pass', \
         'Expected these validation suffixes: {}, but received these validations: {}'.format(
             result.get('expected'), result.get('actual')
