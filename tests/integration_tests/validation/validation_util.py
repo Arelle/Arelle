edb69a539bf2d87925acd5cfb1c266fa87dcf2c3
aaroncameron-wk <aaron.cameron@workiva.com>
2022-07-06 11:04:37 -0600

    Support validating individual TESTCASE conformance suites

--- tests/integration_tests/validation/validation_util.py
+++ tests/integration_tests/validation/validation_util.py
@@ -18,21 +18,25 @@ def get_test_data(args):
     results = []
     model_document = cntlr.modelManager.modelXbrl.modelDocument
     if model_document is not None:
-        if model_document.type == ModelDocument.Type.TESTCASESINDEX:
-            for tc in sorted(model_document.referencesDocument.keys(), key=lambda doc: doc.uri):
-                uri_dir_parts = os.path.dirname(tc.uri).split('/')
-                test_case_dir = '/'.join(uri_dir_parts[-2:])
-                if hasattr(tc, "testcaseVariations"):
-                    for mv in tc.testcaseVariations:
-                        param = pytest.param(
-                            {
-                                'status': mv.status,
-                                'expected': mv.expected,
-                                'actual': mv.actual
-                            },
-                            id='{}/{}'.format(test_case_dir, str(mv.id or mv.name))
-                        )
-                        results.append(param)
+        test_cases = []
+        if model_document.type == ModelDocument.Type.TESTCASE:
+            test_cases.append(model_document)
+        elif model_document.type == ModelDocument.Type.TESTCASESINDEX:
+            test_cases = sorted(model_document.referencesDocument.keys(), key=lambda doc: doc.uri)
+        for test_case in test_cases:
+            uri_dir_parts = os.path.dirname(test_case.uri).split('/')
+            test_case_dir = '/'.join(uri_dir_parts[-2:])
+            if hasattr(test_case, "testcaseVariations"):
+                for mv in test_case.testcaseVariations:
+                    param = pytest.param(
+                        {
+                            'status': mv.status,
+                            'expected': mv.expected,
+                            'actual': mv.actual
+                        },
+                        id='{}/{}'.format(test_case_dir, str(mv.id or mv.name))
+                    )
+                    results.append(param)
     cntlr.modelManager.close()
     PackageManager.close()
     PluginManager.close()
