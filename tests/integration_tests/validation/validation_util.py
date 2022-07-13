b5acb3b5b842b1a1ae99e992a323e265895f6fd9
Steven Bronson <steven.bronson@workiva.com>
2022-07-13 06:11:26 -0400

    Move validation_util.get_test_data cleanup to a finally block.

--- tests/integration_tests/validation/validation_util.py
+++ tests/integration_tests/validation/validation_util.py
@@ -15,31 +15,33 @@ def get_test_data(args):
     :rtype: list of ::class:: `~pytest.param`
     """
     cntlr = parseAndRun(args)
-    results = []
-    model_document = cntlr.modelManager.modelXbrl.modelDocument
-    if model_document is not None:
-        test_cases = []
-        if model_document.type in (ModelDocument.Type.TESTCASE, ModelDocument.Type.REGISTRYTESTCASE):
-            test_cases.append(model_document)
-        elif model_document.type == ModelDocument.Type.TESTCASESINDEX:
-            test_cases = sorted(model_document.referencesDocument.keys(), key=lambda doc: doc.uri)
-        else:
-            raise 'Unhandled model document type: {}'.format(model_document.type)
-        for test_case in test_cases:
-            uri_dir_parts = os.path.dirname(test_case.uri).split('/')
-            test_case_dir = '/'.join(uri_dir_parts[-2:])
-            if hasattr(test_case, "testcaseVariations"):
-                for mv in test_case.testcaseVariations:
-                    param = pytest.param(
-                        {
-                            'status': mv.status,
-                            'expected': mv.expected,
-                            'actual': mv.actual
-                        },
-                        id='{}/{}'.format(test_case_dir, str(mv.id or mv.name))
-                    )
-                    results.append(param)
-    cntlr.modelManager.close()
-    PackageManager.close()
-    PluginManager.close()
-    return results
+    try:
+        results = []
+        model_document = cntlr.modelManager.modelXbrl.modelDocument
+        if model_document is not None:
+            test_cases = []
+            if model_document.type in (ModelDocument.Type.TESTCASE, ModelDocument.Type.REGISTRYTESTCASE):
+                test_cases.append(model_document)
+            elif model_document.type == ModelDocument.Type.TESTCASESINDEX:
+                test_cases = sorted(model_document.referencesDocument.keys(), key=lambda doc: doc.uri)
+            else:
+                raise 'Unhandled model document type: {}'.format(model_document.type)
+            for test_case in test_cases:
+                uri_dir_parts = os.path.dirname(test_case.uri).split('/')
+                test_case_dir = '/'.join(uri_dir_parts[-2:])
+                if hasattr(test_case, "testcaseVariations"):
+                    for mv in test_case.testcaseVariations:
+                        param = pytest.param(
+                            {
+                                'status': mv.status,
+                                'expected': mv.expected,
+                                'actual': mv.actual
+                            },
+                            id='{}/{}'.format(test_case_dir, str(mv.id or mv.name))
+                        )
+                        results.append(param)
+        return results
+    finally:
+        cntlr.modelManager.close()
+        PackageManager.close()
+        PluginManager.close()
