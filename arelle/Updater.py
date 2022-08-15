0f140c5027e14164610ad36639fb78701e00e46c
Austin M. Matherne <Austin.Matherne@Workiva.com>
2022-08-15 16:56:06 -0500

    Formatting cleanup

--- arelle/Updater.py
+++ arelle/Updater.py
@@ -1,18 +1,23 @@
-'''
+"""
 Created on May 30, 2010
 
 @author: Mark V Systems Limited
 (c) Copyright 2011 Mark V Systems Limited, All rights reserved.
-'''
-import tkinter.messagebox, webbrowser, os, threading
+"""
+import os
+import threading
+import tkinter.messagebox
+
 
 def checkForUpdates(cntlr):
     if not cntlr.webCache.workOffline:
         # check for updates in background
         import threading
+
         thread = threading.Thread(target=lambda c=cntlr: backgroundCheckForUpdates(c))
         thread.daemon = True
         thread.start()
+
 
 def backgroundCheckForUpdates(cntlr):
     actualUrl = None
@@ -20,67 +25,80 @@ def backgroundCheckForUpdates(cntlr):
     try:
         attachmentFileName = cntlr.webCache.getAttachmentFilename(cntlr.updateURL)
         if attachmentFileName:
-            cntlr.showStatus("") # clear web loading status entry
+            cntlr.showStatus("")  # clear web loading status entry
             cntlr.uiThreadQueue.put((checkUpdateUrl, [cntlr, attachmentFileName]))
     except:
         pass
-    cntlr.showStatus("") # clear web loading status entry
+    cntlr.showStatus("")  # clear web loading status entry
+
 
 def checkUpdateUrl(cntlr, attachmentFileName):
     # get latest header file
     try:
         from arelle import WebCache, Version
+
         filename = os.path.basename(attachmentFileName)
         if filename and "-20" in filename:
             i = filename.index("-20") + 1
-            filenameDate = filename[i:i+10]
+            filenameDate = filename[i : i + 10]
             versionDate = Version.version[0:10]
             if filenameDate > versionDate:
                 # newer
                 reply = tkinter.messagebox.askyesnocancel(
-                            _("arelle\u2122 - Updater"),
-                            _("Update {0} is available, running version is {1}.  \n\nDownload now?    \n\n(Arelle will exit before installing.)").format(
-                                  filenameDate, versionDate),
-                            parent=cntlr.parent)
+                    _("arelle\u2122 - Updater"),
+                    _(
+                        "Update {0} is available, running version is {1}.  \n\nDownload now?    \n\n(Arelle will exit before installing.)"
+                    ).format(filenameDate, versionDate),
+                    parent=cntlr.parent,
+                )
                 if reply is None:
                     return False
                 if reply:
-                    thread = threading.Thread(target=lambda u=actualUrl: backgroundDownload(cntlr, u))
+                    thread = threading.Thread(
+                        target=lambda u=actualUrl: backgroundDownload(cntlr, u)
+                    )
                     thread.daemon = True
                     thread.start()
             else:
                 if filenameDate < versionDate:
-                    msg = _("Arelle running version, {0}, is newer than the downloadable version, {1}.").format(
-                              versionDate, filenameDate)
+                    msg = _(
+                        "Arelle running version, {0}, is newer than the downloadable version, {1}."
+                    ).format(versionDate, filenameDate)
                 else:
-                    msg = _("Arelle running version, {0}, is the same as the downloadable version.").format(
-                              versionDate)
-                tkinter.messagebox.showwarning(_("arelle\u2122 - Updater"), msg, parent=cntlr.parent)
-
+                    msg = _(
+                        "Arelle running version, {0}, is the same as the downloadable version."
+                    ).format(versionDate)
+                tkinter.messagebox.showwarning(
+                    _("arelle\u2122 - Updater"), msg, parent=cntlr.parent
+                )
     except:
         pass
 
     return
+
 
 def backgroundDownload(cntlr, url):
     filepathtmp = cntlr.webCache.getfilename(cntlr.updateURL, reload=True)
     cntlr.modelManager.showStatus(_("Download ompleted"), 5000)
     filepath = os.path.join(os.path.dirname(filepathtmp), os.path.basename(url))
     os.rename(filepathtmp, filepath)
-    cntlr.uiThreadQueue.put((install, [cntlr,filepath]))
+    cntlr.uiThreadQueue.put((install, [cntlr, filepath]))
 
-def install(cntlr,filepath):
+
+def install(cntlr, filepath):
     import sys
+
     if sys.platform.startswith("win"):
         os.startfile(filepath)
     else:
         if sys.platform in ("darwin", "macos"):
-            command = 'open'
-        else: # linux/unix
-            command = 'xdg-open'
+            command = "open"
+        else:  # linux/unix
+            command = "xdg-open"
         try:
             import subprocess
-            subprocess.Popen([command,filepath])
+
+            subprocess.Popen([command, filepath])
         except:
             pass
     cntlr.uiThreadQueue.put((cntlr.quit, []))
