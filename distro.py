d3e43248e98962ad1bd25cbb75ec67d8239c45a9
Austin M. Matherne <Austin.Matherne@Workiva.com>
2022-09-15 21:45:47 -0500

    Streamline cx_Freeze build

--- distro.py
+++ distro.py
@@ -1,275 +1,131 @@
 """
-Created on Jan 30, 2011
-
 @author: Mark V Systems Limited
 (c) Copyright 2011 Mark V Systems Limited, All rights reserved.
-
-  pip3.9 or python3.9 -m pip (followed by install below)
-    >> windows only may need install wheel
-  install lxml pg8000 pymysql pyodbc
-    >> windows only install cx_Oracle
-  install numpy matplotlib
-  install rdflib
-  install isodate regex aniso8601 graphviz holidays openpyxl Pillow
-  #  install pycrypto  << end user installed only if using any plugin/security module
-  install cx_freeze cherrypy cheroot tornado
-  pip install --no-cache --use-pep517 pycountry
-
-  may need to reinstall pycountry to get pep517 format
-  install pycountry: pip uninstall -y pycountry ; pip install --no-cache --use-pep517 pycountry
-
-to install pycrypto on windows (by end-users using plugin/security modules) see:
-  https://www.dariawan.com/tutorials/python/python-3-install-pycrypto-windows/
-
 """
 import os
 import sys
 
+from cx_Freeze import Executable, setup
+from setuptools import find_packages
+
 VERSION_FILE = "version.txt"
 
-options = {}
-executables = []
+LINUX_PLATFORM = "linux"
+MACOS_PLATFORM = "darwin"
+WINDOWS_PLATFORM = "win32"
 
+cliExecutable = Executable(script="arelleCmdLine.py")
+packages = find_packages()
+includeFiles = [
+    (os.path.normcase("arelle/config"), "config"),
+    (os.path.normcase("arelle/doc"), "doc"),
+    (os.path.normcase("arelle/images"), "images"),
+    (os.path.normcase("arelle/locale"), "locale"),
+    (os.path.normcase("arelle/examples"), "examples"),
+    (os.path.normcase("arelle/examples/plugin"), os.path.normcase("examples/plugin")),
+    (
+        os.path.normcase("arelle/examples/plugin/locale/fr/LC_MESSAGES"),
+        os.path.normcase("examples/plugin/locale/fr/LC_MESSAGES"),
+    ),
+    (os.path.normcase("arelle/plugin"), "plugin"),
+]
+includeLibs = [
+    "aniso8601",
+    "graphviz",
+    "gzip",
+    "holidays",
+    "isodate",
+    "lxml._elementpath",
+    "lxml.etree",
+    "lxml.html",
+    "lxml",
+    "numpy.core._methods",
+    "numpy.lib.format",
+    "numpy",
+    "openpyxl",
+    "pg8000",
+    "PIL",
+    "pycountry",
+    "pymysql",
+    "rdflib.extras",
+    "rdflib.plugins.memory",
+    "rdflib.plugins.parsers",
+    "rdflib.plugins.serializers.rdfxml",
+    "rdflib.plugins.serializers.turtle",
+    "rdflib.plugins.serializers.xmlwriter",
+    "rdflib.plugins.serializers",
+    "rdflib.plugins.sparql",
+    "rdflib.plugins.stores",
+    "rdflib.plugins",
+    "rdflib.tools",
+    "rdflib",
+    "regex",
+    "sqlite3",
+    "zlib",
+]
+options = {
+    "build_exe": {
+        "include_files": includeFiles,
+        "includes": includeLibs,
+        "packages": packages,
+    }
+}
 
-if sys.platform in ("darwin", "linux"):
-    from setuptools import find_packages
+if os.path.exists(VERSION_FILE):
+    includeFiles.append((VERSION_FILE, VERSION_FILE))
 
-    try:
-        from cx_Freeze import setup, Executable
+if os.path.exists("arelle/plugin/EdgarRenderer"):
+    includeLibs.append("cherrypy")
+    includeLibs.append("dateutil")
+    includeLibs.append("dateutil.relativedelta")
+    includeLibs.append("matplotlib")
+    includeLibs.append("matplotlib.pyplot")
+    includeLibs.append("pyparsing")
+    includeLibs.append("pytz")
+    includeLibs.append("six")
+    includeLibs.append("tornado")
 
-        executables = [
-            Executable(script="arelleGUI.py", target_name="arelleGUI"),
-            Executable(script="arelleCmdLine.py"),
-        ]
-    except:
-        from setuptools import setup
-
-        executables = []
-
-    packages = find_packages(
-        ".",  # note that new setuptools finds plugin and lib unwanted stuff
-        exclude=["*.plugin.*", "*.lib.*"],
-    )
-
-    includeFiles = [
-        ("arelle/config", "config"),
-        ("arelle/doc", "doc"),
-        ("arelle/images", "images"),
-        ("arelle/locale", "locale"),
-        ("arelle/examples", "examples"),
-        ("arelle/examples/plugin", "examples/plugin"),
-        (
-            "arelle/examples/plugin/locale/fr/LC_MESSAGES",
-            "examples/plugin/locale/fr/LC_MESSAGES",
-        ),
-        ("arelle/plugin", "plugin"),
-    ]
-    if sys.platform == "darwin":
-        includeFiles.append(("arelle/scripts-macOS", "scripts"))
-        includeFiles.append(("libs/macos/Tktable2.11", "Tktable2.11"))
-    else:
-        includeFiles.append(("arelle/scripts-unix", "scripts"))
-        if os.path.exists("/etc/redhat-release"):
-            # extra libraries needed for red hat
-            includeFiles.append(("/usr/lib64/libexslt.so.0", "libexslt.so"))
-            includeFiles.append(("/usr/lib64/libxml2.so", "libxml2.so"))
-            # for some reason redhat needs libxml2.so.2 as well
-            includeFiles.append(("/usr/lib64/libxml2.so.2", "libxml2.so.2"))
-            includeFiles.append(("/usr/lib64/libxslt.so.1", "libxslt.so"))
-            includeFiles.append(
-                ("/lib64/libz.so.1", "libz.so.1")
-            )  # not standard in RHEL6
-            includeFiles.append(
-                ("/usr/lib64/liblzma.so.5", "liblzma.so.5")
-            )  # not standard in RHEL6
-            includeFiles.append(("/usr/local/lib/tcl8.6", "tcl8.6"))
-            includeFiles.append(("/usr/local/lib/tk8.6", "tk8.6"))
-
-    if os.path.exists(VERSION_FILE):
-        includeFiles.append((VERSION_FILE, VERSION_FILE))
-
-    includeLibs = [
-        "lxml",
-        "lxml.etree",
-        "lxml._elementpath",
-        "lxml.html",
-        "pg8000",
-        "pymysql",
-        "sqlite3",
-        "numpy",
-        "numpy.core._methods",
-        "numpy.lib.format",
-        # note cx_Oracle isn't here because it is version and machine specific,
-        # ubuntu not likely working
-        # more rdflib plugin modules may need to be added later
-        "rdflib",
-        "rdflib.extras",
-        "rdflib.tools",
-        "rdflib.plugins",
-        "rdflib.plugins.memory",
-        "rdflib.plugins.parsers",
-        "rdflib.plugins.serializers",
-        "rdflib.plugins.serializers.rdfxml",
-        "rdflib.plugins.serializers.turtle",
-        "rdflib.plugins.serializers.xmlwriter",
-        "rdflib.plugins.sparql",
-        "rdflib.plugins.stores",
-        "isodate",
-        "regex",
-        "gzip",
-        "zlib",
-        "aniso8601",
-        "graphviz",
-        "holidays",
-        "openpyxl",
-        "PIL",  # to install PIL it's named Pillow
-        "pycountry"  # to install pycountry: pip uninstall -y pycountry ; pip install --no-cache --use-pep517 pycountry
-        # only installed by end-users when using security plugins: 'Crypto', 'Crypto.Cipher', 'Crypto.Cipher.AES' # install pycrypto not another crypto module
-        # 'google_api_python_client', 'oauth2client', 'six', 'httplib2', 'uritemplate', 'pyasn1', 'rsa', 'pyasn1_modules' # google-api-python-client
-    ]
-
-    excludeLibs = []
-
-    if os.path.exists("arelle/plugin/EdgarRenderer"):
-        includeLibs += [
-            "cherrypy",  # 'cherrypy.wsgiserver.wsgiserver3',
-            "dateutil",
-            "pytz",  # pytz installed by dateutil
-            "dateutil.relativedelta",
-            "tornado",
-            "pyparsing",
-            "matplotlib",
-            "matplotlib.pyplot",
-        ]
-
-    options = dict(
-        build_exe={
-            "include_files": includeFiles,
-            #
-            # rdflib & isodate egg files: rename .zip cpy lib & egg-info
-            # subdirectories to site-packages directory
-            #
-            "includes": includeLibs,
-            "excludes": excludeLibs,
-            "packages": packages,
-        }
-    )
+if sys.platform == LINUX_PLATFORM:
+    guiExecutable = Executable(script="arelleGUI.py", target_name="arelleGUI")
+    includeFiles.append(("arelle/scripts-unix", "scripts"))
+    if os.path.exists("/etc/redhat-release"):
+        includeFiles.append(("/usr/lib64/libexslt.so.0", "libexslt.so"))
+        includeFiles.append(("/usr/lib64/libxml2.so", "libxml2.so"))
+        includeFiles.append(("/usr/lib64/libxml2.so.2", "libxml2.so.2"))
+        includeFiles.append(("/usr/lib64/libxslt.so.1", "libxslt.so"))
+        includeFiles.append(("/lib64/libz.so.1", "libz.so.1"))
+        includeFiles.append(("/usr/lib64/liblzma.so.5", "liblzma.so.5"))
+        includeFiles.append(("/usr/local/lib/tcl8.6", "tcl8.6"))
+        includeFiles.append(("/usr/local/lib/tk8.6", "tk8.6"))
+elif sys.platform == MACOS_PLATFORM:
+    guiExecutable = Executable(script="arelleGUI.py", target_name="arelleGUI")
+    includeFiles.append(("arelle/scripts-macOS", "scripts"))
+    includeFiles.append(("libs/macos/Tktable2.11", "Tktable2.11"))
     options["bdist_mac"] = {
         "iconfile": "arelle/images/arelle.icns",
         "bundle_name": "Arelle",
     }
-
-
-elif sys.platform == "win32":
-    from setuptools import find_packages
-    from cx_Freeze import setup, Executable
-
-    # FIXME: this should use the entry_points mechanism
-    packages = find_packages(".")
-    print("packages={}".format(packages))
-    includeFiles = [
-        ("arelle\\config", "config"),
-        ("arelle\\doc", "doc"),
-        ("arelle\\images", "images"),
-        ("arelle\\locale", "locale"),
-        ("arelle\\examples", "examples"),
-        ("arelle\\examples\\plugin", "examples/plugin"),
-        (
-            "arelle\\examples\\plugin\\locale\\fr\\LC_MESSAGES",
-            "examples/plugin/locale/fr/LC_MESSAGES",
-        ),
-        ("arelle\\plugin", "plugin"),
-        ("arelle\\scripts-windows", "scripts"),
-    ]
+elif sys.platform == WINDOWS_PLATFORM:
+    guiExecutable = Executable(
+        script="arelleGUI.pyw",
+        base="Win32GUI",
+        icon="arelle\\images\\arelle16x16and32x32.ico",
+    )
+    includeFiles.append(("arelle\\scripts-windows", "scripts"))
     if "arelle.webserver" in packages:
         includeFiles.append("QuickBooks.qwc")
-
-    if os.path.exists(VERSION_FILE):
-        includeFiles.append(VERSION_FILE)
-
-    includeLibs = [
-        "lxml",
-        "lxml.etree",
-        "lxml._elementpath",
-        "lxml.html",
-        "pg8000",
-        "pymysql",
-        "cx_Oracle",
-        "pyodbc",
-        "sqlite3",
-        "numpy",
-        "numpy.core._methods",
-        "numpy.lib.format",  # additional modules of numpy
-        # more rdflib plugin modules may need to be added later
-        "rdflib",
-        "rdflib.extras",
-        "rdflib.tools",
-        "rdflib.plugins",
-        "rdflib.plugins.memory",
-        "rdflib.plugins.parsers",
-        "rdflib.plugins.serializers",
-        "rdflib.plugins.serializers.rdfxml",
-        "rdflib.plugins.serializers.turtle",
-        "rdflib.plugins.serializers.xmlwriter",
-        "rdflib.plugins.sparql",
-        "rdflib.plugins.stores",
-        "isodate",
-        "regex",
-        "gzip",
-        "zlib",
-        "aniso8601",
-        "graphviz",
-        "holidays",
-        "openpyxl",
-        "PIL",
-        "pycountry",
-        # only installed by end-users when using security plugins: 'Crypto', 'Crypto.Cipher', 'Crypto.Cipher.AES',
-        "requests",
-        "requests_negotiate_sspi",
-    ]
-    # uncomment the next line if cx_Freezing with EdgarRenderer
-    # note that openpyxl must be 2.1.4 at this time
-    # removed tornado
-    if os.path.exists("arelle/plugin/EdgarRenderer"):
-        includeLibs += [
-            "cherrypy",  # 'cherrypy.wsgiserver.wsgiserver3',
-            "dateutil",
-            "dateutil.relativedelta",
-            "six",
-            "pyparsing",
-            "matplotlib",
-            "matplotlib.pyplot",
-        ]
-
-    options = dict(
-        build_exe={
-            "include_files": includeFiles,
-            "include_msvcr": True,  # include MSVCR100
-            # "icon": 'arelle\\images\\arelle16x16and32x32.ico',
-            "packages": packages,
-            #
-            # rdflib & isodate egg files: rename .zip cpy lib & egg-info
-            # subdirectories to site-packages directory
-            #
-            "includes": includeLibs,
-        }
+    # note cx_Oracle isn't included for unix builds because it is version and machine specific.
+    includeLibs.append("cx_Oracle")
+    includeLibs.append("pyodbc")
+    includeLibs.append("requests")
+    includeLibs.append("requests_negotiate_sspi")
+    options["build_exe"]["include_msvcr"] = True
+else:
+    raise ValueError(
+        f"Frozen builds are supported on Windows, Linux, and macOS. Platform {sys.platform} is not supported."
     )
 
-    # windows uses arelleGUI.exe to launch in GUI mode, arelleCmdLine.exe in command line mode
-    executables = [
-        Executable(
-            script="arelleGUI.pyw",
-            base="Win32GUI",
-            icon="arelle\\images\\arelle16x16and32x32.ico",
-        ),
-        Executable(
-            script="arelleCmdLine.py",
-        ),
-    ]
-else:
-    executables = []
-
 setup(
+    executables=[guiExecutable, cliExecutable],
     options=options,
-    executables=executables,
 )
