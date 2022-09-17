c932b99b6edce5dc9ecfb0d66e504d0a96371713
sherif <selgamal0@gmail.com>
2022-09-17 18:24:53 +0200

    Add type hints to arelle.XbrlUtil

--- arelle/XbrlUtil.py
+++ arelle/XbrlUtil.py
@@ -5,22 +5,32 @@ Created on Nov 26, 2010
 (c) Copyright 2010 Mark V Systems Limited, All rights reserved.
 '''
 from __future__ import annotations
-from typing import Any
+from typing import Any, Sequence, TYPE_CHECKING, cast
 import math
 from arelle.ModelValue import QName, DateTime
 from arelle.ModelObject import ModelObject, ModelAttribute
 from arelle.XmlValidate import UNKNOWN, VALID, VALID_ID, validate as xmlValidate
 
-S_EQUAL = 0 # ordinary S-equality from 2.1 spec
-S_EQUAL2 = 1 # XDT definition adds QName comparisions
-XPATH_EQ = 2 # XPath EQ on all types
-VALIDATE_BY_STRING_VALUE = 3
+if TYPE_CHECKING:
+    from arelle.ModelXbrl import ModelXbrl
 
-NO_IDs_EXCLUDED = 0
-ALL_IDs_EXCLUDED = 1
-TOP_IDs_EXCLUDED = 2 # only ancestor IDs are excluded
+S_EQUAL: int = 0 # ordinary S-equality from 2.1 spec
+S_EQUAL2: int = 1 # XDT definition adds QName comparisons
+XPATH_EQ: int = 2 # XPath EQ on all types
+VALIDATE_BY_STRING_VALUE: int = 3
 
-def nodesCorrespond(dts1, elt1, elt2, dts2=None, equalMode=XPATH_EQ, excludeIDs=ALL_IDs_EXCLUDED):
+NO_IDs_EXCLUDED: int = 0
+ALL_IDs_EXCLUDED: int = 1
+TOP_IDs_EXCLUDED: int = 2 # only ancestor IDs are excluded
+
+def nodesCorrespond(
+    dts1: ModelXbrl,
+    elt1: ModelObject | ModelAttribute | None,
+    elt2: ModelObject | ModelAttribute | None,
+    dts2: ModelXbrl | None = None,
+    equalMode: int = XPATH_EQ,
+    excludeIDs: int = ALL_IDs_EXCLUDED
+) -> bool:
     if elt1 is None:
         return elt2 is None #both can be empty sequences (no element) and true
     elif elt2 is None or not isinstance(elt1, (ModelObject,ModelAttribute)) or not isinstance(elt2, (ModelObject,ModelAttribute)):
@@ -37,40 +47,53 @@ def nodesCorrespond(dts1, elt1, elt2, dts2=None, equalMode=XPATH_EQ, excludeIDs=
     return sEqual(dts1, elt1, elt2, equalMode=equalMode, dts2=dts2, excludeIDs=ALL_IDs_EXCLUDED)
 
 # dts1 is modelXbrl for first element
-# dts2 is for second element assumed same unless dts2 and ns2 to ns1 maping table provided
+# dts2 is for second element assumed same unless dts2 and ns2 to ns1 mapping table provided
 #   (as used in versioning reports and multi instance
 
-def equalityHash(elt, equalMode=S_EQUAL, excludeIDs=NO_IDs_EXCLUDED):
+def equalityHash(
+    elt: ModelObject | Sequence[ModelObject],
+    equalMode: int = S_EQUAL,
+    excludeIDs: int = NO_IDs_EXCLUDED
+) -> int:
     if isinstance(elt, ModelObject):
         try:
             if equalMode == S_EQUAL:
-                return elt._hashSEqual
+                return cast(int, getattr(elt, '_hashSEqual'))
             else:
-                return elt._hashXpathEqual
+                return cast(int, getattr(elt, '_hashXpathEqual'))
         except AttributeError:
             dts = elt.modelXbrl
+            assert isinstance(dts, ModelXbrl)
             if not hasattr(elt,"xValid"):
-                xmlValidate(dts, elt)
-            hashableValue = elt.sValue if equalMode == S_EQUAL else elt.xValue
+                xmlValidate(dts, elt)  # type: ignore[no-untyped-call]
+            hashableValue = getattr(elt, 'sValue') if equalMode == S_EQUAL else getattr(elt, 'xValue')
             if isinstance(hashableValue,float) and math.isnan(hashableValue):
                 hashableValue = (hashableValue,elt)    # ensure this NaN only compares to itself and no other NaN
             _hash = hash((elt.elementQname,
                           hashableValue,
-                          tuple(sorted(attributeDict(dts, elt, (), equalMode, excludeIDs, distinguishNaNs=True).items(),
+                          tuple(sorted(attributeDict(dts, elt, set(), equalMode, excludeIDs, distinguishNaNs=True).items(),
                                        key=lambda item: item[0])), # must sort so attrs always hashed in same order
                           tuple(equalityHash(child,equalMode,excludeIDs) for child in childElements(elt))
                           ))
             if equalMode == S_EQUAL:
-                elt._hashSEqual = _hash
+                setattr(elt, '_hashSEqual', _hash)
             else:
-                elt._hashXpathEqual = _hash
+                setattr(elt, '_hashXpathEqual', _hash)
             return _hash
     elif isinstance(elt, (tuple,list,set)):
         return hash( tuple(equalityHash(i) for i in elt) )
     else:
         return hash(None)
 
-def sEqual(dts1, elt1, elt2, equalMode=S_EQUAL, excludeIDs=NO_IDs_EXCLUDED, dts2=None, ns2ns1Tbl=None):
+def sEqual(
+    dts1: ModelXbrl,
+    elt1: ModelObject,
+    elt2: ModelObject,
+    equalMode: int = S_EQUAL,
+    excludeIDs: int = NO_IDs_EXCLUDED,
+    dts2: ModelXbrl | None = None,
+    ns2ns1Tbl: dict[str, str] | None = None
+) -> bool:
     if dts2 is None: dts2 = dts1
     if elt1.localName != elt2.localName:
         return False
@@ -80,9 +103,9 @@ def sEqual(dts1, elt1, elt2, equalMode=S_EQUAL, excludeIDs=NO_IDs_EXCLUDED, dts2
     elif elt1.namespaceURI != elt2.namespaceURI:
         return False
     if not hasattr(elt1,"xValid"):
-        xmlValidate(dts1, elt1)
+        xmlValidate(dts1, elt1)  # type: ignore[no-untyped-call]
     if not hasattr(elt2,"xValid"):
-        xmlValidate(dts2, elt2)
+        xmlValidate(dts2, elt2)  # type: ignore[no-untyped-call]
     children1 = childElements(elt1)
     children2 = childElements(elt2)
     if len(children1) != len(children2):
@@ -93,8 +116,8 @@ def sEqual(dts1, elt1, elt2, equalMode=S_EQUAL, excludeIDs=NO_IDs_EXCLUDED, dts2
                    # VALIDATE_BY_STRING_VALUE if len(children1) and elt1.xValid == VALID else
                    equalMode
                    ) or
-        attributeDict(dts1, elt1, (), equalMode, excludeIDs) !=
-        attributeDict(dts2, elt2, (), equalMode, excludeIDs, ns2ns1Tbl)):
+        attributeDict(dts1, elt1, set(), equalMode, excludeIDs) !=
+        attributeDict(dts2, elt2, set(), equalMode, excludeIDs, ns2ns1Tbl)):
         return False
     excludeChildIDs = excludeIDs if excludeIDs != TOP_IDs_EXCLUDED else NO_IDs_EXCLUDED
     for i in range( len(children1) ):
@@ -102,12 +125,21 @@ def sEqual(dts1, elt1, elt2, equalMode=S_EQUAL, excludeIDs=NO_IDs_EXCLUDED, dts2
             return False
     return True
 
-def attributeDict(modelXbrl, elt, exclusions=set(), equalMode=S_EQUAL, excludeIDs=NO_IDs_EXCLUDED, ns2ns1Tbl=None, keyByTag=False, distinguishNaNs=False):
+def attributeDict(
+    modelXbrl: ModelXbrl,
+    elt: ModelObject,
+    exclusions: set[str] = set(),
+    equalMode: int = S_EQUAL,
+    excludeIDs: int = NO_IDs_EXCLUDED,
+    ns2ns1Tbl: dict[str, str] | None= None,
+    keyByTag: bool = False,
+    distinguishNaNs: bool = False
+) -> dict[QName, Any]:  # value can be any element value
     if not hasattr(elt,"xValid"):
-        xmlValidate(modelXbrl, elt)
+        xmlValidate(modelXbrl, elt)  # type: ignore[no-untyped-call]
     attrs = {}
     # TBD: replace with validated attributes
-    for modelAttribute in elt.xAttributes.values():
+    for modelAttribute in getattr(elt, 'xAttributes', {}).values():
         attrTag = modelAttribute.attrTag
         ns, sep, localName = attrTag.partition('}')
         attrNsURI = ns[1:] if sep else None
@@ -135,45 +167,60 @@ def attributeDict(modelXbrl, elt, exclusions=set(), equalMode=S_EQUAL, excludeID
                 pass  # what should be done if attribute failed to have psvi value
     return attrs
 
-def attributes(modelXbrl, elt, exclusions=set(), ns2ns1Tbl=None, keyByTag=False) -> tuple[Any, Any]:
+def attributes(
+    modelXbrl: ModelXbrl,
+    elt: ModelObject,
+    exclusions: set[str] = set(),
+    ns2ns1Tbl: dict[str, str] | None = None,
+    keyByTag: bool = False
+) -> tuple[tuple[QName, Any], ...]:
     a = attributeDict(modelXbrl, elt, exclusions, ns2ns1Tbl=ns2ns1Tbl, keyByTag=keyByTag)
     return tuple( (k,a[k]) for k in sorted(a.keys()) )
 
-def childElements(elt):
+def childElements(elt: ModelObject) -> list[ModelObject]:
     return [child for child in elt if isinstance(child,ModelObject)]
 
-def xEqual(elt1, elt2, equalMode=S_EQUAL):
+def xEqual(elt1: ModelObject, elt2: ModelObject, equalMode: int = S_EQUAL) -> bool:
     if not hasattr(elt1,"xValid"):
-        xmlValidate(elt1.modelXbrl, elt1)
+        xmlValidate(elt1.modelXbrl, elt1)  # type: ignore[no-untyped-call]
     if not hasattr(elt2,"xValid"):
-        xmlValidate(elt2.modelXbrl, elt2)
+        xmlValidate(elt2.modelXbrl, elt2)  # type: ignore[no-untyped-call]
     if equalMode == VALIDATE_BY_STRING_VALUE:
         return elt1.stringValue == elt2.stringValue
     elif equalMode == S_EQUAL: # formula WG e-mail 2018-09-06: or (equalMode == S_EQUAL2 and not isinstance(elt1.sValue, QName)):
-        return elt1.sValue == elt2.sValue
+        return cast(bool, getattr(elt1, 'sValue') == getattr(elt2, 'sValue'))
     else: # includes dimension S-equal2, use xpath-2 equality.
-        if isinstance(elt1.xValue, DateTime) and isinstance(elt2.xValue, DateTime) and elt1.xValue.dateOnly != elt2.xValue.dateOnly:
+        elt1_xValue = getattr(elt1, 'xValue')
+        elt2_xValue = getattr(elt2, 'xValue')
+        if isinstance(elt1_xValue, DateTime) \
+            and isinstance(elt2_xValue, DateTime) \
+                and getattr(elt1_xValue, 'dateOnly') \
+                    != getattr(elt2_xValue, 'dateOnly'):
             return False
-        return elt1.xValue == elt2.xValue
+        return cast(bool, elt1_xValue == elt2_xValue)
 
-def vEqual(elt1, elt2):
+def vEqual(elt1: ModelObject, elt2: ModelObject) -> bool:
     if not hasattr(elt1,"xValid"):
-        xmlValidate(elt1.modelXbrl, elt1)
+        xmlValidate(elt1.modelXbrl, elt1)  # type: ignore[no-untyped-call]
     if not hasattr(elt2,"xValid"):
-        xmlValidate(elt2.modelXbrl, elt2)
-    return elt1.sValue == elt2.sValue
+        xmlValidate(elt2.modelXbrl, elt2)  # type: ignore[no-untyped-call]
+    return cast(bool, getattr(elt1, 'sValue') == getattr(elt2, 'sValue'))
 
-def typedValue(dts, element, attrQname=None):
+def typedValue(
+    dts: ModelXbrl | None,
+    element: ModelObject,
+    attrQname: QName | None = None
+) -> Any:  # This can by any type
     try:
         if attrQname: # PSVI attribute value
-            modelAttribute = element.xAttributes[attrQname.clarkNotation]
+            modelAttribute = getattr(element, 'xAttributes')[attrQname.clarkNotation]
             if modelAttribute.xValid >= VALID:
                 return modelAttribute.xValue
         else: # PSVI element value (of text)
-            if element.xValid >= VALID:
-                return element.xValue
+            if getattr(element, 'xValid') >= VALID:
+                return getattr(element, 'xValue')
     except (AttributeError, KeyError):
         if dts:
-            xmlValidate(dts, element, recurse=False, attrQname=attrQname)
+            xmlValidate(dts, element, recurse=False, attrQname=attrQname)  # type: ignore[no-untyped-call]
             return typedValue(None, element, attrQname=attrQname)
     return None
