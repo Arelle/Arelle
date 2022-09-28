8d685a7d8f3dd612cbd5d96bd1d2169dd1ac3d10
Sage Smith <sage.smith@workiva.com>
2022-09-28 14:48:39 -0600

    Dynamic Version.py

--- arelle/Version.py
+++ arelle/Version.py
@@ -1,10 +1,43 @@
-'''
+"""
 This module represents the time stamp when Arelle was last built
 
-@author: Mark V Systems Limited
-(c) Copyright 2020 Mark V Systems Limited, All rights reserved.
+@author: Workiva
+(c) Copyright 2022 Workiva, All rights reserved.
 
-'''
-__version__ = '1.2020.05.10'  # number version of code base and date compiled
-version = '2020-05-10 16:47 UTC'  # string version of date compiled
-copyrightLatestYear = '2020'  # string version of year compiled
+"""
+from __future__ import annotations
+
+from datetime import datetime
+
+
+def getBuildVersion() -> str | None:
+    try:
+        import arelle._version
+        return arelle._version.version
+    except ModuleNotFoundError:
+        return None
+
+
+def getGitHash() -> str | None:
+    import subprocess
+    p = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, encoding='utf-8')
+    if not p.stderr:
+        return p.stdout.strip()
+    return None
+
+
+def getDefaultVersion() -> str:
+    return '0.0.0'
+
+
+def getVersion() -> str:
+    for version_fetcher in [getBuildVersion, getGitHash, getDefaultVersion]:
+        fetched_version = version_fetcher()
+        if fetched_version is not None:
+            return fetched_version
+    raise ValueError('Version not set')
+
+
+__version__ = getVersion()
+version = __version__
+copyrightLatestYear = datetime.now().year
