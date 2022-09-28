f47125c6c1a91518ea7af7bd1707fc37560be49e
Patrik <21142447+ggravlingen@users.noreply.github.com>
2022-09-28 18:21:08 +0000

    Run Black and isort

--- tests/integration_tests/validation/validation_util.py
+++ tests/integration_tests/validation/validation_util.py
@@ -1,15 +1,16 @@
 """A helper to extract test data results."""
 
 from __future__ import annotations
-from collections import Counter
+
 import os.path
+from collections import Counter
 from pathlib import PurePosixPath
 
 import pytest
-
-from arelle.CntlrCmdLine import parseAndRun
-from arelle import ModelDocument, PackageManager, PluginManager
 from _pytest.mark.structures import ParameterSet
+
+from arelle import ModelDocument, PackageManager, PluginManager
+from arelle.CntlrCmdLine import parseAndRun
 
 
 def get_test_data(
@@ -31,39 +32,68 @@ def get_test_data(
         model_document = cntlr.modelManager.modelXbrl.modelDocument
         if model_document is not None:
             test_cases: list[ModelDocument.ModelDocument] = []
-            if model_document.type in (ModelDocument.Type.TESTCASE, ModelDocument.Type.REGISTRYTESTCASE):
+            if model_document.type in (
+                ModelDocument.Type.TESTCASE,
+                ModelDocument.Type.REGISTRYTESTCASE,
+            ):
                 test_cases.append(model_document)
             elif model_document.type == ModelDocument.Type.TESTCASESINDEX:
-                test_cases = sorted(model_document.referencesDocument.keys(), key=lambda doc: doc.uri)
+                test_cases = sorted(
+                    model_document.referencesDocument.keys(), key=lambda doc: doc.uri
+                )
             else:
-                raise Exception('Unhandled model document type: {}'.format(model_document.type))
+                raise Exception(
+                    "Unhandled model document type: {}".format(model_document.type)
+                )
             for test_case in test_cases:
                 path = PurePosixPath(test_case.uri)
-                test_case_path_tail = path.parts[-3:-1] if path.name == 'index.xml' else path.parts[-2:]
-                if not getattr(test_case, "testcaseVariations", None) and os.path.relpath(test_case.filepath, model_document.modelXbrl.fileSource.basefile) not in expected_empty_testcases:
+                test_case_path_tail = (
+                    path.parts[-3:-1] if path.name == "index.xml" else path.parts[-2:]
+                )
+                if (
+                    not getattr(test_case, "testcaseVariations", None)
+                    and os.path.relpath(
+                        test_case.filepath, model_document.modelXbrl.fileSource.basefile
+                    )
+                    not in expected_empty_testcases
+                ):
                     test_cases_with_no_variations.add(test_case_path_tail)
                 else:
                     for mv in test_case.testcaseVariations:
-                        test_id = '{}/{}'.format('/'.join(test_case_path_tail), mv.id)
+                        test_id = "{}/{}".format("/".join(test_case_path_tail), mv.id)
                         param = pytest.param(
                             {
-                                'status': mv.status,
-                                'expected': mv.expected,
-                                'actual': mv.actual
+                                "status": mv.status,
+                                "expected": mv.expected,
+                                "actual": mv.actual,
                             },
                             id=test_id,
-                            marks=[pytest.mark.xfail()] if test_id in expected_failure_ids else []
+                            marks=[pytest.mark.xfail()]
+                            if test_id in expected_failure_ids
+                            else [],
                         )
                         results.append(param)
         if test_cases_with_no_variations:
-            raise Exception(f"Some test cases don't have any variations: {sorted(test_cases_with_no_variations)}.")
+            raise Exception(
+                f"Some test cases don't have any variations: {sorted(test_cases_with_no_variations)}."
+            )
         test_id_frequencies = Counter(p.id for p in results)
-        nonunique_test_ids = {test_id: count for test_id, count in test_id_frequencies.items() if count > 1}
+        nonunique_test_ids = {
+            test_id: count
+            for test_id, count in test_id_frequencies.items()
+            if count > 1
+        }
         if nonunique_test_ids:
-            raise Exception(f'Some test IDs are not unique.  Frequencies of nonunique test IDs: {nonunique_test_ids}.')
-        nonexistent_expected_failure_ids = expected_failure_ids - set(test_id_frequencies)
+            raise Exception(
+                f"Some test IDs are not unique.  Frequencies of nonunique test IDs: {nonunique_test_ids}."
+            )
+        nonexistent_expected_failure_ids = expected_failure_ids - set(
+            test_id_frequencies
+        )
         if nonexistent_expected_failure_ids:
-            raise Exception(f"Some expected failure IDs don't match any test cases: {sorted(nonexistent_expected_failure_ids)}.")
+            raise Exception(
+                f"Some expected failure IDs don't match any test cases: {sorted(nonexistent_expected_failure_ids)}."
+            )
         return results
     finally:
         cntlr.modelManager.close()
