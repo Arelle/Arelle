3984937763e239b3a523a9fafed143846007f7b1
Patrik <21142447+ggravlingen@users.noreply.github.com>
2022-09-29 18:33:50 +0000

    Ignore test_esef_ixbrl_conformance_suite

--- conftest.py
+++ conftest.py
@@ -10,8 +10,14 @@ This module supports the conformance tests to validate that Arelle is
 working properly.  See arelle_test.py.
 
 '''
-
+import pytest
 import os
+from _pytest.nodes import Collector
+
+TESTS_TO_SKIP = [
+    # This test is slow to collect so we skip it during collections
+    "test_esef_ixbrl_conformance_suite"
+]
 
 def pytest_addoption(parser):
     tests_default = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -21,3 +27,8 @@ def pytest_addoption(parser):
     parser.addoption('--tests', default=tests_default,
                      help='.ini file to load test suites from (default is arelle/confi/arelle_test.ini)')
 
+
+collect_ignore = [
+    # Test collection of this file is very slow so we ignore it
+    "tests/integration_tests/validation/ESEF/test_esef_ixbrl_conformance_suite.py"
+]
