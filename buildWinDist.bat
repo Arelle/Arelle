e4433d4e34464aabc5afaf1c9a2cd07dfd0c74dd
aaroncameron-wk <aaron.cameron@workiva.com>
2022-10-27 12:28:44 -0600

    Local frozen builds on Windows

---
+++ buildWinDist.bat
@@ -0,0 +1,8 @@
+rem Make directories
+mkdir build,dist
+rem Rebuild messages.pot internationalization file
+python pygettext.py -v -o arelle\locale\messages.pot arelle\*.pyw arelle\*.py
+rem Regenerate messages catalog (doc/messagesCatalog.xml)
+python generateMessagesCatalog.py
+rem - name: Build exe
+python distro.py build_exe
