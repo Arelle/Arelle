aa5f14a019def09ceb8034ae1f8e5d406ed28fe3
Austin M. Matherne <Austin.Matherne@Workiva.com>
2023-04-19 10:41:18 -0400

    Mock path exists in unit tests to make sure tests pass/fail the same regardless of the system they are run on

--- tests/unit_tests/arelle/test_cntlr.py
+++ tests/unit_tests/arelle/test_cntlr.py
@@ -4,21 +4,25 @@ from arelle import Cntlr
 
 
 @patch('io.open')
+@patch('os.path.exists')
 @patch('arelle.Cntlr.Cntlr.setUiLanguage')
 @patch('arelle.ModelManager.ModelManager.setLocale')
-def test_cntlr_cache_enabled(_, mock_codes, mock_open):
+def test_cntlr_cache_enabled(_, mock_codes, mock_exists, mock_open):
     mock_codes.return_value = ['en-US', 'en_US', 'en']
+    mock_exists.return_value = True
     cntlr = Cntlr.Cntlr(uiLang=None)
-    assert mock_open.call_count == 1
+    assert mock_open.call_count == 3
     cntlr.saveConfig()
-    assert mock_open.call_count == 2
+    assert mock_open.call_count == 4
 
 @patch('io.open')
+@patch('os.path.exists')
 @patch('arelle.Cntlr.Cntlr.setUiLanguage')
 @patch('arelle.ModelManager.ModelManager.setLocale')
-def test_cntlr_cache_disabled(_, mock_codes, mock_open):
+def test_cntlr_cache_disabled(_, mock_codes, mock_exists, mock_open):
     mock_codes.return_value = ['en-US', 'en_US', 'en']
+    mock_exists.return_value = True
     cntlr = Cntlr.Cntlr(uiLang=None, disable_persistent_config=True)
-    assert mock_open.call_count == 0
+    assert mock_open.call_count == 1
     cntlr.saveConfig()
-    assert mock_open.call_count == 0
+    assert mock_open.call_count == 1
