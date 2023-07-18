a499ebd1fe1d38f4ec9a0eb5001175f1b7854bb5
allenbross-wf <allen.bross@workiva.com>
2023-07-18 08:41:28 -0600

    Updated regex for internet connectivity flags

--- arelleCmdLine.py
+++ arelleCmdLine.py
@@ -4,20 +4,28 @@ Use this module to start Arelle in command line modes
 
 See COPYRIGHT.md for copyright information.
 '''
+import itertools
 import os
 import sys
-import re
-from arelle.SocketUtils import INTERNET_CONNECTIVITY, OFFLINE
-
+import regex as re
 
 if sys.platform == "darwin" and getattr(sys, 'frozen', False):
     for i in range(len(sys.path)):  # signed code can't contain python modules
         sys.path.append(sys.path[i].replace("MacOS", "Resources"))
 
-if re.compile(rf"(\s|^)--({INTERNET_CONNECTIVITY}|{INTERNET_CONNECTIVITY.lower()})(\s+|=){OFFLINE}(\s|$)") in sys.argv:
-    from arelle.SocketUtils import warnSocket
-    warnSocket()
+from arelle.SocketUtils import INTERNET_CONNECTIVITY, OFFLINE, warnSocket
 
+conn_regex = rf'(\s|^)--({INTERNET_CONNECTIVITY}|{INTERNET_CONNECTIVITY.lower()})'
+offline_regex = rf'{OFFLINE}(\s|$)'
+comp_eq = re.compile(conn_regex + '=' + offline_regex)
+comp_conn = re.compile(conn_regex)
+comp_offline = re.compile(offline_regex)
+for arg in sys.argv:
+    if re.match(comp_eq, arg):
+        warnSocket()
+for arg1, arg2 in itertools.pairwise(sys.argv):
+    if re.match(comp_conn, arg1) and re.match(comp_offline, arg2):
+        warnSocket()
 
 from arelle.BetaFeatures import BETA_OBJECT_MODEL_FEATURE, enableNewObjectModel
 
