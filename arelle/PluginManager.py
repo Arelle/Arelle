d393e51d83e0dc2cbdf1aedbd147c46e6abfe637
Aaron Cameron <aaron.cameron@workiva.com>
2023-11-27 11:24:21 -0700

    Implement EntryPointRef for finding and storing plugin entry points

--- arelle/PluginManager.py
+++ arelle/PluginManager.py
@@ -6,6 +6,7 @@ based on pull request 4
 '''
 from __future__ import annotations
 import os, sys, types, time, ast, importlib, io, json, gettext, traceback
+from dataclasses import dataclass
 from importlib.metadata import entry_points, EntryPoint
 import importlib.util
 import logging
@@ -624,16 +625,13 @@ def addPluginModule(name: str) -> dict[str, Any] | None:
     :param name: The name to search for
     :return: The module information dictionary, if added. Otherwise, None.
     """
-    unloaded_arelle_plugins = discoverPluginEntryPoints(name)
-    if len(unloaded_arelle_plugins) > 1:
-        error_msg = f'Multiple pip installed plugins with name {name} in group arelle.plugin'
-        logPluginTrace(error_msg, logging.ERROR)
-    plugin_module_info = None
-    if unloaded_arelle_plugins:
-        plugin_module_info = entryPointToModuleInfo(unloaded_arelle_plugins[0])
-    if not plugin_module_info or not plugin_module_info.get("name"):
-        plugin_module_info = moduleModuleInfo(name)
-    return addPluginModuleInfo(plugin_module_info)
+    entryPointRef = EntryPointRef.get(name)
+    pluginModuleInfo = None
+    if entryPointRef:
+        pluginModuleInfo = entryPointRef.createModuleInfo()
+    if not pluginModuleInfo or not pluginModuleInfo.get("name"):
+        pluginModuleInfo = moduleModuleInfo(moduleURL=name)
+    return addPluginModuleInfo(pluginModuleInfo)
 
 
 def reloadPluginModule(name):
@@ -712,6 +710,13 @@ def entryPointToModuleInfo(entryPoint: EntryPoint) -> dict:
     """
     pluginUrl = entryPoint.load()
     return moduleModuleInfo(pluginUrl())
+_entryPointRefCache: list[EntryPointRef] | None = None
+_entryPointRefAliasCache: dict[str, list[EntryPointRef]] | None = None
+_entryPointRefSearchTermEndings = [
+    '/__init__.py',
+    '.py'
+    '/'
+]
 
 
 def discoverPluginEntryPoints(name: str | None = None) -> list[EntryPoint]:
@@ -727,3 +732,171 @@ def discoverPluginEntryPoints(name: str | None = None) -> list[EntryPoint]:
         if name is not None:
             matches = matches.select(name=name)
         return list(matches)
+@dataclass
+class EntryPointRef:
+    aliases: set[str]
+    entryPoint: EntryPoint | None
+    moduleFilename: str | None
+    moduleInfo: dict | None
+
+    def createModuleInfo(self) -> dict | None:
+        """
+        Creates a module information dictionary from the entry point ref.
+        :return: A module inforomation dictionary
+        """
+        if self.entryPoint is not None:
+            return moduleModuleInfo(entryPoint=self.entryPoint)
+        return moduleModuleInfo(moduleURL=self.moduleFilename)
+
+    @staticmethod
+    def fromEntryPoint(entryPoint: EntryPoint) -> EntryPointRef | None:
+        """
+        Given an entry point, retrieves the subset of information from __pluginInfo__ necessary to
+        determine if the entry point should be imported as a plugin.
+        :param entryPoint:
+        :return:
+        """
+        pluginUrlFunc = entryPoint.load()
+        pluginUrl = pluginUrlFunc()
+        return EntryPointRef.fromFilepath(pluginUrl, entryPoint)
+
+    @staticmethod
+    def fromFilepath(filepath: str, entryPoint: EntryPoint | None = None) -> EntryPointRef | None:
+        """
+        Given a filepath, retrieves a subset of information from __pluginInfo__ necessary to
+        determine if the entry point should be imported as a plugin.
+        :param filepath: Path to plugin, can be a directory or .py filepath
+        :param entryPoint: Optional entry point information to include in aliases/moduleInfo
+        :return:
+        """
+        moduleFilename = _cntlr.webCache.getfilename(filepath)
+        if moduleFilename:
+            moduleFilename = normalizeModuleFilename(moduleFilename)
+        aliases = set()
+        if entryPoint:
+            aliases.add(entryPoint.name)
+        moduleInfo: dict | None = None
+        if moduleFilename:
+            moduleInfo = parsePluginInfo(moduleFilename, moduleFilename, entryPoint)
+            if moduleInfo is None:
+                return None
+            if "name" in moduleInfo:
+                aliases.add(moduleInfo["name"])
+            if "aliases" in moduleInfo:
+                aliases |= set(moduleInfo["aliases"])
+        return EntryPointRef(
+            aliases={EntryPointRef._normalizePluginSearchTerm(a) for a in aliases},
+            entryPoint=entryPoint,
+            moduleFilename=moduleFilename,
+            moduleInfo=moduleInfo,
+        )
+
+    @staticmethod
+    def discoverAll() -> list[EntryPointRef]:
+        """
+        Retrieve all plugin entry points, cached on first run.
+        :return: List of all discovered entry points.
+        """
+        global _entryPointRefCache
+        if _entryPointRefCache is None:
+            assert _pluginBase is not None
+            _entryPointRefCache = EntryPointRef._discoverBuiltIn([], cast(str, _pluginBase)) + EntryPointRef._discoverInstalled()
+        return _entryPointRefCache
+
+    @staticmethod
+    def _discoverBuiltIn(entryPointRefs: list[EntryPointRef], directory: str) -> list[EntryPointRef]:
+        """
+        Recursively retrieve all plugin entry points in the given directory.
+        :param entryPointRefs: Working list of entry point refs to append to.
+        :param directory: Directory to search for entry points within.
+        :return: List of discovered entry points.
+        """
+        for fileName in sorted(os.listdir(directory)):
+            if fileName in (".", "..", "__pycache__", "__init__.py", ".DS_Store"):
+                continue  # Ignore these entries
+            filePath = os.path.join(directory, fileName)
+            if os.path.isdir(filePath):
+                EntryPointRef._discoverBuiltIn(entryPointRefs, filePath)
+            if os.path.isfile(filePath) and os.path.basename(filePath).endswith(".py"):
+                # If `filePath` references .py file directly, use it
+                moduleFilePath = filePath
+            elif os.path.isdir(filePath) and os.path.exists(initFilePath := os.path.join(filePath, "__init__.py")):
+                # Otherwise, if `filePath` is a directory containing `__init__.py`, use that
+                moduleFilePath = initFilePath
+            else:
+                continue
+            entryPointRef = EntryPointRef.fromFilepath(moduleFilePath)
+            if entryPointRef is not None:
+                entryPointRefs.append(entryPointRef)
+        return entryPointRefs
+
+    @staticmethod
+    def _discoverInstalled() -> list[EntryPointRef]:
+        """
+        Retrieve all installed plugin entry points.
+        :return: List of all discovered entry points.
+        """
+        entryPoints: list[EntryPoint]
+        if sys.version_info < (3, 10):
+            entryPoints = [e for e in entry_points().get('arelle.plugin', [])]
+        else:
+            entryPoints = list(entry_points(group='arelle.plugin'))
+        entryPointRefs = []
+        for entryPoint in entryPoints:
+            entryPointRef = EntryPointRef.fromEntryPoint(entryPoint)
+            if entryPointRef is not None:
+                entryPointRefs.append(entryPointRef)
+        return entryPointRefs
+
+    @staticmethod
+    def get(search: str) -> EntryPointRef | None:
+        """
+        Retrieve an entry point ref with a matching name or alias.
+        May return None of no matches are found.
+        Throws an exception if multiple entry point refs match the search term.
+        :param search: Only retrieve entry point matching the given search text.
+        :return: Matching entry point ref, if found.
+        """
+        entryPointRefs = EntryPointRef.search(search)
+        if len(entryPointRefs) == 0:
+            return None
+        elif len(entryPointRefs) > 1:
+            paths = [r.moduleFilename for r in entryPointRefs]
+            raise Exception(_('Multiple entry points matched search term "{}": {}').format(search, paths))
+        return entryPointRefs[0]
+
+    @staticmethod
+    def _normalizePluginSearchTerm(search: str) -> str:
+        """
+        Normalizes the given search term or searchable text by:
+          Making slashes consistent
+          Removing common endings
+        :param search: Search term or searchable text
+        :return: Normalized string
+        """
+        search = search.replace('\\', '/')
+        while True:
+            for ending in _entryPointRefSearchTermEndings:
+                if search.endswith(ending):
+                    search = search[:-len(ending)]
+                    break
+            return search
+
+    @staticmethod
+    def search(search: str) -> list[EntryPointRef] | None:
+        """
+        Retrieve entry point module information matching provided search text.
+        A map of aliases to matching entry points is cached on the first run.
+        :param search: Only retrieve entry points matching the given search text.
+        :return: List of matching module infos.
+        """
+        global _entryPointRefAliasCache
+        if _entryPointRefAliasCache is None:
+            entryPointRefAliasCache = defaultdict(list)
+            entryPointRefs = EntryPointRef.discoverAll()
+            for entryPointRef in entryPointRefs:
+                for alias in entryPointRef.aliases:
+                    entryPointRefAliasCache[alias].append(entryPointRef)
+            _entryPointRefAliasCache = entryPointRefAliasCache
+        search = EntryPointRef._normalizePluginSearchTerm(search)
+        return _entryPointRefAliasCache.get(search, [])
