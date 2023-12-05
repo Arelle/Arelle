0ebcbcd859c4ab785ba0941169563fb79911dec8
Aaron Cameron <aaron.cameron@workiva.com>
2023-12-05 13:06:32 -0700

    NL: Add cached document=>object maps to plugin data

--- arelle/plugin/validate/NL/PluginValidationDataExtension.py
+++ arelle/plugin/validate/NL/PluginValidationDataExtension.py
@@ -3,9 +3,13 @@ See COPYRIGHT.md for copyright information.
 """
 from __future__ import annotations
 
+from collections import defaultdict
 from dataclasses import dataclass
 
+from arelle.ModelDtsObject import ModelFact
+from arelle.ModelInstanceObject import ModelUnit, ModelContext
 from arelle.ModelValue import QName
+from arelle.ModelXbrl import ModelXbrl
 from arelle.utils.validate.PluginValidationData import PluginValidationData
 
 
@@ -21,3 +25,34 @@ class PluginValidationDataExtension(PluginValidationData):
     documentResubmissionUnsurmountableInaccuraciesQn: QName
     entrypointRoot: str
     entrypoints: set[str]
+
+    _contextsByDocument: dict[str, list[ModelContext]] | None = None
+    _factsByDocument: dict[str, list[ModelFact]] | None = None
+    _unitsByDocument: dict[str, list[ModelUnit]] | None = None
+
+    def contextsByDocument(self, modelXbrl: ModelXbrl) -> dict[str, list[ModelContext]]:
+        if self._contextsByDocument is not None:
+            return self._contextsByDocument
+        contextsByDocument = defaultdict(list)
+        for context in modelXbrl.contexts.values():
+            contextsByDocument[context.modelDocument.filepath].append(context)
+        self._contextsByDocument = dict(contextsByDocument)
+        return self._contextsByDocument
+
+    def factsByDocument(self, modelXbrl: ModelXbrl) -> dict[str, list[ModelFact]]:
+        if self._factsByDocument is not None:
+            return self._factsByDocument
+        factsByDocument = defaultdict(list)
+        for fact in modelXbrl.facts:
+            factsByDocument[fact.modelDocument.filepath].append(fact)
+        self._factsByDocument = dict(factsByDocument)
+        return self._factsByDocument
+
+    def unitsByDocument(self, modelXbrl: ModelXbrl) -> dict[str, list[ModelUnit]]:
+        if self._unitsByDocument is not None:
+            return self._unitsByDocument
+        unitsByDocument = defaultdict(list)
+        for unit in modelXbrl.units.values():
+            unitsByDocument[unit.modelDocument.filepath].append(unit)
+        self._unitsByDocument = dict(unitsByDocument)
+        return self._unitsByDocument
