4b27b61233f2fb86aaed1f28fdfe936a676a8390
Steven Bronson <steven.bronson@workiva.com>
2023-12-18 14:32:54 -0500

    Don't run the same conformance suite multiple times.

--- tests/integration_tests/validation/discover_tests.py
+++ tests/integration_tests/validation/discover_tests.py
@@ -42,7 +42,12 @@ def generate_config_entries(config: ConformanceSuiteConfig) -> Iterable[Entry]:
 
 def main() -> None:
     output: list[Entry] = []
+    config_names_seen: set[str] = set()
     for config in ALL_CONFORMANCE_SUITE_CONFIGS:
+        # configurations don't necessarily have unique names, e.g. malformed UTR
+        if config.name in config_names_seen:
+            continue
+        config_names_seen.add(config.name)
         output.extend(generate_config_entries(config))
     json.dump(output, sys.stdout, indent=4)
     print()
