0f4cf404e60c16168c6c9c42c58eb9d0fb1a5d52
Austin M. Matherne <Austin.Matherne@Workiva.com>
2023-12-21 15:18:24 -0500

    Update calc 1.1 testcase plugin not to ignore warnings

--- tests/plugin/testcaseCalc11ValidateSetup.py
+++ tests/plugin/testcaseCalc11ValidateSetup.py
@@ -1,18 +1,11 @@
-'''
-This plug-in removes xmlns="http://www.w3.org/1999/xhtml" from
-escaped html in text content of expected instance facts for inline XBRL text facts
-
-(c) Copyright 2019 Mark V Systems Limited, All rights reserved.
-'''
-try:
-    import regex as re
-except ImportError:
-    import re
-from arelle.XhtmlValidate import htmlEltUriAttrs, resolveHtmlUri
+"""
+See COPYRIGHT.md for copyright information.
+"""
 from arelle.ValidateXbrlCalcs import ValidateCalcsMode as CalcsMode
+from arelle.Version import authorLabel, copyrightLabel
+
 
 def testcaseVariationLoaded(testInstance, testcaseInstance, modelTestcaseVariation):
-    testInstance.modelManager.formulaOptions.testcaseResultsCaptureWarnings = False
     for result in modelTestcaseVariation.iter("{*}result"):
         for n, v in result.attrib.items():
             if n.endswith("mode"):
@@ -20,20 +13,20 @@ def testcaseVariationLoaded(testInstance, testcaseInstance, modelTestcaseVariati
                     testInstance.modelManager.validateCalcs = CalcsMode.ROUND_TO_NEAREST
                 elif v == "truncate":
                     testInstance.modelManager.validateCalcs = CalcsMode.TRUNCATION
-        if result.tag.endswith("warning"):
-            testInstance.modelManager.formulaOptions.testcaseResultsCaptureWarnings = True
-                    
+
+
 def testcaseVariationExpectedResult(modelTestcaseVariation):
     for result in modelTestcaseVariation.iter("{*}warning"):
         return result.text
+
 
 __pluginInfo__ = {
     'name': 'Testcase obtain expected calc 11 mode from variation/result@mode',
     'version': '0.9',
     'description': "This plug-in removes xxx.  ",
-    'license': 'Apache-2',
-    'author': 'Mark V Systems Limited',
-    'copyright': '(c) Copyright 2019 Mark V Systems Limited, All rights reserved.',
+    'license': "Apache-2",
+    'author': authorLabel,
+    'copyright': copyrightLabel,
     # classes of mount points (required)
     'TestcaseVariation.Xbrl.Loaded': testcaseVariationLoaded,
     'ModelTestcaseVariation.ExpectedResult': testcaseVariationExpectedResult
