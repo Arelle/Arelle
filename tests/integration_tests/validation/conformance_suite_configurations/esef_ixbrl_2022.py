33e21def8fc5d8a1e3c36dbfe62d89c2bfaf53d8
Austin M. Matherne <Austin.Matherne@Workiva.com>
2024-01-17 15:54:09 -0500

    Align esef shard count with CI cores

--- tests/integration_tests/validation/conformance_suite_configurations/esef_ixbrl_2022.py
+++ tests/integration_tests/validation/conformance_suite_configurations/esef_ixbrl_2022.py
@@ -86,5 +86,5 @@ config = ConformanceSuiteConfig(
     name=PurePath(__file__).stem,
     plugins=frozenset({'validate/ESEF'}),
     public_download_url='https://www.esma.europa.eu/sites/default/files/library/esef_conformance_suite_2022.zip',
-    shards=5,
+    shards=4,
 )
