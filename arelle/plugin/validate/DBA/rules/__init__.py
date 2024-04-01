d9653af3bbf20256d9a865cfa190910de1e2f1f2
Aaron Cameron <aaron.cameron@workiva.com>
2024-04-01 10:00:48 -0600

    validate/DBA: Sort facts in getValidFactPairs

--- arelle/plugin/validate/DBA/rules/__init__.py
+++ arelle/plugin/validate/DBA/rules/__init__.py
@@ -13,8 +13,8 @@ from arelle.XmlValidateConst import VALID
 
 
 def getValidFactPairs(modelXbrl: ModelXbrl, qname1: QName, qname2: QName) -> Iterator[tuple[ModelFact, ModelFact]]:
-    facts1: set[ModelFact] = modelXbrl.factsByQname.get(qname1, set())
-    facts2: set[ModelFact] = modelXbrl.factsByQname.get(qname2, set())
+    facts1: list[ModelFact] = sorted(modelXbrl.factsByQname.get(qname1, set()), key=lambda f: f.objectIndex)
+    facts2: list[ModelFact] = sorted(modelXbrl.factsByQname.get(qname2, set()), key=lambda f: f.objectIndex)
     for fact1, fact2 in itertools.product(facts1, facts2):
         if fact1.xValid < VALID or fact2.xValid < VALID:
             continue
