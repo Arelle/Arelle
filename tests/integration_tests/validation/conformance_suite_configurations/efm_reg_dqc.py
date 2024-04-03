d61b66af05279d469ce4570cdbe6802a4f6248b7
Aaron Cameron <aaron.cameron@workiva.com>
2024-04-03 13:49:21 -0600

    Add EFM regression conformance suite configs

---
+++ tests/integration_tests/validation/conformance_suite_configurations/efm_reg_dqc.py
@@ -0,0 +1,24 @@
+from pathlib import PurePath, Path
+from tests.integration_tests.validation.conformance_suite_config import ConformanceSuiteConfig, ConformanceSuiteAssetConfig, AssetSource
+
+
+config = ConformanceSuiteConfig(
+    args=[
+        '--disclosureSystem', 'efm-pragmatic',
+        '--formula', 'run',
+    ],
+    assets=[
+        ConformanceSuiteAssetConfig.conformance_suite(
+            Path('efm_reg_dqc.zip'),
+            entry_point=Path('index.xml'),
+            source=AssetSource.S3_PUBLIC,
+        )
+    ],
+    cache_version_id='FR1AEVo5AdJcSAoSphxQbpGVsMLmXvIF',
+    ci_enabled=False,
+    info_url='N/A',
+    name=PurePath(__file__).stem,
+    plugins=frozenset({'validate/EFM', 'inlineXbrlDocumentSet'}),
+    test_case_result_options='match-any',
+    shards=64,
+)
