536eaccafa4b1cbac585614738a8511b813a411a
Aaron Cameron <aaron.cameron@workiva.com>
2024-07-16 10:38:12 -0600

    Add ability to configure logging filters

--- arelle/api/Session.py
+++ arelle/api/Session.py
@@ -79,6 +79,7 @@ class Session:
         sourceZipStream: BinaryIO | None = None,
         responseZipStream: BinaryIO | None = None,
         logHandler: logging.Handler | None = None,
+        logFilters: list[logging.Filter] | None = None,
     ) -> bool:
         """
         Perform a run using the given options.
@@ -111,6 +112,7 @@ class Session:
             if not self._cntlr.logger:
                 self._cntlr.startLogging(
                     logFileName='logToBuffer',
+                    logFilters=logFilters,
                     logHandler=logHandler,
                     logTextMaxLength=options.logTextMaxLength,
                     logRefObjectProperties=logRefObjectProperties,
@@ -126,6 +128,7 @@ class Session:
                     logFileMode=options.logFileMode,
                     logFormat=(options.logFormat or "[%(messageCode)s] %(message)s - %(file)s"),
                     logLevel=(options.logLevel or "DEBUG"),
+                    logFilters=logFilters,
                     logHandler=logHandler,
                     logToBuffer=options.logFile == 'logToBuffer',
                     logTextMaxLength=options.logTextMaxLength,  # e.g., used by EdgarRenderer to require buffered logging
