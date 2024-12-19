ec6f406e20b5bb6479ec89ac0227e1be0a980631
Austin M. Matherne <Austin.Matherne@Workiva.com>
2024-12-19 13:31:54 -0500

    Update XBRL 2.1 conformance suite

--- tests/integration_tests/validation/conformance_suite_configurations/xbrl_2_1.py
+++ tests/integration_tests/validation/conformance_suite_configurations/xbrl_2_1.py
@@ -1,5 +1,10 @@
-from pathlib import PurePath, Path
-from tests.integration_tests.validation.conformance_suite_config import ConformanceSuiteConfig, ConformanceSuiteAssetConfig, AssetSource
+from pathlib import Path, PurePath
+
+from tests.integration_tests.validation.conformance_suite_config import (
+    AssetSource,
+    ConformanceSuiteAssetConfig,
+    ConformanceSuiteConfig,
+)
 
 config = ConformanceSuiteConfig(
     args=[
@@ -7,19 +12,24 @@ config = ConformanceSuiteConfig(
     ],
     assets=[
         ConformanceSuiteAssetConfig.conformance_suite(
-            Path('XBRL-CONF-2014-12-10.zip'),
-            entry_point=Path('XBRL-CONF-2014-12-10/xbrl.xml'),
-            public_download_url='https://www.xbrl.org/2014/XBRL-CONF-2014-12-10.zip',
+            Path('XBRL-CONF-2024-12-17.zip'),
+            entry_point=Path('XBRL-CONF-2024-12-17/xbrl.xml'),
+            public_download_url='https://www.xbrl.org/2014/XBRL-CONF-2024-12-17.zip',
             source=AssetSource.S3_PUBLIC,
         ),
     ],
-    expected_failure_ids=frozenset(f'XBRL-CONF-2014-12-10/Common/{s}' for s in [
+    expected_failure_ids=frozenset(f'XBRL-CONF-2024-12-17/Common/{s}' for s in [
         # 202.02b in the absence of source/target constraints, an empty href doesn't pose a problem
         # 202-02b-HrefResolutionCounterExample-custom.xml Expected: valid, Actual: arelle:hrefWarning
         '200-linkbase/202-xlinkLocator.xml:V-02b',
     ]),
+    expected_missing_testcases=frozenset([f"XBRL-CONF-2024-12-17/Common/{s}" for s in [
+        "related-standards/xlink/arc-duplication/arc-duplication-testcase.xml",
+        "related-standards/xml-schema/uniqueParticleAttribution/uniqueParticleAttribution-testcase.xml",
+    ]]),
     info_url='https://specifications.xbrl.org/work-product-index-group-base-spec-base-spec.html',
     name=PurePath(__file__).stem,
     network_or_cache_required=False,
+    strict_testcase_index=False,
     shards=3,
 )
