d47ed63038d0713b6ab44af9717be2eb734fe383
Austin M. Matherne <Austin.Matherne@Workiva.com>
2024-12-20 13:12:03 -0500

    Remove expected table linkbase failure

--- tests/integration_tests/validation/conformance_suite_configurations/xbrl_table_linkbase_1_0.py
+++ tests/integration_tests/validation/conformance_suite_configurations/xbrl_table_linkbase_1_0.py
@@ -15,15 +15,6 @@ config = ConformanceSuiteConfig(
             source=AssetSource.S3_PUBLIC,
         ),
     ],
-    expected_additional_testcase_errors={f'table-linkbase-conformance-2024-12-17/tests/{s}': val for s, val in {
-        # 2024-12-17 the expected output of this test case changed.
-        # The test case source hasn't been updated since 2014, but changes weren't published until now.
-        '0100-table/0170-parent-child-order/0170-parent-child-order-testcase.xml:v-01': frozenset({
-            'arelle:tableModelAttributesMismatch',
-            'arelle:tableModelConstraintsMismatch',
-            'arelle:tableModelElementMismatch',
-        }),
-    }.items()},
     info_url='https://specifications.xbrl.org/work-product-index-table-linkbase-table-linkbase-1.0.html',
     name=PurePath(__file__).stem,
     network_or_cache_required=False,
