d15a01472d9254438e94b207c2b0d9fb6c716a0a
Stuart Rowan <stuart.rowan@strr.org>
2025-03-03 10:03:24 +0000

    Tidy up new tests and comments in them
    Also test that newer unicode space characters such as em space are ignored as required.

--- tests/unit_tests/arelle/test_xmlutil.py
+++ tests/unit_tests/arelle/test_xmlutil.py
@@ -39,9 +39,14 @@ REPLACE_WHITESPACE_TESTS = [
     ("\n", " "),
     ("\r", " "),
     ("\t", " "),
+    # literal or entity \v and \f are illegal in XML but we shouldn't be touching them
     ("\r\v\t", " \v "),
+    ("\r\f\t", " \f "),
     ("\r\t\n", "   "),
     ("\t\t \n\n \r\r", " " * 8),
+    # Python's whitespace (\s, str.isspace) definition includes em space (U+2003)
+    # but XSD replace does not so we shouldn't be touching it
+    ("1\u2003", "1\u2003"),
     (" m u s h r o o m  ", " m u s h r o o m  "),
     (" m u s h\tr o o m  ", " m u s h r o o m  "),
 ]
@@ -50,7 +55,7 @@ REPLACE_WHITESPACE_TESTS = [
 @pytest.mark.parametrize("value, expected", REPLACE_WHITESPACE_TESTS)
 def test_replaceWhitespace(value, expected):
     result = replaceWhitespace(value)
-    assert result == expected, f"""Input: "{repr(value)}"; Output: "{repr(result)}"; Wanted: "{repr(expected)}" """
+    assert result == expected
 
 
 COLLAPSE_WHITESPACE_TESTS = [
@@ -65,17 +70,18 @@ COLLAPSE_WHITESPACE_TESTS = [
     (" " * 10 + "1  1" + " " * 10, "1 1"),
     ("\r \n \t", ""),
     ("\r \n \t", ""),
+    # literal or entity \v and \f are illegal in XML but we shouldn't be touching them
     ("\r\v\t", "\v"),
-    ("\r \f \t", "\f"),
-    ("\v\f", "\v\f"),
-    ("\v  \f", "\v \f"),
-    ("\n \f \v \f \n", "\f \v \f"),
+    ("\r\f\t", "\f"),
     (" x  xm   xml    xmln     ", "x xm xml xmln"),
-    ("time: \n\tround\n\ttuit  \r\n", "time: round tuit")
+    ("time: \n\tround\n\ttuit  \r\n", "time: round tuit"),
+    # Python's whitespace (\s, str.strip) definition includes em space (U+2003)
+    # but XSD collapse does not so we shouldn't be touching it
+    (" \u2003 \u2003  ", "\u2003 \u2003"),
 ]
 
 
 @pytest.mark.parametrize("value, expected", COLLAPSE_WHITESPACE_TESTS)
 def test_collapseWhitespace(value, expected):
     result = collapseWhitespace(value)
-    assert result == expected, f"""Input: "{repr(value)}"; Output: "{repr(result)}"; Wanted: "{repr(expected)}" """
+    assert result == expected
