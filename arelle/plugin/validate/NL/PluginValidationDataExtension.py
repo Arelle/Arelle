ccb57632c8468009f799d64320b3e33bdb70510a
Aaron Cameron <aaron.cameron@workiva.com>
2025-06-02 15:27:22 -0600

    NL: Helper function for iterating over a linkbase arcs

--- arelle/plugin/validate/NL/PluginValidationDataExtension.py
+++ arelle/plugin/validate/NL/PluginValidationDataExtension.py
@@ -7,11 +7,12 @@ from collections import defaultdict
 from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
-from typing import Any, TYPE_CHECKING, cast
+from typing import Any, TYPE_CHECKING, cast, Iterable
 
 import regex as re
 from lxml.etree import _Comment, _ElementTree, _Entity, _ProcessingInstruction, _Element
 
+from arelle import XbrlConst
 from arelle.FunctionIxt import ixtNamespaces
 from arelle.ModelDocument import ModelDocument, Type as ModelDocumentType
 from arelle.ModelDtsObject import ModelConcept
@@ -115,6 +116,33 @@ class ExtensionDocumentData:
     hrefXlinkRole: str | None
     linkbases: list[LinkbaseData]
 
+    def iterArcsByType(
+            self,
+            linkbaseType: LinkbaseType,
+            includeArcroles: set[QName] | None = None,
+            excludeArcroles: set[QName] | None = None,
+    ) -> Iterable[_Element]:
+        """
+        Returns a list of LinkbaseData objects for the specified LinkbaseType.
+        """
+        for linkbase in self.iterLinkbasesByType(linkbaseType):
+            for arc in linkbase.arcs:
+                if includeArcroles is not None:
+                    if arc.get(XbrlConst.qnXlinkArcRole.clarkNotation) not in includeArcroles:
+                        continue
+                if excludeArcroles is not None:
+                    if arc.get(XbrlConst.qnXlinkArcRole.clarkNotation) in excludeArcroles:
+                        continue
+                yield arc
+
+    def iterLinkbasesByType(self, linkbaseType: LinkbaseType) -> Iterable[LinkbaseData]:
+        """
+        Returns a list of LinkbaseData objects for the specified LinkbaseType.
+        """
+        for linkbase in self.linkbases:
+            if linkbase.linkbaseType == linkbaseType:
+                yield linkbase
+
 
 @dataclass(frozen=True)
 class HiddenElementsData:
@@ -136,10 +164,14 @@ class InlineHTMLData:
 
 @dataclass(frozen=True)
 class LinkbaseData:
+    arcs: list[_Element]
     basename: str
     element: _Element
-    hasArcs: bool
     linkbaseType: LinkbaseType | None
+
+    @property
+    def hasArcs(self) -> bool:
+        return len(self.arcs) > 0
 
 
 @dataclass
@@ -446,9 +478,9 @@ class PluginValidationDataExtension(PluginData):
                 arcQn = linkbaseType.getArcQn()
                 arcs = list(linkElt.iterdescendants(tag=arcQn.clarkNotation))
                 linkbases.append(LinkbaseData(
+                    arcs=arcs,
                     basename=modelDocument.basename,
                     element=linkElt,
-                    hasArcs=len(arcs) > 0,
                     linkbaseType=linkbaseType,
                 ))
         return linkbases
