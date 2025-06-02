a66a078d8d6c4067d3055f4d5ce9c71249a2bb0c
Aaron Cameron <aaron.cameron@workiva.com>
2025-06-02 11:06:53 -0600

    Update NL conformance suite configuration

--- tests/integration_tests/validation/conformance_suite_configurations/nl_inline_2024.py
+++ tests/integration_tests/validation/conformance_suite_configurations/nl_inline_2024.py
@@ -19,11 +19,6 @@ config = ConformanceSuiteConfig(
         *NL_PACKAGES['NL-INLINE-2024'],
     ],
     expected_additional_testcase_errors={f"conformance-suite-2024-sbr-domein-handelsregister/tests/{s}": val for s, val in {
-        'G3-5-3_1/index.xml:TC2_invalid': {
-            'arelle:ixdsTargetNotDefined': 1,
-            # This test is looking at the usage of the target attribute and does not import the correct taxonomy urls
-            'requiredEntryPointNotImported': 1,
-        },
         'G3-6-3_3/index.xml:TC2_invalid': {
             # Testcase expects only 3.6.3.3, but has a filename that has invalid characters (3.6.3.3)
             # AND {base} with > 20 characters (3.6.3.1)
@@ -38,11 +33,19 @@ config = ConformanceSuiteConfig(
             'taggedTextFactOnlyInLanguagesOtherThanLanguageOfAReport': 5,
         },
         'G5-1-3_1/index.xml:TC1_valid': {
+            'extensionTaxonomyWrongFilesStructure': 1,
             # This test is looking at the import of the Other GAAP entry point and thus does not import
             # the standard GAAP or IFRS
             'requiredEntryPointNotImported': 1,
         },
+        'G3-5-3_1/index.xml:TC2_invalid': {
+            'arelle:ixdsTargetNotDefined': 1,
+            'extensionTaxonomyWrongFilesStructure': 1,
+            # This test is looking at the usage of the target attribute and does not import the correct taxonomy urls
+            'requiredEntryPointNotImported': 1,
+        },
         'G5-1-3_2/index.xml:TC1_valid': {
+            'extensionTaxonomyWrongFilesStructure': 1,
             # This test is looking at the import of the Other GAAP entry point and thus does not import
             # the standard GAAP or IFRS
             'requiredEntryPointNotImported': 1,
@@ -85,13 +88,6 @@ config = ConformanceSuiteConfig(
         'conformance-suite-2024-sbr-domein-handelsregister/tests/G3-5-1_5/index.xml:TC2_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/G3-5-1_5/index.xml:TC3_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/G3-6-2_1/index.xml:TC2_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_1/index.xml:TC3_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_1/index.xml:TC4_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_1/index.xml:TC5_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_1/index.xml:TC6_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_1/index.xml:TC7_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_2/index.xml:TC2_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-1_2/index.xml:TC3_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-1-2_2/index.xml:TC2_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-2-0_1/index.xml:TC2_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/G4-2-0_2/index.xml:TC2_invalid',
@@ -130,7 +126,6 @@ config = ConformanceSuiteConfig(
         'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_4_3/index.xml:TC5_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_5/index.xml:TC2_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_5/index.xml:TC3_invalid',
-        'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_6/index.xml:TC3_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_6/index.xml:TC4_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_8_G4-4-5/index.xml:TC2_invalid',
         'conformance-suite-2024-sbr-domein-handelsregister/tests/RTS_Annex_IV_Par_8_G4-4-5/index.xml:TC3_invalid',
