b9c5f03559ac13a8340d12051dbc9b31dc60678c
Steven Bronson <steven.bronson@workiva.com>
2025-07-22 21:29:04 -0400

    Add context and unit comparison utilities.

---
+++ arelle/utils/Equivalence.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    TypeVar,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+    from arelle.ModelXbrl import ModelXbrl
+
+T = TypeVar('T')
+K = TypeVar('K')
+
+def partitionIntoEquivalenceClasses(items: Iterable[T], key: Callable[[T], K]) -> dict[K, tuple[T, ...]]:
+    d = defaultdict(list)
+    for item in items:
+        d[key(item)].append(item)
+    return {k: tuple(v) for k, v in d.items()}
+
+def getDuplicateItemPairs(modelXbrl: ModelXbrl, partition: Callable[[ModelXbrl], dict[Any, tuple[T, ...]]]) -> list[tuple[T, T]]:
+    return [
+        (context, exemplar)
+        for exemplar, *contexts in partition(modelXbrl).values()
+        for context in contexts
+    ]
