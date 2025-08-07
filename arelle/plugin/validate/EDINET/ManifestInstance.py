ec0a108e9375325ddb138852a0465933b80ec552
Aaron Cameron <aaron.cameron@workiva.com>
2025-08-07 09:19:06 -0600

    EDINET.EC5800E.FATAL_ERROR_INVALID_MANIFEST

--- arelle/plugin/validate/EDINET/ManifestInstance.py
+++ arelle/plugin/validate/EDINET/ManifestInstance.py
@@ -3,19 +3,25 @@ See COPYRIGHT.md for copyright information.
 """
 from __future__ import annotations
 
+import traceback
 import zipfile
 from collections import defaultdict
 from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
+from typing import cast
 
 from lxml import etree
 from lxml.etree import _Element
 
 from arelle import XbrlConst
+from arelle.Cntlr import Cntlr
 from arelle.FileSource import FileSource
 from arelle.ModelValue import QName, qname
+from arelle.typing import TypeGetText
 from . import Constants
+
+_: TypeGetText
 
 
 @dataclass(frozen=True)
@@ -39,6 +45,17 @@ class ManifestTocItem:
     ref: str
     start: QName | None
     end: QName | None
+
+
+def _invalidManifestError(cntlr: Cntlr, file: str, message: str | None = None) -> None:
+    if message is None:
+        message = _("Please correct the content.")
+    cntlr.error(
+        "EDINET.EC5800E.FATAL_ERROR_INVALID_MANIFEST",
+        _("The manifest file definition is incorrect <file=%(file)s>. %(message)s"),
+        file=file,
+        message=message,
+    )
 
 
 def _parseManifestTocItems(parentElt: _Element, parentQName: QName | None) -> list[ManifestTocItem]:
@@ -66,7 +83,7 @@ def _parseManifestDoc(xmlRootElement: _Element, path: Path) -> list[ManifestInst
     titlesByLang = {}
     base = path.parent
     tocElts = list(xmlRootElement.iterchildren(tag=Constants.qnEdinetManifestTocComposition.clarkNotation))
-    assert len(tocElts) == 1, 'There should be exactly one tocComposition element in the manifest.'
+    assert len(tocElts) == 1, _('There should be exactly one tocComposition element in the manifest.')
     for titleElt in tocElts[0].iterchildren(tag=Constants.qnEdinetManifestTitle.clarkNotation):
         lang = titleElt.attrib.get(XbrlConst.qnXmlLang.clarkNotation, "")
         titlesByLang[lang] = titleElt.text.strip() if titleElt.text else ""
@@ -75,7 +92,7 @@ def _parseManifestDoc(xmlRootElement: _Element, path: Path) -> list[ManifestInst
     for tocItem in tocItems:
         tocItemsByRef[tocItem.ref].append(tocItem)
     listElts = list(xmlRootElement.iterchildren(tag=Constants.qnEdinetManifestList.clarkNotation))
-    assert len(listElts) == 1, 'There should be exactly one list element in the manifest.'
+    assert len(listElts) == 1, _('There should be exactly one list element in the manifest.')
     for instanceElt in listElts[0].iterchildren(tag=Constants.qnEdinetManifestInstance.clarkNotation):
         instanceId = str(instanceElt.attrib.get("id", ""))
         instanceType = str(instanceElt.attrib.get("type", ""))
@@ -99,27 +116,54 @@ def _parseManifestDoc(xmlRootElement: _Element, path: Path) -> list[ManifestInst
 
 @lru_cache(1)
 def parseManifests(filesource: FileSource) -> list[ManifestInstance]:
+    assert filesource.cntlr is not None, "FileSource controller must be set before parsing manifests."
+    cntlr = filesource.cntlr
     instances: list[ManifestInstance] = []
     if filesource.isArchive:
         if filesource.isTaxonomyPackage:
             return instances
         if filesource.reportPackage is not None:
             return instances
-        for _archiveFile in (filesource.dir or ()):
-            if not Path(_archiveFile).stem.startswith('manifest'):
-                continue
-            assert isinstance(filesource.fs, zipfile.ZipFile), \
-                "The EDINET plugin only supports archives in .zip format."
-            with filesource.fs.open(_archiveFile) as manifestDoc:
-                xmlRootElement = etree.fromstring(manifestDoc.read())
-                instances.extend(_parseManifestDoc(xmlRootElement, Path(_archiveFile)))
+        if not isinstance(filesource.fs, zipfile.ZipFile):
+            _invalidManifestError(
+                cntlr,
+                cast(str, filesource.url),
+                _("The EDINET plugin only supports archives in .zip format.")
+            )
+        else:
+            for _archiveFile in (filesource.dir or ()):
+                if not Path(_archiveFile).stem.startswith('manifest'):
+                    continue
+                try:
+                    with filesource.fs.open(_archiveFile) as manifestDoc:
+                        xmlRootElement = etree.fromstring(manifestDoc.read())
+                        instances.extend(_parseManifestDoc(xmlRootElement, Path(_archiveFile)))
+                except AssertionError as err:
+                    _invalidManifestError(cntlr, str(_archiveFile), str(err))
+                except Exception as err:
+                    _invalidManifestError(cntlr, str(_archiveFile))
+                    cntlr.addToLog(_("[Exception] Failed to parse manifest \"{0}\": \n{1} \n{2}").format(
+                        str(_archiveFile),
+                        err,
+                        traceback.format_exc()
+                    ))
     elif (dirpath := Path(str(filesource.url))).is_dir():
         for file in dirpath.rglob("*"):
             if not file.is_file():
                 continue
             if not file.stem.startswith('manifest'):
                 continue
-            with open(file, 'rb') as manifestDoc:
-                xmlRootElement = etree.fromstring(manifestDoc.read())
-                instances.extend(_parseManifestDoc(xmlRootElement, file))
+            try:
+                with open(file, 'rb') as manifestDoc:
+                    xmlRootElement = etree.fromstring(manifestDoc.read())
+                    instances.extend(_parseManifestDoc(xmlRootElement, file))
+            except AssertionError as err:
+                _invalidManifestError(cntlr, str(file), str(err))
+            except Exception as err:
+                _invalidManifestError(cntlr, str(file))
+                cntlr.addToLog(_("[Exception] Failed to parse manifest \"{0}\": \n{1} \n{2}").format(
+                    str(file),
+                    err,
+                    traceback.format_exc()
+                ))
     return instances
