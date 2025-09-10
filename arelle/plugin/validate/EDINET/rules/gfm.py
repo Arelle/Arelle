459525f418659214d8f40504c076bdc065a8dfa2
Derek Gengenbacher <derek.gengenbacher@workiva.com>
2025-09-10 14:23:42 -0600

    Implement GFM.1.5.7 and 1.5.8

--- arelle/plugin/validate/EDINET/rules/gfm.py
+++ arelle/plugin/validate/EDINET/rules/gfm.py
@@ -32,6 +32,9 @@ from ..PluginValidationDataExtension import PluginValidationDataExtension
 
 _: TypeGetText
 
+BEGINNING_AND_ENDING_WHITE_SPACE = regex.compile(r'^\s|\s$')
+DISALLOWED_LABEL_WHITE_SPACE_CHARACTERS = regex.compile(r'\s{2,}')
+DISALLOWED_LABEL_CHARACTERS = regex.compile(r'<')
 GFM_CONTEXT_DATE_PATTERN = regex.compile(r"^[12][0-9]{3}-[01][0-9]-[0-3][0-9]$")
 GFM_RECOMMENDED_NAMESPACE_PREFIXES = {
     XbrlConst.xbrli: ("xbrli",),
@@ -686,16 +689,90 @@ def rule_gfm_1_5_6(
     EDINET.EC5700W: [GFM 1.5.6] The length of a label must be less than 511 characters unless it its role is documentation.
     """
     labelRelationshipSet = val.modelXbrl.relationshipSet(XbrlConst.conceptLabel)
-    if labelRelationshipSet is not None:
-        for concept in val.modelXbrl.qnameConcepts.values():
-            labelRels = labelRelationshipSet.fromModelObject(concept)
-            for rel in labelRels:
-                label = rel.toModelObject
-                if label.role != XbrlConst.documentationLabel and label.viewText() is not None and len(label.viewText()) > 511:
+    if labelRelationshipSet is None:
+        return
+    for concept in val.modelXbrl.qnameConcepts.values():
+        labelRels = labelRelationshipSet.fromModelObject(concept)
+        for rel in labelRels:
+            label = rel.toModelObject
+            if label.role != XbrlConst.documentationLabel and label.viewText() is not None and len(label.viewText()) > 511:
+                yield Validation.warning(
+                    codes='EDINET.EC5700W.GFM.1.5.6',
+                    msg=_("The concept of '%(concept)s' has a label classified as '%(role)s' that is longer than 511 characters: %(label)s"),
+                    concept=concept.qname,
+                    role=label.role,
+                    label=label.viewText(),
+                    modelObject=label
+                )
+
+
+@validation(
+    hook=ValidationHook.XBRL_FINALLY,
+    disclosureSystems=[DISCLOSURE_SYSTEM_EDINET],
+)
+def rule_gfm_1_5_7(
+        pluginData: PluginValidationDataExtension,
+        val: ValidateXbrl,
+        *args: Any,
+        **kwargs: Any,
+) -> Iterable[Validation]:
+    """
+    EDINET.EC5700W: [GFM 1.5.7] A label cannot contain the "<" character or consecutive white space characters including
+                    but not limited to: space, carriage return, line feed or tab.
+    """
+    labelRelationshipSet = val.modelXbrl.relationshipSet(XbrlConst.conceptLabel)
+    if labelRelationshipSet is None:
+        return
+    for concept in val.modelXbrl.qnameConcepts.values():
+        labelRels = labelRelationshipSet.fromModelObject(concept)
+        for rel in labelRels:
+            label = rel.toModelObject
+            if label.role != XbrlConst.documentationLabel and label.viewText() is not None:
+                if DISALLOWED_LABEL_CHARACTERS.match(label.viewText()):
                     yield Validation.warning(
-                        codes='EDINET.EC5700W.GFM.1.5.6',
-                        msg=_("The concept of \"{concept}\" has a label classified as \"{role}\" that is longer than 511 characters: {label}".format(
-                            concept=concept.qname, role=label.role, label=label.viewText()
-                        )),
+                        codes='EDINET.EC5700W.GFM.1.5.7',
+                        msg=_("The concept of '%(concept)s' has a label classified as '%(role)s that contains the '<' character: %(label)s"),
+                        concept=concept.qname,
+                        role=label.role,
+                        label=label.viewText(),
                         modelObject=label
                     )
+                elif DISALLOWED_LABEL_WHITE_SPACE_CHARACTERS.match(label.viewText()):
+                    yield Validation.warning(
+                        codes='EDINET.EC5700W.GFM.1.5.7',
+                        msg=_("The concept of '%(concept)s' has a label classified as '%(role)s' that contains consecutive white space characters: %(label)s"),
+                        concept=concept.qname,
+                        role=label.role,
+                        label=label.viewText(),
+                        modelObject=label
+                    )
+
+
+@validation(
+    hook=ValidationHook.XBRL_FINALLY,
+    disclosureSystems=[DISCLOSURE_SYSTEM_EDINET],
+)
+def rule_gfm_1_5_8(
+        pluginData: PluginValidationDataExtension,
+        val: ValidateXbrl,
+        *args: Any,
+        **kwargs: Any,
+) -> Iterable[Validation]:
+    """
+    EDINET.EC5700W: [GFM 1.5.8] A label should not begin or end with a white space character
+    """
+    labelRelationshipSet = val.modelXbrl.relationshipSet(XbrlConst.conceptLabel)
+    if labelRelationshipSet is None:
+        return
+    for concept in val.modelXbrl.qnameConcepts.values():
+        labelRels = labelRelationshipSet.fromModelObject(concept)
+        for rel in labelRels:
+            label = rel.toModelObject
+            if label.viewText() is not None and BEGINNING_AND_ENDING_WHITE_SPACE.match(label.viewText()):
+                yield Validation.warning(
+                    codes='EDINET.EC5700W.GFM.1.5.6',
+                    msg=_("The concept of '%(concept)s' has a label that contains disallowed white space either at the begining or the end: %(label)s"),
+                    concept=concept.qname,
+                    label=label.viewText(),
+                    modelObject=label
+                )
