450e9b7a2fe081bbbe624f107bd06f2eee5fe219
Aaron Cameron <aaron.cameron@workiva.com>
2025-09-12 13:10:55 -0600

    EDINET: Correct testcase filings affected by cover page validations

--- tests/integration_tests/validation/conformance_suite_configurations/edinet.py
+++ tests/integration_tests/validation/conformance_suite_configurations/edinet.py
@@ -16,9 +16,14 @@ config = ConformanceSuiteConfig(
     ],
     cache_version_id='cs2wODrDheJqDIm1kEU4Qwk8jwd7DfQu',
     expected_additional_testcase_errors={f"*{s}": val for s, val in {
+        # EDINET.EC5700W.GFM.1.1.3: valid05.zip (and testcases built from it) references
+        # non-existent and non-standard "http://www.xbrl.org/2003/xbrl-instance-2003-09-30.xsd".
         # EDINET.EC8027W: Some of our "valid" documents define presentation and/or definition
         # links with multiple root elements. Keeping these out of the conformance suite
         # until we are more confident in our interpretation of the EDINET rule.
+        "EC5700W.GFM.1.2.13/index.xml:invalid01": {
+            "EDINET.EC5700W.GFM.1.1.3": 1,
+        },
         "EC8024E/index.xml:invalid01": {
             "EDINET.EC8027W": 1,
         },
@@ -27,6 +32,9 @@ config = ConformanceSuiteConfig(
         },
         "EC8062W/index.xml:invalid01": {
             "EDINET.EC8027W": 1,
+        },
+        "EC8075W/index.xml:invalid01": {
+            "EDINET.EC8027W": 2,
         },
         "valid/index.xml:valid01": {
             "EDINET.EC8027W": 2,
