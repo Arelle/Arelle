e3ae44536559e5e3bcb3dcccf0d4451c7b24b15e
Derek Gengenbacher <derek.gengenbacher@workiva.com>
2025-10-01 14:54:05 -0600

    Add GFM.1.8.5

--- tests/integration_tests/validation/conformance_suite_configurations/edinet.py
+++ tests/integration_tests/validation/conformance_suite_configurations/edinet.py
@@ -126,6 +126,11 @@ ADDITIONAL_INVALID_ERRORS = {
         # Modified version of valid03
         "EDINET.EC2002W": 1,
     },
+    "EC5700W.GFM.1.8.5/index.xml:invalid01": {
+        # Modified version of valid03
+        "EDINET.EC2002W": 1,
+        "EDINET.EC5700W.GFM.1.8.4": 1
+    },
     "EC5806E/index.xml:invalid01": {
         # Instance duplicated means table of contents are included twice.
         "EDINET.EC2005E": 2,
