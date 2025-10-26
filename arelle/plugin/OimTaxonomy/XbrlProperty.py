6ca3a87f004fab84d0df7858644a10a8ce6224ea
Herman Fischer <fischer@markv.com>
2025-10-26 11:35:32 -0700

    remove property defaultValue, rewrite jsonschema allOf Not for fastjsonschema

--- arelle/plugin/OimTaxonomy/XbrlProperty.py
+++ arelle/plugin/OimTaxonomy/XbrlProperty.py
@@ -22,7 +22,6 @@ class XbrlPropertyType(XbrlReferencableTaxonomyObject):
     taxonomy: XbrlTaxonomyModuleType
     name: QNameKeyType # (required) The name is a QName that uniquely identifies the property type object.
     dataType: QName # (required) Indicates the dataType of the property value. These are provided as a QName based on the datatypes specified in the XBRL 2.1 specification and any custom datatype defined in the taxonomy.
-    defaultValue: Optional[str] # (optional) Default value
     enumerationDomain: Optional[QName] # (optional) Used to specify the QName of a domain object that is used to derive enumerated domain members QNames that can be used for the property.
     definitional: Union[bool, DefaultFalse] # (optional) Indicates if the property is definitional. If changes to the property change the meaning of the object it is definitional, if the property provides extra information about the object it is not definitional. If no value is provided the attribute defaults to false.
     allowedObjects: set[QName] # (optional) List of allowable objects that the property can be used with. For example the balance property can only be used with concept objects.
