c9be87dc17fcb80fe909c172289ce9e1074637ec
Herman Fischer <fischer@markv.com>
2025-11-08 16:16:14 -0800

    add error code ESEF.2.2.8.duplicatedIdAttribute for dup IDs already being detected

--- arelle/plugin/validate/ESEF/__init__.py
+++ arelle/plugin/validate/ESEF/__init__.py
@@ -64,6 +64,7 @@ _: TypeGetText
 ESEF_DISCLOSURE_SYSTEM_TEST_PROPERTY = "ESEFplugin"
 
 ixErrorPattern = re.compile(r"ix11[.]|xmlSchema[:]|(?!xbrl.5.2.5.2|xbrl.5.2.6.2)xbrl[.]|xbrld[ti]e[:]|utre[:]")
+dupIdErrorPattern = re.compile(r"xml.3.3.1:idMustBeUnique|ix11.14.1.2:uniqueIxId")
 
 
 def validateEntity(modelXbrl: ModelXbrl, filename:str, filesource: FileSource) -> None:
@@ -305,6 +306,14 @@ class ESEFPlugin(PluginHooks):
                             _("RTS on ESEF requires inline XBRL, no facts were reported."),
                             modelObject=modelXbrl)
             return # never loaded properly
+
+        disclosureSystemYear = getDisclosureSystemYear(modelXbrl)
+        if disclosureSystemYear >= 2025:
+            numDupIdErrors = sum(dupIdErrorPattern.match(e) is not None for e in modelXbrl.errors if isinstance(e,str))
+            if numDupIdErrors:
+                modelXbrl.warning("ESEF.2.2.8.duplicatedIdAttribute",
+                                _("ID attributes should be unique, %(numDupIdErrors)s such errors were reported."),
+                                modelObject=modelXbrl, numDupIdErrors=numDupIdErrors)
 
         numXbrlErrors = sum(ixErrorPattern.match(e) is not None for e in modelXbrl.errors if isinstance(e,str))
         if numXbrlErrors:
