85a0df2110d60df2a565cafe2439dfda1c7c985a
Aaron Cameron <aaron.cameron@workiva.com>
2025-11-17 11:17:28 -0700

    *formula

--- tests/integration_tests/validation/conformance_suite_configurations/xbrl_formula_1_0.py
+++ tests/integration_tests/validation/conformance_suite_configurations/xbrl_formula_1_0.py
@@ -8,7 +8,8 @@ config = ConformanceSuiteConfig(
             entry_point=Path('formula/tests/index.xml'),
         ),
     ],
-    expected_failure_ids=frozenset([
+    expected_failure_ids=frozenset(f'formula/tests/{s}' for s in [
+        '10000 Formula/12061-Formula-Processing-DimensionRules/12061 Dimension Rules.xml:V-13',
         '40000 Filters/48210-PeriodFilter-Processing-Period/48210 PeriodFilter Processing Period.xml:V-04',
     ]),
     info_url='https://specifications.xbrl.org/release-history-formula-1.0-formula-conf.html',
