0701560dd7a8644d7b7e4e8fae51a09070a5d670
Derek Gengenbacher <derek.gengenbacher@workiva.com>
2025-12-19 09:08:24 -0700

    Convert dba_multi to dba_multi_2024

--- tests/integration_tests/validation/conformance_suite_configurations/dba_multi_2024.py
+++ tests/integration_tests/validation/conformance_suite_configurations/dba_multi_2024.py
@@ -8,10 +8,10 @@ config = ConformanceSuiteConfig(
     ],
     assets=[
         ConformanceSuiteAssetConfig.local_conformance_suite(
-            Path('dba_multi'),
+            Path('dba_multi_2024'),
             entry_point=Path('index.xml'),
         ),
-        ConformanceSuiteAssetConfig.public_taxonomy_package(Path('ARL-XBRL20221001-20221117.zip')),
+        ConformanceSuiteAssetConfig.public_taxonomy_package(Path('ARL-XBRL20241001-20240930-3_U.zip')),
     ],
     info_url='https://erhvervsstyrelsen.dk/vejledning-teknisk-vejledning-og-dokumentation-regnskab-20-taksonomier-aktuelle',
     name=PurePath(__file__).stem,
