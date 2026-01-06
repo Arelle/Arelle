e21f97ec07e71aa60ee5320de8addb254d0a464d
Aaron Cameron <aaron.cameron@workiva.com>
2026-01-05 21:46:10 -0700

    CS config: ignore_levels

--- tests/integration_tests/validation/conformance_suite_configurations/kvk_nt19.py
+++ tests/integration_tests/validation/conformance_suite_configurations/kvk_nt19.py
@@ -1,5 +1,6 @@
 from pathlib import PurePath, Path
 
+from arelle.testengine.ErrorLevel import ErrorLevel
 from tests.integration_tests.validation.assets import NL_PACKAGES
 from tests.integration_tests.validation.conformance_suite_config import ConformanceSuiteConfig, ConformanceSuiteAssetConfig, AssetSource
 
@@ -19,6 +20,11 @@ config = ConformanceSuiteConfig(
     ],
     base_taxonomy_validation='none',
     disclosure_system='NT19',
+    ignore_levels=frozenset({
+        ErrorLevel.NOT_SATISFIED,
+        ErrorLevel.OK,
+        ErrorLevel.WARNING,
+    }),
     info_url='https://www.sbr-nl.nl/sites/default/files/bestanden/taxonomie/20240301%20SBR%20Filing%20Rules%20NT19.pdf',
     name=PurePath(__file__).stem,
     plugins=frozenset({'validate/NL'}),
