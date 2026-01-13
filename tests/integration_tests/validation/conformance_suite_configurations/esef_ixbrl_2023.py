5e5e698d4815db9c4aa34a51b6dfeaebccf0359b
Aaron Cameron <aaron.cameron@workiva.com>
2026-01-13 12:34:19 -0700

    Preprocessing function for conformance suite configs

--- tests/integration_tests/validation/conformance_suite_configurations/esef_ixbrl_2023.py
+++ tests/integration_tests/validation/conformance_suite_configurations/esef_ixbrl_2023.py
@@ -5,6 +5,23 @@ from tests.integration_tests.validation.conformance_suite_config import (
     ConformanceSuiteConfig, ConformanceSuiteAssetConfig, AssetSource
 )
 
+
+def _preprocessing_func(config: ConformanceSuiteConfig) -> None:
+    with open(
+            config.entry_point_root /
+            "tests/inline_xbrl/G2-6-1_3/index.xml",
+            "r+"
+    ) as f:
+        content = f.read()
+        # Test case references TC2_invalid.zip, but actual file in suite has .xbr extension.
+        content = content.replace("TC2_invalid.zip", "TC2_invalid.xbr")
+        # Test case references TC3_invalid.zip, but actual file in suite has .xbri extension.
+        content = content.replace("TC3_invalid.zip", "TC3_invalid.xbri")
+        f.seek(0)
+        f.write(content)
+        f.truncate()
+
+
 ZIP_PATH = Path("esef_conformance_suite_2023.zip")
 EXTRACTED_PATH = Path(ZIP_PATH.stem)
 config = ConformanceSuiteConfig(
@@ -25,9 +42,6 @@ config = ConformanceSuiteConfig(
     expected_failure_ids=frozenset(f"tests/inline_xbrl/{s}" for s in [
         # Test report uses older domain item type (http://www.xbrl.org/dtr/type/non-numeric) forbidden by ESEF.3.2.2.
         "G3-1-2/index.xml:TC2_valid",
-        # These tests reference zip files, which do not exist in the conformance suite.
-        "G2-6-1_3/index.xml:TC2_invalid",
-        "G2-6-1_3/index.xml:TC3_invalid",
         # The following test case fails because of the `tech_duplicated_facts1` formula, which incorrectly fires.
         # It does not take into account the language attribute on the fact.
         # Facts are not duplicates if their language attributes are different.
@@ -36,6 +50,7 @@ config = ConformanceSuiteConfig(
     info_url="https://www.esma.europa.eu/document/esef-conformance-suite-2023",
     name=PurePath(__file__).stem,
     plugins=frozenset({"validate/ESEF"}),
+    preprocessing_func=_preprocessing_func,
     shards=8,
     test_case_result_options="match-any",
 )
