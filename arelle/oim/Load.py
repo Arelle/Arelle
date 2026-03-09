17d4661828abe77ab7f6f916c1ea99bfa02cd7d2
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-03-09 14:51:56 -0400

    Integrate TC metadata parsing into xBRL-CSV loading pipeline

--- arelle/oim/Load.py
+++ arelle/oim/Load.py
@@ -22,6 +22,7 @@ from arelle import (ModelDocument, ModelXbrl, PackageManager, UrlUtil,
                     ValidateXbrlDimensions, XbrlConst, XmlUtil, XmlValidate)
 from arelle.ModelValue import (DATETIME, dateTime, dayTimeDuration, qname,
                                yearMonthDuration)
+from arelle.oim._tc.metadata.parser import parse_tc_metadata
 from arelle.PluginManager import pluginClassMethods
 from arelle.PrototypeInstanceObject import DimValuePrototype
 from arelle.PythonUtil import attrdict, isLegacyAbs, strTruncate
@@ -1229,6 +1230,15 @@ def _loadFromOIM(cntlr, error, warning, modelXbrl, oimFile, mappedUri):
             documentInfoProperties.add("final")
             reportProperties = {"documentInfo", "tableTemplates", "tables", "parameters", "parameterURL", "dimensions", "decimals", "links"}
             columnProperties = {"comment", "decimals", "dimensions", "propertyGroups", "parameterURL", "propertiesFrom"}
+            tcMetadataResult = parse_tc_metadata(oimObject, namespaces)
+            modelXbrl.tcMetadata = tcMetadataResult.metadata
+            for err in tcMetadataResult.errors:
+                error(
+                    err.code,
+                    _("Invalid table constraints metadata: %(error)s"),
+                    modelObject=modelXbrl,
+                    error=str(err),
+                )
 
         entityNaQName = qname(re.sub("/xbrl-(json|csv)$","/entities",documentType), "NA")
         allowedDuplicatesFeature = ALL
