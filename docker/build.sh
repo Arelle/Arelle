26d6fe0361bd26e8590d00f74bc20ee7f6fb803a
Ryan Geiser <ryangholdings@gmail.com>
2026-03-19 11:11:26 -0600

    Remove build scripts

--- docker/build.sh
+++
@@ -1,6 +0,0 @@
-# Move Docker compose file to Arelle directory
-rm docker-compose.yml
-cp ./docker/docker-compose.yml .
-
-# Build and run docker container with docker compose
-docker compose -f docker-compose.yml up -d --build
