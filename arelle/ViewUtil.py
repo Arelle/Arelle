d1ba28fba6a0eb4a021cd93cdaefe3ceed272f51
Stuart Rowan <stuart.rowan@strr.org>
2026-04-03 15:32:59 +0100

    Take opportunity to clean up the ViewUtil methods, specifically:
    Use match/case
    Use list comprehensions
    Use Counter
    Use next()
    
    No functional change intended.

--- arelle/ViewUtil.py
+++ arelle/ViewUtil.py
@@ -1,16 +1,18 @@
-'''
+"""
 See COPYRIGHT.md for copyright information.
-'''
-from arelle import (XmlUtil, XbrlConst)
+"""
+
 from __future__ import annotations
 
 import os
-from arelle.typing import TypeGetText
+from collections import Counter
+from operator import attrgetter
 from typing import TYPE_CHECKING
 
 from arelle import XbrlConst, XmlUtil
 from arelle.ModelObject import ModelObject
 from arelle.ModelRelationshipSet import ModelRelationshipSet
+from arelle.typing import TypeGetText
 
 if TYPE_CHECKING:
     from arelle.ModelXbrl import ModelXbrl
@@ -25,28 +27,31 @@ def titleFromUri(arcrole: str) -> str:
 
 
 def baseSetArcroleLabel(arcrole: str) -> str:  # with sort char in first position
-    if arcrole == "XBRL-dimensions":
-        return _("1Dimension")
-    elif arcrole == "XBRL-formulae":
-        return _("1Formula")
-    elif arcrole == "Table-rendering":
-        return _("1Rendering")
-    elif arcrole == XbrlConst.parentChild:
-        return _("1Presentation")
-    elif arcrole in (XbrlConst.summationItem, XbrlConst.summationItem11):
-        return _("1Calculation")
-    elif arcrole == XbrlConst.widerNarrower:
-        return "1Anchoring"
-    else:
-        return "2" + titleFromUri(arcrole)
+    match arcrole:
+        case "XBRL-dimensions":
+            return _("1Dimension")
+        case "XBRL-formulae":
+            return _("1Formula")
+        case "Table-rendering":
+            return _("1Rendering")
+        case XbrlConst.parentChild:
+            return _("1Presentation")
+        case XbrlConst.summationItem | XbrlConst.summationItem11:
+            return _("1Calculation")
+        case XbrlConst.widerNarrower:
+            return "1Anchoring"
+        case _:
+            return "2" + titleFromUri(arcrole)
 
 
 def labelroleLabel(role: str) -> str:  # with sort char in first position
-    if role == XbrlConst.standardLabel:
-        return _("1Standard Label")
-    elif role == XbrlConst.conceptNameLabelRole:
-        return _("0Name")
-    return "3" + titleFromUri(role)
+    match role:
+        case XbrlConst.standardLabel:
+            return _("1Standard Label")
+        case XbrlConst.conceptNameLabelRole:
+            return _("0Name")
+        case _:
+            return "3" + titleFromUri(role)
 
 
 def baseSetArcroles(modelXbrl: ModelXbrl):
@@ -56,57 +61,75 @@ def baseSetArcroles(modelXbrl: ModelXbrl):
     )
 
 
-def labelroles(modelXbrl, includeConceptName=False):
+def labelroles(modelXbrl: ModelXbrl, includeConceptName=False) -> list[tuple[str, str]]:
     # returns sorted list of tuples of arcrole basename and uri
-    return sorted(set((XbrlConst.labelroleLabel(r),r)
-                        for r in (modelXbrl.labelroles | ({XbrlConst.conceptNameLabelRole} if includeConceptName else set()))
-                        if r is not None))
+    allRoles: set[str] = (
+        (modelXbrl.labelroles | {XbrlConst.conceptNameLabelRole})
+        if includeConceptName
+        else modelXbrl.labelroles
+    )
+    return sorted((labelroleLabel(r), r) for r in allRoles if r is not None)
 
 
 # clean references for viewability
 def viewReferences(concept):
-    references = []
-    for refrel in concept.modelXbrl.relationshipSet(XbrlConst.conceptReference).fromModelObject(concept):
-        ref = refrel.toModelObject
-        if ref is not None:
-            references.append(ref.viewText())
-    return ", ".join(references)
+    return ", ".join(
+        ref.viewText()
+        for refrel in concept.modelXbrl.relationshipSet(
+            XbrlConst.conceptReference
+        ).fromModelObject(concept)
+        if (ref := refrel.toModelObject) is not None
+    )
+
 
 def referenceURI(concept):
-    for refrel in concept.modelXbrl.relationshipSet(XbrlConst.conceptReference).fromModelObject(concept):
-        ref = refrel.toModelObject
-        if ref is not None:
-            for resourceElt in ref.iter():
-                if isinstance(resourceElt,ModelObject) and resourceElt.localName == "URI":
-                    return XmlUtil.text(resourceElt)
-    return None
+    return next(
+        (
+            XmlUtil.text(resourceElt)
+            for refrel in concept.modelXbrl.relationshipSet(
+                XbrlConst.conceptReference
+            ).fromModelObject(concept)
+            if (ref := refrel.toModelObject) is not None
+            for resourceElt in ref.iter()
+            if isinstance(resourceElt, ModelObject) and resourceElt.localName == "URI"
+        ),
+        None,
+    )
+
 
 def groupRelationshipSet(modelXbrl, arcrole, linkrole, linkqname, arcqname):
-    if isinstance(arcrole, (list,tuple)): # (group-name, [arcroles])
+    if isinstance(arcrole, (list, tuple)):  # (group-name, [arcroles])
         arcroles = arcrole[1]
-        relationshipSet = ModelRelationshipSet(modelXbrl, arcroles[0], linkrole, linkqname, arcqname)
-        for arcrole in arcroles[1:]:
-            if arcrole != ARCROLE_GROUP_DETECT_STR:
-                rels = modelXbrl.relationshipSet(arcrole, linkrole, linkqname, arcqname)
-                if rels:
-                    relationshipSet.modelRelationships.extend(rels.modelRelationships)
-        relationshipSet.modelRelationships.sort(key=lambda rel: rel.order)
+        relationshipSet = ModelRelationshipSet(
+            modelXbrl, arcroles[0], linkrole, linkqname, arcqname
+        )
+        relationshipSet.modelRelationships.extend(
+            rel
+            for arc in arcroles[1:]
+            if arc != ARCROLE_GROUP_DETECT_STR
+            if (rels := modelXbrl.relationshipSet(arc, linkrole, linkqname, arcqname))
+            for rel in rels.modelRelationships
+        )
+        relationshipSet.modelRelationships.sort(key=attrgetter("order"))
     else:
-        relationshipSet = modelXbrl.relationshipSet(arcrole, linkrole, linkqname, arcqname)
+        relationshipSet = modelXbrl.relationshipSet(
+            arcrole, linkrole, linkqname, arcqname
+        )
     return relationshipSet
 
+
 def groupRelationshipLabel(arcrole):
-    if isinstance(arcrole, (list,tuple)): # (group-name, [arcroles])
+    if isinstance(arcrole, (list, tuple)):  # (group-name, [arcroles])
         arcroleName = arcrole[0]
     else:
         arcroleName = baseSetArcroleLabel(arcrole)[1:]
     return arcroleName
 
+
 def sortCountExpected(expected):
     if isinstance(expected, list):
-        _expected = {}
-        for e in expected:
-            _expected[e] = _expected.get(e, 0) + 1
-        return [str(e) if qty == 1 else "{} ({})".format(e,qty)
-                for e, qty in sorted(_expected.items(), key=lambda i:str(i[0]))]
+        return [
+            s if qty == 1 else f"{s} ({qty})"
+            for s, qty in sorted((str(e), qty) for e, qty in Counter(expected).items())
+        ]
     return expected
