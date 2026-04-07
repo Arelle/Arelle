1c3ccc2ee7e554f0161f0b4343abb5f0fc6087ea
Stuart Rowan <stuart.rowan@strr.org>
2026-04-07 15:47:35 +0100

    Remove dead code.
    py3unquote was leftover from when Arelle used Python 2.

--- arelle/PythonUtil.py
+++ arelle/PythonUtil.py
@@ -20,50 +20,6 @@ from arelle.typing import OptionalString
 
 STR_NUM_TYPES = (str, int, float, Decimal, fractions.Fraction)
 
-# python 3 unquote, because py2 unquote doesn't do utf-8 correctly
-def py3unquote(string, encoding='utf-8', errors='replace'):
-    """Replace %xx escapes by their single-character equivalent. The optional
-    encoding and errors parameters specify how to decode percent-encoded
-    sequences into Unicode characters, as accepted by the bytes.decode()
-    method.
-    By default, percent-encoded sequences are decoded with UTF-8, and invalid
-    sequences are replaced by a placeholder character.
-
-    unquote('abc%20def') -> 'abc def'.
-    """
-    if string == '':
-        return string
-    res = string.split('%')
-    if len(res) == 1:
-        return string
-    if encoding is None:
-        encoding = 'utf-8'
-    if errors is None:
-        errors = 'replace'
-    # pct_sequence: contiguous sequence of percent-encoded bytes, decoded
-    pct_sequence = b''
-    string = res[0]
-    for item in res[1:]:
-        try:
-            if not item:
-                raise ValueError
-            pct_sequence += str(bytearray.fromhex(item[:2]))
-            rest = item[2:]
-            if not rest:
-                # This segment was just a single percent-encoded character.
-                # May be part of a sequence of code units, so delay decoding.
-                # (Stored in pct_sequence).
-                continue
-        except ValueError:
-            rest = '%' + item
-        # Encountered non-percent-encoded characters. Flush the current
-        # pct_sequence.
-        string += pct_sequence.decode(encoding, errors) + rest
-        pct_sequence = b''
-    if pct_sequence:
-        # Flush the final pct_sequence
-        string += pct_sequence.decode(encoding, errors)
-    return string
 
 def pyTypeName(object):
     try:
