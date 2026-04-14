804b333330281d385124d8609eb99eac865d21be
volodymyrtur-wk <volodymyr.tur@workiva.com>
2026-04-14 14:06:49 +0300

    ModelDocumentType: Add type hints, refactor type ignore comments, and improve type safety

--- arelle/ModelDocumentType.py
+++ arelle/ModelDocumentType.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+import io
+
 from lxml import etree
 
 from arelle import FileSource
@@ -9,31 +13,31 @@ class ModelDocumentType:
 
     Static class of Enumerated type representing modelDocument type
     """
-    UnknownXML=0
-    UnknownNonXML=1
-    UnknownTypes=1  # to test if any unknown type, use <= Type.UnknownTypes
-    firstXBRLtype=2  # first filetype that is XBRL and can hold a linkbase, etc inside it
-    SCHEMA=2
-    LINKBASE=3
-    INSTANCE=4
-    INLINEXBRL=5
-    lastXBRLtype=5  # first filetype that is XBRL and can hold a linkbase, etc inside it
-    DTSENTRIES=6  # multiple schema/linkbase Refs composing a DTS but not from an instance document
-    INLINEXBRLDOCUMENTSET=7
-    VERSIONINGREPORT=8
-    TESTCASESINDEX=9
-    TESTCASE=10
-    REGISTRY=11
-    REGISTRYTESTCASE=12
-    XPATHTESTSUITE=13
-    RSSFEED=14
-    ARCSINFOSET=15
-    FACTDIMSINFOSET=16
-    HTML=17
+    UnknownXML: int = 0
+    UnknownNonXML: int = 1
+    UnknownTypes: int = 1  # to test if any unknown type, use <= Type.UnknownTypes
+    firstXBRLtype: int = 2  # first filetype that is XBRL and can hold a linkbase, etc inside it
+    SCHEMA: int = 2
+    LINKBASE: int = 3
+    INSTANCE: int = 4
+    INLINEXBRL: int = 5
+    lastXBRLtype: int = 5  # first filetype that is XBRL and can hold a linkbase, etc inside it
+    DTSENTRIES: int = 6  # multiple schema/linkbase Refs composing a DTS but not from an instance document
+    INLINEXBRLDOCUMENTSET: int = 7
+    VERSIONINGREPORT: int = 8
+    TESTCASESINDEX: int = 9
+    TESTCASE: int = 10
+    REGISTRY: int = 11
+    REGISTRYTESTCASE: int = 12
+    XPATHTESTSUITE: int = 13
+    RSSFEED: int = 14
+    ARCSINFOSET: int = 15
+    FACTDIMSINFOSET: int = 16
+    HTML: int = 17
 
-    TESTCASETYPES = (TESTCASESINDEX, TESTCASE, REGISTRY, REGISTRYTESTCASE, XPATHTESTSUITE)
+    TESTCASETYPES: tuple[int, ...] = (TESTCASESINDEX, TESTCASE, REGISTRY, REGISTRYTESTCASE, XPATHTESTSUITE)
 
-    typeName = ("unknown XML",
+    typeName: tuple[str, ...] = ("unknown XML",
                 "unknown non-XML",
                 "schema",
                 "linkbase",
@@ -53,13 +57,14 @@ class ModelDocumentType:
                 "html non-XBRL")
 
     @staticmethod
-    def identify(filesource: FileSource, filepath: str) -> int:
-        _type = ModelDocumentType.UnknownNonXML
-        _file, = filesource.file(filepath, stripDeclaration=True, binary=True)
+    def identify(filesource: FileSource.FileSource, filepath: str) -> int:
+        _type: int = ModelDocumentType.UnknownNonXML
+        _file: io.BytesIO = filesource.file(filepath, stripDeclaration=True, binary=True)[0]  # type: ignore[assignment]
         try:
-            _rootElt = True
-            _maybeHtml = False
+            _rootElt: bool = True
+            _maybeHtml: bool = False
             for _event, elt in etree.iterparse(_file, events=("start",), recover=True, huge_tree=True):
+                assert isinstance(elt, etree.Element)
                 if _rootElt:
                     _rootElt = False
                     _type = {"testcases": ModelDocumentType.TESTCASESINDEX,
@@ -69,19 +74,19 @@ class ModelDocumentType:
                              "testcase": ModelDocumentType.TESTCASE,
                              "testSet": ModelDocumentType.TESTCASE,
                              "rss": ModelDocumentType.RSSFEED
-                        }.get(etree.QName(elt).localname)
+                        }.get(etree.QName(elt).localname, ModelDocumentType.UnknownXML)
                     if _type:
                         break
                     _type = {"{http://www.xbrl.org/2003/instance}xbrl": ModelDocumentType.INSTANCE,
                              "{http://www.xbrl.org/2003/linkbase}linkbase": ModelDocumentType.LINKBASE,
                              "{http://www.w3.org/2001/XMLSchema}schema": ModelDocumentType.SCHEMA,
                              "{http://xbrl.org/2008/registry}registry": ModelDocumentType.REGISTRY
-                             }.get(elt.tag, ModelDocumentType.UnknownXML)
-                    if _type == ModelDocumentType.UnknownXML and elt.tag.endswith("html"):
+                             }.get(elt.tag, ModelDocumentType.UnknownXML)  # type: ignore[arg-type]
+                    if _type == ModelDocumentType.UnknownXML and elt.tag.endswith("html"):  # type: ignore[union-attr,arg-type]
                         _maybeHtml = True
                     else:
                         break # stop parsing
-                if XbrlConst.ixbrlTagPattern.match(elt.tag):
+                if XbrlConst.ixbrlTagPattern.match(str(elt.tag)):
                     _type = ModelDocumentType.INLINEXBRL
                     break
             if _type == ModelDocumentType.UnknownXML and _maybeHtml:
