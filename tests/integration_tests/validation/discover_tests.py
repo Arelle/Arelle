013684ecb0dcf01dae80a24cc469520a88cb750d
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-04-23 21:39:20 -0700

    Bump Python patch versions

--- tests/integration_tests/validation/discover_tests.py
+++ tests/integration_tests/validation/discover_tests.py
@@ -16,9 +16,9 @@ ALL_PYTHON_VERSIONS = (
     '3.11',
     '3.12',
     '3.13',
-    '3.14.2',
+    '3.14.4',
 )
-LATEST_PYTHON_VERSION = '3.14.2'
+LATEST_PYTHON_VERSION = '3.14.4'
 
 
 class Entry(TypedDict, total=False):
