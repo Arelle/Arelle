9785bce23315fd0ae78031508181918ac3137f43
Ryan Geiser <ryangholdings@gmail.com>
2026-06-03 17:48:58 -0600

    Print --about information before starting webserver in docker and cleanup comments

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,8 +4,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+python arelleCmdLine.py --about
 python arelleCmdLine.py --webserver=0.0.0.0:8080
-
-## Optionally run with gunicorn with 2 workers, recycling workers after each request
-## WARNING: Uncomment only one command at a time.
-# gunicorn "arelle.CntlrCmdLine:wsgiApplication()" --workers 2 --timeout 180 --max-requests 1 --bind 0.0.0.0:8080
