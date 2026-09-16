e4385070fd2509b1b1caa3b7a368a55e01bd3f5d
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-09-16 15:11:22 +0000

    Format concept reference property view

--- arelle/ModelDtsObject.py
+++ arelle/ModelDtsObject.py
@@ -968,12 +968,23 @@ class ModelConcept(ModelNamableTerm, ModelParticle):
         else:
             _labelProperty = ("label", _labelDefault)
 
-        _refT = tuple((self.modelXbrl.roleTypeDefinition(_ref.role, _lang), " ",  # type: ignore[union-attr]
-                       tuple((_refPart.localName, _refPart.stringValue.strip())
-                             for _refPart in _ref.iterchildren()))  # type: ignore[union-attr]
-                      for _refRel in sorted(self.modelXbrl.relationshipSet(XbrlConst.conceptReference).fromModelObject(self),
-                                            key=lambda r:r.toModelObject.roleRefPartSortKey())  # type: ignore[union-attr]
-                      for _ref in (_refRel.toModelObject,))
+        _refT = tuple(
+            (
+                self.modelXbrl.roleTypeDefinition(_ref.role, _lang),  # type: ignore[union-attr]
+                " ",
+                tuple(
+                    (_refPart.localName, _refPart.stringValue.strip())
+                    for _refPart in _ref.iterchildren()  # type: ignore[union-attr]
+                ),
+            )
+            for _refRel in sorted(
+                self.modelXbrl.relationshipSet(
+                    XbrlConst.conceptReference
+                ).fromModelObject(self),
+                key=lambda r: r.toModelObject.roleRefPartSortKey(),  # type: ignore[union-attr]
+            )
+            for _ref in (_refRel.toModelObject,)
+        )
         _refsStrung = " ".join(_refPart.stringValue.strip()
                                for _refRel in self.modelXbrl.relationshipSet(XbrlConst.conceptReference).fromModelObject(self)
                                for _refPart in _refRel.toModelObject.iterchildren())  # type: ignore[union-attr]
