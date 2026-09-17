f77d0398b9b6f28017ddd7747fded440d24e53a9
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-09-17 18:24:05 +0000

    Remove obsolete standalone profiler
    The profiler cannot import its dependencies and requires a file on the
    original developer's machine.

--- tests/unit_tests/arelle/test_import.py
+++ tests/unit_tests/arelle/test_import.py
@@ -7,7 +7,6 @@ import pytest
 from tests.unit_tests.common import THIRD_PARTY_PATH_PREFIXES
 
 KNOWN_FAILURES = frozenset([
-    "arelle.CntlrProfiler",
     "arelle.FunctionXfi",
     "arelle.ViewCsvRelationshipSet",
     "arelle.archive.LoadSavePreLbCsv",
